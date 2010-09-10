8f2a642d9c559ca6d66cffed92ab784047485f41
Ken Zalewski <zalewski@nysenate.gov>
2010-09-10 03:26:05 -0400

    No longer rsyncing the templates, since these are also customized for the current environment.

--- scripts/rsync_bluebird.sh
+++ scripts/rsync_bluebird.sh
@@ -27,10 +27,6 @@ done
 
 # rsync the scripts/, senateProduction/, and www/ dirs, but exclude sync
 # for all sites/ directories that are of the form *.*.  This will skip
-# sd99.crm.nysenate.gov, but not "all" and "default".
+# <instance>.crm.nysenate.gov, but not "all" and "default".
 rsync -av $dry_run_opt $delete_opt --exclude sites/*.* $HOME/Bluebird-CRM/* /data/
-
-# Since the sites/ subdirs are skipped above (other than "all" and "default",
-# the following rsync handles any "template" sites.
-rsync -avR $dry_run_opt $delete_opt $HOME/Bluebird-CRM/./www/*/sites/template.* /data/
 
