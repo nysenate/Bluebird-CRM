5c90eef3c573f65f95fda1f0014f86340172edb7
Ken Zalewski <zalewski@nysenate.gov>
2010-09-10 13:46:44 -0400

    Display chown and chgrp commands as they happen.

--- scripts/fixPermissions.sh
+++ scripts/fixPermissions.sh
@@ -7,21 +7,23 @@ webdir=$basedir/www
 defuser="www-data"
 defgroup="bluebird"
 
-set -x
-
 for d in nyss nyssdev; do
   dirpath=$webdir/$d
   if [ -d $dirpath ]; then
-    chown -R $defuser:$defgroup $dirpath
-    chmod -R ug+rw,o-w $dirpath
+    ( set -x
+      chown -R $defuser:$defgroup $dirpath
+      chmod -R ug+rw,o-w $dirpath
+    )
   fi
 done
 
 for d in senateProduction senateDevelopment; do
   dirpath=$basedir/$d
   if [ -d $dirpath ]; then
-    chgrp -R $defgroup $dirpath
-    chmod -R ug+rw,o-w $dirpath
+    ( set -x
+      chgrp -R $defgroup $dirpath
+      chmod -R ug+rw,o-w $dirpath
+    )
   fi
 done
 
