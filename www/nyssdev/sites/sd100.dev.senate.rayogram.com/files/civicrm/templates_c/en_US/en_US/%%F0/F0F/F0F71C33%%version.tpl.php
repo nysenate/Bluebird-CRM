7bae07021bba2497cab96f903ab6b8b25d03c511
Ken Zalewski <zalewski@nysenate.gov>
2010-09-10 00:53:40 -0400

    Deleted all nyssdev/sites subdirectories other than template.dev.sneate.rayogram.com, since all others are created as part of the instance initialization process and are not part of the source tree.  Deleted some extraneous files in /data/www also.

--- www/nyssdev/sites/sd100.dev.senate.rayogram.com/files/civicrm/templates_c/en_US/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-05-28 16:20:33
-         compiled from CRM/common/version.tpl */ ?>
-exported
--- www/nyssdev/sites/sd99.dev.senate.rayogram.com/files/civicrm/templates_c/en_US/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-07-06 10:37:47
-         compiled from CRM/common/version.tpl */ ?>
-exported
