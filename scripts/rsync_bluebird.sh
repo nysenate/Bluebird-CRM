d2a4bc03ef541ae6bad9995733f4db934ddf898f
Ken Zalewski <zalewski@nysenate.gov>
2010-09-10 02:44:58 -0400

    More accurate syncing to avoid modifying/deleting any CRM sites other than template.crm.nysenate.gov.

--- scripts/rsync_bluebird.sh
+++ scripts/rsync_bluebird.sh
@@ -25,5 +25,12 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-rsync -av $dry_run_opt $delete_opt $HOME/Bluebird-CRM/* /data/
+# rsync the scripts/, senateProduction/, and www/ dirs, but exclude sync
+# for all sites/ directories that are of the form *.*.  This will skip
+# sd99.crm.nysenate.gov, but not "all" and "default".
+rsync -av $dry_run_opt $delete_opt --exclude sites/*.* $HOME/Bluebird-CRM/* /data/
+
+# Since the sites/ subdirs are skipped above (other than "all" and "default",
+# the following rsync handles any "template" sites.
+rsync -avR $dry_run_opt $delete_opt $HOME/Bluebird-CRM/./www/*/sites/template.* /data/
 
