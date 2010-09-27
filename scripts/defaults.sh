26556c9e620fbcfe4d1fabe690490d4ec9764206
Ken Zalewski <zalewski.senate@gmail.com>
2010-09-27 13:39:59 -0400

    Assign reasonable defaults to configuration parameters.

---
+++ scripts/defaults.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# defaults.sh - Shell defaults when using the Bluebird config file.
+#
+# Project: BluebirdCRM
+# Author: Ken Zalewski
+# Organization: New York State Senate
+# Date: 2010-09-27
+#
+
+DEFAULT_CONFIG_FILE=/etc/bluebird.ini
+
+DEFAULT_DB_HOST=localhost
+DEFAULT_DB_USER=crmadmin
+DEFAULT_DB_PASS=NONE
+DEFAULT_DB_DRUPAL_PREFIX=senate_d_
+DEFAULT_DB_CIVICRM_PREFIX=senate_c_
+
+DEFAULT_HTTP_USER=loadsenate
+DEFAULT_HTTP_PASS=NONE
+
+DEFAULT_APP_ROOTDIR=/opt/bluebird
+DEFAULT_WWW_ROOTDIR=/var/www
+
+DEFAULT_BACKUP_HOST=localhost
+DEFAULT_BACKUP_ROOTDIR=/crmbackups
+
+DEFAULT_BASE_DOMAIN=crm.nysenate.gov
+
+DEFAULT_OWNER_USER=www-data
+DEFAULT_OWNER_GROUP=bluebird
+
