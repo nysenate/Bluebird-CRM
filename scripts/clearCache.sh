51e4217d085c101f77cadcedaf5de517636c7b74
Ken Zalewski <zalewski.senate@gmail.com>
2010-09-27 14:48:19 -0400

    Using defaults.sh to set reasonable defaults for config parameters.  Using configurable table prefixes.

--- scripts/clearCache.sh
+++ scripts/clearCache.sh
@@ -2,31 +2,50 @@
 #
 # clearCache.sh
 #
+# Project: BluebirdCRM
+# Author: Ken Zalewski
+# Organization: New York State Senate
+# Date: 2010-09-15
+# Revised: 2010-09-27
+#
 
 prog=`basename $0`
 script_dir=`dirname $0`
 execSql=$script_dir/execSql.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
 
 if [ $# -ne 1 ]; then
   echo "Usage: $prog instanceName" >&2
   exit 1
 fi
 
+db_civicrm_prefix=`$readConfig --group globals db.civicrm.prefix`
+db_drupal_prefix=`$readConfig --group globals db.drupal.prefix`
+
 instance="$1"
+
+[ "$db_civicrm_prefix" ] || db_civicrm_prefix="$DEFAULT_DB_CIVICRM_PREFIX"
+[ "$db_drupal_prefix" ] || db_drupal_prefix="$DEFAULT_DB_DRUPAL_PREFIX"
+
+echo "Clearing CiviCRM database caches"
+sql="truncate civicrm_cache; truncate civicrm_menu; truncate civicrm_uf_match;"
+( set -x
+  $execSql "$db_civicrm_prefix$instance" -c "$sql"
+)
+
+echo "Clearing CiviCRM filesystem caches"
+( set -x
+  rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/templates_c/*
+  rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/css/*
+  rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/js/*
+)
 
 echo "Clearing Drupal database caches"
 sql="truncate cache; truncate cache_page; truncate cache_form; truncate cache_update; truncate cache_menu; truncate cache_block; truncate cache_filter; truncate sessions;"
-$execSql senate_d_$instance -c "$sql"
-
-echo "Clearing CiviCRM database caches"
-sql="truncate civicrm_cache; truncate civicrm_menu; truncate civicrm_uf_match";
-set -x
-$execSql senate_c_$instance -c "$sql"
-
-echo "Clearing CiviCRM filesystem caches"
-rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/templates_c/*
-rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/css/*
-rm -rf /data/files/$instance.crm.nysenate.gov/civicrm/js/*
-set +x
+( set -x
+  $execSql "$db_drupal_prefix$instance" -c "$sql"
+)
 
 exit 0
