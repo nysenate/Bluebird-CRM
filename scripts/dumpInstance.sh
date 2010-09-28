1ae2404f5edf49ef870c404e2a2dd19c626c1115
Ken Zalewski <zalewski.senate@gmail.com>
2010-09-28 06:30:41 -0400

    Better debugging and error control.

--- scripts/dumpInstance.sh
+++ scripts/dumpInstance.sh
@@ -6,11 +6,15 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-09-12
+# Revised: 2010-09-27
 #
 
 prog=`basename $0`
 script_dir=`dirname $0`
 execSql=$script_dir/execSql.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
 
 if [ $# -ne 1 ]; then
   echo "Usage: $prog instanceName" >&2
@@ -18,14 +22,18 @@ if [ $# -ne 1 ]; then
 fi
 
 instance="$1"
-
-echo "Dumping Drupal database for instance [$instance]"
-set -x
-$execSql --dump senate_d_$instance > senate_d_$instance.sql
-set +x
+db_civicrm_prefix=`$readConfig --ig $instance db.civicrm.prefix` || db_civicrm_prefix="$DEFAULT_DB_CIVICRM_PREFIX"
+db_drupal_prefix=`$readConfig --ig $instance db.drupal.prefix` || db_drupal_prefix="$DEFAULT_DB_DRUPAL_PREFIX"
+errcode=0
 
 echo "Dumping CiviCRM database for instance [$instance]"
-set -x
-$execSql --dump senate_c_$instance > senate_c_$instance.sql
+( set -x
+  $execSql --dump $db_civicrm_prefix$instance > $db_civicrm_prefix$instance.sql
+) || errcode=$(($errcode | 1))
 
-exit 0
+echo "Dumping Drupal database for instance [$instance]"
+( set -x
+  $execSql --dump $db_drupal_prefix$instance > $db_drupal_prefix$instance.sql
+) || errcode=$(($errcode | 2))
+
+exit $errcode
