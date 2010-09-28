b21ca5042b3529c7ae6e8bf48db1f3c73a3bd891
Ken Zalewski <zalewski.senate@gmail.com>
2010-09-28 05:54:52 -0400

    Added --instance option to specify a particular instance to run SQL against.

--- scripts/execSql.sh
+++ scripts/execSql.sh
@@ -6,6 +6,7 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-09-23
+# Revised: 2010-09-27
 #
 
 prog=`basename $0`
@@ -15,35 +16,55 @@ readConfig=$script_dir/readConfig.sh
 . $script_dir/defaults.sh
 
 if [ $# -lt 1 ]; then
-  echo "Usage: $prog dbName [-f sqlFile | -c sqlCommand] [-d] [-h host] [-u user] [-p password]" >&2
+  echo "Usage: $prog dbName [-f sqlFile | -c sqlCommand] [-d] [-i instance] [-h host] [-u user] [-p password] [--drupal]" >&2
   exit 1
 fi
 
 sqlfile=
 sqlcmd=
 dump_db=0
+instance=
+dbhost=
+dbuser=
+dbpass=
 dbname=
-dbhost=`$readConfig --group globals db.host`
-dbuser=`$readConfig --group globals db.user`
-dbpass=`$readConfig --group globals db.pass`
+db_prefix_keyname=db.civicrm.prefix
+default_db_prefix="$DEFAULT_DB_CIVICRM_PREFIX"
 
 while [ $# -gt 0 ]; do
   case "$1" in
     -f|--sqlfile) shift; sqlfile="$1" ;;
     -c|--cmd) shift; sqlcmd="$1" ;;
     -d|--dump) dump_db=1 ;;
+    -i|--instance) shift; instance="$1" ;;
     -h|--host) shift; dbhost="$1" ;;
     -u|--user) shift; dbuser="$1" ;;
     -p|--pass*) shift; dbpass="$1" ;;
+    --drupal) db_prefix_keyname=db.drupal.prefix; default_db_prefix="$DEFAULT_DB_DRUPAL_PREFIX" ;;
     -*) echo "$prog: $1: Invalid option" >&2; exit 1 ;;
     *) dbname="$1" ;;
   esac
   shift
 done
 
-[ "$dbhost" ] || dbhost=$DEFAULT_DB_HOST
-[ "$dbuser" ] || dbhost=$DEFAULT_DB_USER
-[ "$dbpass" ] || dbhost=$DEFAULT_DB_PASS
+ig_opt="--global"
+
+if [ "$instance" ]; then
+  ig_opt="--ig $instance"
+  if [ ! "$dbname" ]; then
+    db_basename=`$readConfig --instance $instance db.basename`
+    if [ $? -ne 0 ]; then
+      echo "$prog: $instance: Instance not found" >&2
+      exit 1
+    fi
+    db_prefix=`$readConfig $ig_opt $db_prefix_keyname` || db_prefix=$default_db_prefix
+    dbname="$db_prefix$db_basename"
+  fi
+fi
+ 
+[ "$dbhost" ] || dbhost=`$readConfig $ig_opt db.host` || dbhost=$DEFAULT_DB_HOST
+[ "$dbuser" ] || dbuser=`$readConfig $ig_opt db.user` || dbhost=$DEFAULT_DB_USER
+[ "$dbpass" ] || dbpass=`$readConfig $ig_opt db.pass` || dbhost=$DEFAULT_DB_PASS
 
 if [ $dump_db -eq 1 ]; then
   mysqldump -h $dbhost -u $dbuser -p$dbpass $dbname
