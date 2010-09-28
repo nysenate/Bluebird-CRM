fbd4c69a89a8a4656022b549180ad335ed3a9753
Ken Zalewski <zalewski.senate@gmail.com>
2010-09-28 13:12:07 -0400

    Fine-tuned permissions.

--- scripts/fixPermissions.sh
+++ scripts/fixPermissions.sh
@@ -5,16 +5,19 @@
 # Project: BluebirdCRM
 # Author: Ken Zalewski
 # Organization: New York State Senate
-# Date: 2010-09-27
+# Date: 2010-09-13
+# Revised: 2010-09-28
 #
 
 prog=`basename $0`
 script_dir=`dirname $0`
 readConfig=$script_dir/readConfig.sh
 
-webdir=`$readConfig --group globals www.rootdir`
-owner_user=`$readConfig --group globals owner.user`
-owner_group=`$readConfig --group globals owner.group`
+. $script_dir/defaults.sh
+
+webdir=`$readConfig --global www.rootdir` || webdir="$DEFAULT_WWW_ROOTDIR"
+owner_user=`$readConfig --global owner.user` || owner_user="$DEFAULT_OWNER_USER"
+owner_group=`$readConfig --global owner.group` || owner_group="$DEFAULT_OWNER_GROUP"
 
 if [ ! "$webdir" -o ! "$owner_user" -o ! "$owner_group" ]; then
   echo "$prog: Please set www.rootdir, owner.user, and owner.group in the Bluebird config file." >&2
@@ -22,7 +25,8 @@ if [ ! "$webdir" -o ! "$owner_user" -o ! "$owner_group" ]; then
 fi
 
 set -x
-chown -R $owner_user:$owner_group $webdir
-chmod -R ug+rw,o-w $webdir
+chown -R $owner_user:$owner_group $webdir/sites
+chmod -R u+rw,go+r-w $webdir
+chmod -R ug+rw,o-w $webdir/sites
 
 exit 0
