453737825434f25fcb9cdb8daaec9120474e0b52
Ken Zalewski <zalewski.senate@gmail.com>
2010-10-01 04:46:07 -0400

    Minor changes.

--- civicrm/scripts/commonLibs/config.php
+++ civicrm/scripts/commonLibs/config.php
@@ -1,12 +1,12 @@
 <?php
 
-define('BLUEBIRD_BASEDIR', realpath(dirname(__FILE__).'/../../../');
+define('BLUEBIRD_BASEDIR', realpath(dirname(__FILE__).'/../../../'));
 define('RAYCIVIPATH', BLUEBIRD_BASEDIR.'/modules/civicrm/');
 define('RAYDEBUG', true);
 define('RAYIMPORTDIR', BLUEBIRD_BASEDIR.'/importData/');
 define('RAYROOTDOMAIN', ".crm.nysenate.gov");
 define('RAYROOTDIR', BLUEBIRD_BASEDIR."/drupal/");
-define ('RAYTMP', '/tmp/');
+define('RAYTMP', '/tmp/');
 
 define('SOLRDEBUG', false);
 define('SOLRUPDATE', 1);
