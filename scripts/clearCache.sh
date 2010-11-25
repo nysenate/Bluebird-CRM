d80fdb2a5f8f2c0af3c8fdfa3e0a8542e78958c0
Ken Zalewski <zalewski@nysenate.gov>
2010-11-24 19:18:20 -0500

    Added civicrm_acl_cache, civicrm_acl_contact_cache, and civicrm_group_contact_cache to list of tables to be cleared.

--- scripts/clearCache.sh
+++ scripts/clearCache.sh
@@ -32,7 +32,7 @@ data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT
 base_domain=`$readConfig --ig $instance base.domain` || base_domain="$DEFAULT_BASE_DOMAIN"
 
 echo "Clearing CiviCRM database caches"
-sql="truncate civicrm_cache; truncate civicrm_menu; truncate civicrm_uf_match;"
+sql="truncate civicrm_acl_cache; truncate civicrm_acl_contact_cache; truncate civicrm_cache; truncate civicrm_group_contact_cache; truncate civicrm_menu; truncate civicrm_uf_match;"
 ( set -x
   $execSql -i $instance -c "$sql"
 )
