d28ccc50cf03137283a68e8249180af28321ee65
Ken Zalewski <zalewski.senate@gmail.com>
2010-12-23 14:52:23 -0500

    A Bluebird-aware wrapper around the drush utility.

---
+++ scripts/drush.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# drush.sh - A Bluebird-aware wrapper around the "drush" utility.
+#
+# Project: BluebirdCRM
+# Author: Ken Zalewski
+# Organization: New York State Senate
+# Date: 2010-12-23
+# Revised: 2010-12-23
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+usage() {
+  echo "Usage: $prog instanceName drush_command [drush_arg ...]" >&2
+}
+
+if [ $# -lt 2 ]; then
+  usage
+  exit 1
+fi
+
+instance="$1"
+shift
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: Instance [$instance] not found in config" >&2
+  exit 1
+fi
+
+drupal_rootdir=`$readConfig --ig $instance drupal.rootdir` || drupal_rootdir="$DEFAULT_DRUPAL_ROOTDIR"
+
+cd "$drupal_rootdir"
+INSTANCE_NAME="$instance" drush $@
+
+exit $?
