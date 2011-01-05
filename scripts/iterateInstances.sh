ec148cfbee426df22bf8bc3f616f0fa615d12320
Ken Zalewski <zalewski.senate@gmail.com>
2011-01-05 17:45:10 -0500

    Added --live option, which iterates over those instances that are live (meaning, a CiviCRM database has been created for that instance.)

--- scripts/iterateInstances.sh
+++ scripts/iterateInstances.sh
@@ -6,29 +6,32 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-12-03
-# Revised: 2010-12-23
+# Revised: 2011-01-05
 #
 
 prog=`basename $0`
 script_dir=`dirname $0`
 script_dir=`cd $script_dir; echo $PWD`
 readConfig=$script_dir/readConfig.sh
+execSql=$script_dir/execSql.sh
 
 
 usage() {
   echo "Usage: $prog [--all] [--live] [--locked] [--training] [--set instanceSet] [--instance instance_name] [cmd]" >&2
+  echo "Note: Any occurrence of '%%INSTANCE%%' or '{}' in the command will be replaced by the current instance name." >&2
 }
 
 cmd=
 cmdfile=
 use_all=0
+use_live=0
 instance_set=
 instances=
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --all) use_all=1 ;;
-    --live) ;;
+    --live) use_live=1 ;;
     --locked) instance_set="LOCKED" ;;
     --training) instance_set="training" ;;
     --set|-s) shift; instance_set="$1" ;;
@@ -39,12 +42,27 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if [ $use_all -eq 1 ]; then
+if [ $use_all -eq 1 -o $use_live -eq 1 ]; then
   if [ "$instances" -o "$instance_set" ]; then
-    echo "$prog: Cannot use --all if instances have been specified" >&2
+    echo "$prog: Cannot use --all or --live if instances have been specified">&2
     exit 1
   else
     instances=`$readConfig --list-all-instances | sed "s;^instance:;;"`
+    if [ $use_live -eq 1 ]; then
+      # Iterate over all instances and probe for "live" instances by checking
+      # for the existence of a CiviCRM DB.  We cannot simply execute
+      # "show databases" on the server, since each instance can have its own
+      # database config.  Thus, we iterate over each instance and attempt to
+      # establish a quick connection with its DB to determine if it is "live".
+      echo "Calculating live CRM instances..." >&2
+      live_instances=
+      for instance in $instances; do
+        if $execSql -i $instance 2>/dev/null; then
+          live_instances="$live_instances $instance"
+        fi
+      done
+      instances="$live_instances"
+    fi
   fi
 elif [ "$instance_set" ]; then
   ival=`$readConfig --instance-set "$instance_set"`
@@ -64,9 +82,13 @@ elif [ ! "$instances" ]; then
 fi
 
 for instance in $instances; do
-  realcmd=`echo "$cmd" | sed -e "s;%%INSTANCE%%;$instance;g"`
-  echo "about to exec: $realcmd"
-  $realcmd
+  if $readConfig --instance $instance --quiet; then
+    realcmd=`echo "$cmd" | sed -e "s;%%INSTANCE%%;$instance;g" -e "s;{};$instance;g"`
+    echo "About to exec: $realcmd" >&2
+    $realcmd
+  else
+    echo "$prog: $instance: Instance not found in config file; skipping" >&2
+  fi
 done
 
 exit 0
