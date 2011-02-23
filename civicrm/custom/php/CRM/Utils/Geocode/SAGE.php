66b0315e709de2d93925ebcba3f826ee24e9e518
Ken Zalewski <zalewski.senate@gmail.com>
2011-02-22 19:34:55 -0500

    Added missing "true" return value for successful geocode.

--- civicrm/custom/php/CRM/Utils/Geocode/SAGE.php
+++ civicrm/custom/php/CRM/Utils/Geocode/SAGE.php
@@ -38,68 +38,69 @@ class CRM_Utils_Geocode_SAGE {
      * @return boolean true if we modified the address, false otherwise
      * @static
      */
-    static function format( &$values, $stateName = false )
+    static function format(&$values, $stateName = false)
     {
-        CRM_Utils_System::checkPHPVersion( 5, true );
+        CRM_Utils_System::checkPHPVersion(5, true);
 
         // we need a valid country, else we ignore
-        if ( ! CRM_Utils_Array::value( 'country', $values  ) ) {
+        if (! CRM_Utils_Array::value('country', $values)) {
             return false;
         }
 
-        $config = CRM_Core_Config::singleton( );
+        $config = CRM_Core_Config::singleton();
 
-        $arg = array( );
-        $arg[] = "key=" . urlencode( $config->geoAPIKey );
+        $arg = array();
+        $arg[] = "key=" . urlencode($config->geoAPIKey);
 
-        if (  CRM_Utils_Array::value( 'street_address', $values ) ) {
-            $arg[] = "street=" . urlencode( $values['street_address'] );
+        if (CRM_Utils_Array::value('street_address', $values)) {
+            $arg[] = "street=" . urlencode($values['street_address']);
         }
 
-        $city = CRM_Utils_Array::value( 'city', $values );
-        if ( $city ) {
-            $arg[] = "city=" . urlencode( $city );
+        $city = CRM_Utils_Array::value('city', $values);
+        if ($city) {
+            $arg[] = "city=" . urlencode($city);
         }
 
-        if (  CRM_Utils_Array::value( 'state_province', $values ) ) { 
-            if ( CRM_Utils_Array::value( 'state_province_id', $values ) ) {
-                $stateProvince = CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_StateProvince', $values['state_province_id'] );
+        if (CRM_Utils_Array::value('state_province', $values)) { 
+            if (CRM_Utils_Array::value('state_province_id', $values)) {
+                $stateProvince = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_StateProvince', $values['state_province_id']);
             }
             else {
-                if ( ! $stateName ) {
-                    $stateProvince = CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_StateProvince', $values['state_province'], 'name', 'abbreviation' );
+                if (!$stateName) {
+                    $stateProvince = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_StateProvince', $values['state_province'], 'name', 'abbreviation');
                 }
                 else {
                     $stateProvince = $values['state_province'];
                 }
             }
             // dont add state twice if replicated in city (happens in NZ and other countries, CRM-2632)
-            if ( $stateProvince != $city ) {
-                $arg[] = "state=" . urlencode( $stateProvince );
+            if ($stateProvince != $city) {
+                $arg[] = "state=" . urlencode($stateProvince);
             }
         }
 
-        if (  CRM_Utils_Array::value( 'country', $values ) ) { 
-            $arg[] = "country=" . urlencode( $values['country'] );
+        if (CRM_Utils_Array::value( 'country', $values)) { 
+            $arg[] = "country=" . urlencode($values['country']);
         }
 
-        if (  CRM_Utils_Array::value( 'postal_code', $values ) ) { 
-            $arg[] = "zip5=" . urlencode( $values['postal_code'] );
+        if (CRM_Utils_Array::value('postal_code', $values)) { 
+            $arg[] = "zip5=" . urlencode($values['postal_code']);
         }
 
-        $args = implode( '&', $arg );
+        $args = implode('&', $arg);
 
         $query = 'http://' . self::$_server . self::$_uri . '?' . $args;
 
         require_once 'HTTP/Request.php';
-        $request = new HTTP_Request( $query );
-        $request->sendRequest( );
-        $string = $request->getResponseBody( );
+        $request = new HTTP_Request($query);
+        $request->sendRequest();
+        $string = $request->getResponseBody();
 
         $ret = explode(",", $string);
         if ($ret[0] != "ERROR") {
             $values['geo_code_1'] = $ret[0];
             $values['geo_code_2'] = $ret[1];
+            return true;
         }
         else {
             // reset the geo code values if we did not get any good values
