b38dc207d419463a6df9182a9e3568c41a131819
Ken Zalewski <zalewski.senate@gmail.com>
2011-03-09 10:39:53 -0500

    Modified CIVICRM_CONFDIR to use __FILE__ as basis for path.

--- modules/civicrm/settings_location.php
+++ modules/civicrm/settings_location.php
@@ -4,5 +4,5 @@ if (!isset($_SERVER['HTTP_HOST']) && getenv('INSTANCE_NAME') === false) {
   die("Either HTTP_HOST global or INSTANCE_NAME environment variable must be set.\n");
 }
 
-define('CIVICRM_CONFDIR', '../../../drupal/sites/');
+define('CIVICRM_CONFDIR', dirname(__FILE__).'/../../drupal/sites/');
 
