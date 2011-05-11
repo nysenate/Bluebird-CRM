591973e8055dbbd9cbbfdb85d686d8c9e578412c
Ken Zalewski <zalewski.senate@gmail.com>
2011-05-11 17:29:17 -0400

    Added get_bluebird_instance_config(), which returns all config params for a given instance, plus globals.  Changed get_bluebird_config() to return entire config set.

--- civicrm/scripts/bluebird_config.php
+++ civicrm/scripts/bluebird_config.php
@@ -5,12 +5,15 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-09-10
-# Revised: 2011-05-02
+# Revised: 2011-05-11
 #
 
 define('BASE_DIR', realpath(dirname(__FILE__).'/../../'));
 
 
+/*
+** Locate the Bluebird configuration file.
+*/
 function get_config_filepath($filename)
 {
   $cfg_file = null;
@@ -26,79 +29,92 @@ function get_config_filepath($filename)
 } // get_config_filepath()
 
 
+/*
+** Retrieve an array of all configuration parameters within the Bluebird
+** configuration file.  The array is indexed by the config groups.  Each
+** value is itself an array of name-value pairs.
+*/
 function get_bluebird_config($filename = 'bluebird.cfg')
 {
   $cfg_file = get_config_filepath($filename);
 
   if (!file_exists($cfg_file)) {
-    die("$cfg_file: Bluebird configuration file not found.\n");
+    error_log("$cfg_file: Bluebird configuration file not found.");
+    return null;
   }
 
-  if (isset($_SERVER['HTTP_HOST'])) {
-    $servername = $_SERVER['HTTP_HOST'];
-    $firstdot = strpos($servername, '.');
-    if ($firstdot === false) {
-      $shortname = $servername;
-    }
-    else {
-      $shortname = substr($servername, 0, $firstdot);
-    }
-    $default_base_domain = substr($servername, $firstdot + 1);
+  $bbini = parse_ini_file($cfg_file, true);
+  if ($bbini === false) {
+    error_log("$cfg_file: Unable to parse the Bluebird configuration file.");
+    return null;
   }
-  else if (($shortname = getenv('INSTANCE_NAME')) !== false) {
-    $servername = "";
+
+  return $bbini;
+} // get_bluebird_config()
+
+
+/*
+** Retrieve an array of configuration parameters for a given CRM instance.
+** If no instance is specified, attempt to determine the instance from the
+** HTTP_HOST server variable, or the INSTANCE_NAME environment variable.
+**
+** The instance can be a short name (hostname only), or a server name (fully
+** qualified domain name).
+*/
+function get_bluebird_instance_config($filename, $instance = null)
+{
+  $shortname = null;
+
+  if ($instance == null) {
+    if (isset($_SERVER['HTTP_HOST'])) {
+      $instance = $_SERVER['HTTP_HOST'];
+    }
+    else if (($instance = getenv('INSTANCE_NAME')) === false) {
+      error_log("Unable to determine CRM instance name.");
+      return null;
+    }
+  }
+
+  $firstdot = strpos($instance, '.');
+  if ($firstdot === false) {
+    $shortname = $instance;
     $default_base_domain = "crm.nysenate.gov";
   }
   else {
-    die("Unable to determine CRM instance name.\n");
+    $shortname = substr($instance, 0, $firstdot);
+    $default_base_domain = substr($instance, $firstdot + 1);
   }
-  
-  $bbini = parse_ini_file($cfg_file, true);
-  if ($bbini === false) {
-    die("Unable to parse the Bluebird configuration file.\n");
+
+  $bbini = get_bluebird_config($filename);
+  if ($bbini) {
+    $bbcfg = array();
+    // Grab the globals first.
+    if (isset($bbini['globals'])) {
+      $bbcfg = array_merge($bbcfg, $bbini['globals']);
+    }
+    // Now merge the instance-specific parameters, which override the globals.
+    $instance_key = 'instance:'.$shortname;
+    if (isset($bbini[$instance_key])) {
+      $bbcfg = array_merge($bbcfg, $bbini[$instance_key]);
+    }
   }
-  
-  $dbhost = get_key_value($bbini, $shortname, 'db.host');
-  $dbuser = get_key_value($bbini, $shortname, 'db.user');
-  $dbpass = get_key_value($bbini, $shortname, 'db.pass');
-  $dbciviprefix = get_key_value($bbini, $shortname, 'db.civicrm.prefix');
-  $dbdrupprefix = get_key_value($bbini, $shortname, 'db.drupal.prefix');
-  $db_basename = get_key_value($bbini, $shortname, 'db.basename') or
-    $db_basename = $shortname;
-  $base_domain = get_key_value($bbini, $shortname, 'base.domain') or
-    $base_domain = $default_base_domain;
-  $datadir = get_key_value($bbini, $shortname, 'data.rootdir');
-  $data_basename = get_key_value($bbini, $shortname, 'data.basename') or
-    $data_basename = $shortname;
-  $data_dirname = "$data_basename.$base_domain";
-  $imapaccts = get_key_value($bbini, $shortname, 'imap.accounts');
-  $use_apc = get_key_value($bbini, $shortname, 'cache.apc');
-  $use_memcache = get_key_value($bbini, $shortname, 'cache.memcache');
 
-  // Always set servername, even if HTTP_HOST was set above.  This allows
-  // us to override the domain in the config file.
-  $servername = "$shortname.$base_domain";
+  $db_url = 'mysql://'.$bbcfg['db.user'].':'.$bbcfg['db.pass'].'@'.$bbcfg['db.host'].'/';
+  $db_basename = isset($bbcfg['db.basename']) ? $bbcfg['db.basename'] : $shortname;
+  $base_domain = isset($bbcfg['base.domain']) ? $bbcfg['base.domain'] : $default_base_domain;
+  $civicrm_db_url = $db_url.$bbcfg['db.civicrm.prefix'].$db_basename;
+  $drupal_db_url = $db_url.$bbcfg['db.drupal.prefix'].$db_basename;
+  $data_basename = isset($bbcfg['data.basename']) ? $bbcfg['data.basename'] : $shortname;
 
-  $civicrm_db_url = "mysql://$dbuser:$dbpass@$dbhost/$dbciviprefix$db_basename";
-  $drupal_db_url = "mysql://$dbuser:$dbpass@$dbhost/$dbdrupprefix$db_basename";
-  
-  $bbcfg = array();
-  $bbcfg['base_dir'] = BASE_DIR;
-  $bbcfg['drupal_root'] = BASE_DIR.'/drupal/';
-  $bbcfg['servername'] = $servername;
-  $bbcfg['shortname'] = $shortname;
-  $bbcfg['base_domain'] = $base_domain;
-  $bbcfg['db_basename'] = $db_basename;
-  $bbcfg['drupal_db_url'] = $drupal_db_url;
+  // Add some extra convenience parameters.
   $bbcfg['civicrm_db_url'] = $civicrm_db_url;
-  $bbcfg['data_rootdir'] = $datadir;
-  $bbcfg['data_dirname'] = $data_dirname;
-  $bbcfg['imap_accounts'] = $imapaccts;
-  $bbcfg['use_apc'] = $use_apc;
-  $bbcfg['use_memcache'] = $use_memcache;
-
+  $bbcfg['drupal_db_url'] = $drupal_db_url;
+  $bbcfg['base_dir'] = BASE_DIR;
+  $bbcfg['data_dirname'] = "$data_basename.$base_domain";
+  $bbcfg['servername'] = "$shortname.$base_domain";
+  $bbcfg['shortname'] = $shortname;
   return $bbcfg;
-} // get_bluebird_config()
+} // get_bluebird_instance_config()
 
 
 /*
