5b8cf0a9949818c5d6a8628bf3628d65672353c2
Ken Zalewski <zalewski.senate@gmail.com>
2011-06-01 15:19:34 -0400

    Added space compression for civicrm_contact fields, and consolidated script logic.

--- scripts/fixSpaces.sh
+++ scripts/fixSpaces.sh
@@ -59,6 +59,48 @@ do_field_trim() {
 }
 
 
+do_field_space_compress()
+{
+  tab_name="$1"
+  shift
+  fld_names=$@
+
+  echo "Checking for extra space within $tab_name" >&2
+  where=
+  got_first=0
+  for fldname in $fld_names; do
+    [ $got_first -eq 1 ] && where="$where or "
+    where="$where $fldname like '%  %'"
+    got_first=1
+  done
+
+  sql="select count(*) from $tab_name where $where"
+  cnt=`$execSql -q -i $instance -c "$sql;"`
+  echo "Records to be compressed: $cnt"
+
+  if [ $cnt -gt 0 ]; then
+    if [ $force_ok -eq 0 ]; then
+      confirm_yes_no "Proceed with space compression operation" || return 1
+    fi
+
+    while [ $cnt -gt 0 ]; do
+      sql="update $tab_name set "
+      got_first=0
+      for fldname in $fld_names; do
+        [ $got_first -eq 1 ] && sql="$sql, "
+        sql="$sql $fldname=replace($fldname, '  ', ' ')"
+        got_first=1
+      done
+      sql="$sql where $where"
+      $execSql -i $instance -c "$sql;"
+
+      sql="select count(*) from $tab_name where $where"
+      cnt=`$execSql -q -i $instance -c "$sql;"`
+      echo "Records remaining to be compressed: $cnt"
+    done
+  fi
+}
+
 
 if [ $# -lt 1 ]; then
   usage
@@ -85,27 +127,8 @@ do_field_trim civicrm_contact display_name sort_name addressee_display postal_gr
 
 do_field_trim civicrm_address street_address street_number street_name street_unit supplemental_address_1 supplemental_address_2 city
 
-echo "Checking for extra space within address fields" >&2
-where="street_address like '%  %' or street_number like '%  %' or street_name like '%  %' or street_unit like '%  %' or supplemental_address_1 like '%  %' or supplemental_address_2 like '%  %' or city like '%  %'"
-sql="select count(*) from civicrm_address where $where"
+do_field_space_compress civicrm_contact display_name sort_name addressee_display postal_greeting_display email_greeting_display organization_name
 
-cnt=`$execSql -q -i $instance -c "$sql;"`
-echo "Records to be compressed: $cnt"
-
-if [ $cnt -gt 0 ]; then
-  if [ $force_ok -eq 0 ]; then
-    confirm_yes_no "Proceed with space compression operation" || exit 0
-  fi
-
-  while [ $cnt -gt 0 ]; do
-    sql="update civicrm_address set street_address=replace(street_address,'  ',' '), street_number=replace(street_number,'  ',' '), street_name=replace(street_name,'  ',' '), street_unit=replace(street_unit,'  ',' '), supplemental_address_1=replace(supplemental_address_1,'  ',' '), supplemental_address_2=replace(supplemental_address_2,'  ',' '), city=replace(city,'  ',' ') where $where"
-    $execSql -i $instance -c "$sql;"
-
-    sql="select count(*) from civicrm_address where $where"
-    cnt=`$execSql -q -i $instance -c "$sql;"`
-    echo "Records remaining to be compressed: $cnt"
-  done
-fi
-
+do_field_space_compress civicrm_address street_address street_number street_name street_unit supplemental_address_1 supplemental_address_2 city
 
 exit 0
