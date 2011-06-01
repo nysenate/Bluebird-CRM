e4d0f7a36bd2102b90206cc72f356e1ea7d3eb00
Ken Zalewski <zalewski.senate@gmail.com>
2011-06-01 13:30:05 -0400

    Added space elimination for various contact fields.

--- scripts/fixSpaces.sh
+++ scripts/fixSpaces.sh
@@ -6,7 +6,7 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2011-04-09
-# Revised: 2011-04-10
+# Revised: 2011-06-01
 #
 
 prog=`basename $0`
@@ -20,6 +20,44 @@ force_ok=0
 usage() {
   echo "Usage: $prog instanceName" >&2
 }
+
+
+do_field_trim() {
+  tab_name="$1"
+  shift
+  fld_names=$@
+
+  echo "Checking for leading and trailing spaces in $tab_name" >&2
+  where=
+  got_first=0
+  for fldname in $fld_names; do
+    [ $got_first -eq 1 ] && where="$where or "
+    where="$where $fldname like ' %' or $fldname like '% '"
+    got_first=1
+  done
+
+  sql="select count(*) from $tab_name where $where"
+  cnt=`$execSql -q -i $instance -c "$sql;"`
+  echo "Records to be trimmed: $cnt"
+
+  if [ $cnt -gt 0 ]; then
+    if [ $force_ok -eq 0 ]; then
+      confirm_yes_no "Proceed with trim operation" || return 1
+    fi
+
+    sql="update $tab_name set "
+    got_first=0
+    for fldname in $fld_names; do
+      [ $got_first -eq 1 ] && sql="$sql, "
+      sql="$sql $fldname=trim($fldname)"
+      got_first=1
+    done
+    sql="$sql where $where"
+
+    $execSql -i $instance -c "$sql;"
+  fi
+}
+
 
 
 if [ $# -lt 1 ]; then
@@ -43,26 +81,9 @@ fi
 
 echo "==> Examining CRM instance [$instance]" >&2
 
-echo "Checking for leading and trailing spaces in address fields" >&2
-where="street_address like ' %' or street_address like '% ' or street_number like ' %' or street_number like '% ' or street_name like ' %' or street_name like '% ' or street_unit like ' %' or street_unit like '% ' or supplemental_address_1 like ' %' or supplemental_address_1 like '% ' or supplemental_address_2 like ' %' or supplemental_address_2 like '% ' or city like ' %' or city like '% '"
-sql="select count(*) from civicrm_address where $where"
+do_field_trim civicrm_contact display_name sort_name addressee_display postal_greeting_display email_greeting_display organization_name
 
-cnt=`$execSql -q -i $instance -c "$sql;"`
-echo "Records to be trimmed: $cnt"
-
-if [ $cnt -gt 0 ]; then
-  if [ $force_ok -eq 0 ]; then
-    echo -n "Proceed with trim operation (N/y)? "
-    read ch
-    case "$ch" in
-      [yY]*) ;;
-      *) echo "Aborting."; exit 0 ;;
-    esac
-  fi
-
-  sql="update civicrm_address set street_address=trim(street_address), street_number=trim(street_number), street_name=trim(street_name), street_unit=trim(street_unit), supplemental_address_1=trim(supplemental_address_1), supplemental_address_2=trim(supplemental_address_2), city=trim(city) where $where"
-  $execSql -i $instance -c "$sql;"
-fi
+do_field_trim civicrm_address street_address street_number street_name street_unit supplemental_address_1 supplemental_address_2 city
 
 echo "Checking for extra space within address fields" >&2
 where="street_address like '%  %' or street_number like '%  %' or street_name like '%  %' or street_unit like '%  %' or supplemental_address_1 like '%  %' or supplemental_address_2 like '%  %' or city like '%  %'"
@@ -73,12 +94,7 @@ echo "Records to be compressed: $cnt"
 
 if [ $cnt -gt 0 ]; then
   if [ $force_ok -eq 0 ]; then
-    echo -n "Proceed with space compression operation (N/y)? "
-    read ch
-    case "$ch" in
-      [yY]*) ;;
-      *) echo "Aborting."; exit 0 ;;
-    esac
+    confirm_yes_no "Proceed with space compression operation" || exit 0
   fi
 
   while [ $cnt -gt 0 ]; do
@@ -90,5 +106,6 @@ if [ $cnt -gt 0 ]; then
     echo "Records remaining to be compressed: $cnt"
   done
 fi
+
 
 exit 0
