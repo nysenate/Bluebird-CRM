765628445d52347705b3839c62ca0469ad561513
Brian Shaughnessy <brian@lcdservices.biz>
2011-06-14 22:53:13 -0400

    3282 further cleanup of link dialog

--- modules/civicrm/packages/ckeditor/config.js
+++ modules/civicrm/packages/ckeditor/config.js
@@ -49,14 +49,22 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
         infoTab.remove( 'browse');
         infoTab.remove( 'protocol');
 		
+		var linkTypeItems = infoTab.get( 'linkType' ).items;
+		if ( linkTypeItems.length > 0 ) {
+			var items_no_anchor = linkTypeItems.slice(0, 1).concat( linkTypeItems.slice(2, linkTypeItems.length) );
+			infoTab.get( 'linkType' ).items = items_no_anchor;
+		}
+		
 		// Get a reference to the Target tab. Remove items from target type list.
-		var targetTab = dialogDefinition.getContents( 'target' );
+		/*var targetTab = dialogDefinition.getContents( 'target' );
 		var targetTypeItems = targetTab.get( 'linkTargetType' ).items;
 		if ( targetTypeItems.length > 0 ) {
 			var items_popup = targetTypeItems.slice(0, 1).concat( targetTypeItems.slice(3, targetTypeItems.length) );
 			targetTab.get( 'linkTargetType' ).items = items_popup;
-		}
+		}*/
 		
+		// Remove the target tab altogether
+		dialogDefinition.removeContents( 'target' );
 		
         
     } else if ( dialogName == 'image' ) {
