ce49a9d393fa517d32765055d3ad363ee85f1dfe
Graylin Kim <kim@nysenate.gov>
2011-06-20 11:52:24 -0400

    Add autocomplete text fields and select boxes for contact filtering

--- civicrm/custom/php/CRM/IMAP/Page/Mapper.php
+++ civicrm/custom/php/CRM/IMAP/Page/Mapper.php
@@ -1,7 +1,106 @@
 <?php
 
 require_once 'CRM/Core/Page.php';
+require_once 'CRM/Core/Error.php';
+require_once 'CRM/IMAP/Form/Filter.php';
 
 class CRM_IMAP_Page_Mapper extends CRM_Core_Page {
 
+    function run() {
+        $start = microtime(true);
+        $server = "{webmail.senate.state.ny.us/imap/notls}";
+        $conn = imap_open($server ,'crmdev','p9x64');
+        $ids = imap_search($conn,"ALL",SE_UID);
+        $headers = imap_fetch_overview($conn, implode(',',$ids),FT_UID);
+        foreach($headers as $header) {
+            $date_parts = explode(" ",$header->date);
+            $messages[$header->uid] = array(
+                    'date' => implode(' ',array($date_parts[0],$date_parts[2],$date_parts[1])),
+                    'subject' => $header->subject,
+                    'uid' => $header->uid,
+                    'from' => $header->from,
+                );
+        }
+        $end = microtime(true);
+        CRM_Core_Error::debug('IMAP Layer', $end-$start);
+
+        $start = microtime(true);
+        $db = new mysqli('localhost','root','windows','senate_c_sd99');
+        $result = $db->query(<<<ENDQUERY
+SELECT
+  state.id AS state_id,
+  state.name AS state_name,
+  contact.first_name AS first_name,
+  contact.last_name AS last_name,
+  contact.id AS id,
+  address.city AS city
+FROM civicrm_contact AS contact
+  JOIN civicrm_address AS address
+    ON address.contact_id = contact.id
+  LEFT JOIN civicrm_state_province AS state
+    ON address.state_province_id=state.id
+WHERE contact.is_deleted=0
+ORDER BY state.name
+ENDQUERY
+);
+        while($row = $result->fetch_assoc()) {
+            $contacts[] = $row;
+            $first_names[] = $row['first_name'];
+            $last_names[] = $row['last_name'];
+            $city_names[] = $row['city'];
+            $state_options[$row['state_id']] = $row['state_name'];
+        }
+        $first_names_clean = array_values(array_unique(array_filter($first_names)));
+        $last_names_clean = array_values(array_unique(array_filter($last_names)));
+        $city_names_clean = array_unique(array_filter($city_names));
+        sort($first_names_clean,SORT_STRING);
+        sort($last_names_clean,SORT_STRING);
+        sort($city_names_clean,SORT_STRING);
+        $end = microtime(true);
+        CRM_Core_Error::debug('Mysqli Layer', $end-$start);
+
+        $this->assign('messages',$messages);
+        $this->assign('contacts',$contacts);
+        $this->assign('first_names',json_encode($first_names_clean));
+        $this->assign('last_names',json_encode($last_names_clean));
+        $this->assign('city_names',json_encode($city_names_clean));
+
+        $form = new CRM_Core_Form();
+        $form->addElement('text','first_name','First Name');
+        $form->addElement('text','last_name','Last Name');
+        $form->addElement('text','city','City');
+        $form->addElement('select','state','State',$state_options);
+        $form->setDefaults(array(
+        		'state'=>array('1031') //New York
+            ));
+        $this->assign( 'form', $form->toSmarty());
+
+        parent::run();
+
+        /* Using DB_DataObject
+        require_once 'DB/DataObject.php';
+        $start = microtime(true);
+        $db = new DB_DataObject();
+        $db->query("SELECT *
+        			FROM civicrm_contact
+        			WHERE is_deleted=0");
+        while($db->fetch()) {
+            //$contacts[] = $db->toArray();
+        }
+        $end = microtime(true);
+        CRM_Core_Error::debug('DB_DataObject Layer', $end-$start);
+		*/
+        /* Using DOA_Contact
+        require_once 'CRM/Contact/DAO/Contact.php';
+        $start = microtime(true);
+        $contact = new CRM_Contact_DAO_Contact();
+        $contact->is_deleted=0;
+        $contact->find();
+        while($contact->fetch())
+            //$contacts[] = $contact->toArray();
+            $contacts[] = clone $contact;
+        $end = microtime(true);
+        CRM_Core_Error::debug('DOA Layer',$end-$start);
+        */
+   }
 }
--- civicrm/custom/templates/CRM/IMAP/Page/Mapper.tpl
+++ civicrm/custom/templates/CRM/IMAP/Page/Mapper.tpl
@@ -1,3 +1,55 @@
-<div>
-	Its an IMAP Mapper - IMapper!
+<div class="crm-content-block">
+	<script>
+		{literal}
+		cj(document).ready(function(){
+			{/literal}
+    		first_names = {$first_names};
+    		last_names = {$last_names};
+    		cities = {$city_names};
+    		{literal}
+
+    		imapper_options = {
+				'matchContains': false,
+				'matchCase': false,
+				'matchSubset': true, //Override the
+				'minChars': 2,
+				'scroll': true,
+    		};
+
+    		cj('#city').autocomplete(cities,imapper_options);
+			cj('#first_name').autocomplete(first_names,imapper_options);
+			cj('#last_name').autocomplete(last_names,imapper_options);
+
+		});
+		{/literal}
+	</script>
+	<div class="imapper-filter">
+		<form>
+		{*
+		{$form.street_address.label}{$form.street_address.html}<br/>
+		*}
+		<div>{$form.city.label}{$form.city.html}</div>
+		<div>{$form.first_name.label}{$form.first_name.html}</div>
+		<div>{$form.last_name.label}{$form.last_name.html}</div>
+		{$form.state.label}{$form.state.html}<br/>
+		</form>
+	</div>
+	<div class="imapper-messages">
+        {foreach from=$messages item=message}
+        	<div class="imapper-message" id="imapper_message_{$message.uid}">
+        		<div class="imapper-date">{$message.date}</div>
+        		<div class="imapper-subject">{$message.subject}</div>
+        		<div class="imapper-clear"></div>
+        	</div>
+        {/foreach}
+    </div>
+	{*
+    <div class="imapper-contacts">
+    	{foreach from=$contacts item=contact}
+    		<div class="imapper-contact" id="imapper_contact_{$contact.id}">
+    			<div class="imapper-name">Name: {$contact.display_name}</div>
+    		</div>
+		{/foreach}
+    </div>
+	*}
 </div>
