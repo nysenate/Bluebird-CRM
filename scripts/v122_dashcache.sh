2f83aac0eb4453905be9046b155bbd9c59151622
Brian Shaughnessy <brian@lcdservices.biz>
2011-07-02 18:21:42 -0400

    3082 dashboard caching restructuring

---
+++ scripts/v122_dashcache.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# v122_dashcache.sh
+#
+# Project: BluebirdCRM
+# Author: Brian Shaughnessy
+# Organization: New York State Senate
+# Date: 2011-07-02
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+readConfig=$script_dir/readConfig.sh
+drush=$script_dir/drush.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT_DATA_ROOTDIR"
+webdir=`$readConfig --global drupal.rootdir` || webdir="$DEFAULT_DRUPAL_ROOTDIR"
+base_domain=`$readConfig --ig $instance base.domain` || base_domain="$DEFAULT_BASE_DOMAIN"
+
+###### Begin Update Scripts ######
+
+### CiviCRM ###
+
+## alter dashboard tables
+dashcache="ALTER TABLE civicrm_dashboard DROP content, DROP created_date;
+ALTER TABLE civicrm_dashboard_contact  ADD content TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER weight,  ADD created_date DATETIME NULL DEFAULT NULL AFTER content;"
+$execSql -i $instance -c "$dashcache"
+
+
+
+### Cleanup ###
+
+$script_dir/fixPermissions.sh
+$script_dir/clearCache.sh $instance
