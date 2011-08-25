12376151eae5536a1f23e6e7ed4b547e3b3726d8
Graylin Kim <graylin.kim@gmail.com>
2011-08-25 11:31:45 -0400

    Adds a simple REST API for tag operations.
    Currently implements create, update, and delete at the following paths:
    
        civicrm/ajax/tag/create
        civicrm/ajax/tag/update
        civicrm/ajax/tag/delete
    
    These operations may receive, as $_GET parameters, any number of parameters
    corresponding to field entries in the civicrm_tag table and will apply
    them to the operation intelligently. Update and Delete require the id=tag_id
    argument to be used. Create (obviously) forbids the tag argument.
    
    On success, a response with code == 1 will be returned with the $tag->id of
    the tag that was operated on (the new id for the create operation) in the
    message field.
    
    On failure, the response code is != 1 and the message describes the database
    level problem as provided by the DAO layer.
    
    Currently, errors are not handled very well and requirements are not checked
    before performing operations. These aspects must be cleaned up later...

--- civicrm/custom/php/CRM/Tag/AJAX.php
+++ civicrm/custom/php/CRM/Tag/AJAX.php
@@ -3,11 +3,19 @@
 require_once 'CRM/Core/Page.php';
 require_once 'CRM/Core/DAO.php';
 require_once 'CRM/Utils/System.php';
+require_once 'CRM/Utils/Array.php';
+require_once 'CRM/Core/BAO/Tag.php';
 
 
+// TODO: The DAO layer is injection safe...right?
+// TODO: Needs to return good responses when things go wrong!
+// TODO: How do you tell if a save was successful?
 class CRM_Tag_AJAX extends CRM_Core_Page {
 
-    // The full set of fields that cna be attached to each node.
+    const SUCCESS = 1;
+    const NOT_FOUND = 0;
+    const NOT_DELETED = 2;
+    // The full set of fields that can be attached to each node.
     // If you don't want a field, just comment it out.
     //
     // id, parent_id, and is_tagset are required.
@@ -45,23 +53,21 @@ class CRM_Tag_AJAX extends CRM_Core_Page {
     }
 
     static function tag_tree() {
-        $used_for = $_GET['used_for'];
-
         // We need to build a list of tags ordered by hierarchy and sorted by
         // name. Instead of recursively making mysql queries, we'll make one
         // big query and build the heirarchy with the recursive algorithm below.
-        $query = "SELECT *
-                  FROM civicrm_tag
-                  WHERE used_for LIKE '%{$used_for}%'
-                  ORDER BY name";
-        $dao = CRM_Core_DAO::executeQuery( $query, CRM_Core_DAO::$_nullArray, true, null, false, false );
+        $tags = new CRM_Core_BAO_Tag();
+        $tags->used_for = $_GET['used_for'];
+        $tags->orderBy('name');
+        $tags->find();
 
-        // Grab all the tags one by one and sort them into either the "root" or "nodes" bucket.
-        while ($dao->fetch()) {
-            if (!$dao->parent_id && $dao->is_tagset==0)
-                $roots[] = self::_build_node($dao);
+        // Sort all the tags into root and nodes buckets. This simpifies the process
+        // to building the root nodes by moving tags from the nodes bucket.
+        while ($tags->fetch()) {
+            if (!$tags->parent_id && $tags->is_tagset==0)
+                $roots[] = self::_build_node($tags);
             else
-                $nodes[] = self::_build_node($dao);
+                $nodes[] = self::_build_node($tags);
         }
 
         // Recursively build the tree from each "root" using the "nodes"
@@ -71,8 +77,63 @@ class CRM_Tag_AJAX extends CRM_Core_Page {
             $tree[] = self::_build_tree($root,$nodes);
         }
 
-        echo json_encode($tree);
-        CRM_Utils_System::civiExit( );
+        echo json_encode(array("code"=>self::SUCCESS,"message"=>$tree));
+        CRM_Utils_System::civiExit();
+    }
+
+    static function create() {
+        $tag = new CRM_Core_BAO_Tag();
+        foreach(self::$TAG_FIELDS as $field) {
+            $value = CRM_Utils_Array::value($field, $_GET);
+            if($value) {
+                $tag->$field = $value;
+            }
+        }
+        $tag->insert();
+        echo json_encode(array(
+            "code" => self::SUCCESS,
+            "message" => $tag->id));
+        CRM_Utils_System::civiExit();
+    }
+
+    static function update() {
+        $tag = new CRM_Core_BAO_Tag();
+        $tag->id = CRM_Utils_Array::value('id', $_GET);
+        if($tag->find() && $tag->fetch()) {
+            // Use the current value if a new value wasn't passed in
+            foreach(self::$TAG_FIELDS as $field) {
+                $tag->$field = CRM_Utils_Array::value($field, $_GET, $tag->$field);
+            }
+            $tag->update();
+
+            $result = array(
+                "code" => self::SUCCESS,
+                "message" => $tag->id);
+
+        } else {
+            $result = array(
+                "code" => self::NOT_FOUND,
+                "message" => "Tag with id {$tag->id} was not found. Update failure");
+        }
+
+        echo json_encode($result);
+        CRM_Utils_System::civiExit();
+    }
+
+    static function delete() {
+        $tag = new CRM_Core_BAO_Tag();
+        $tag->id = CRM_Utils_Array::value('id', $_GET);
+        if( $tag->delete() )
+            $result = array(
+                "code" => self::SUCCESS,
+                "message" => $tag->id);
+        else
+            $result = array(
+                "code" => self::NOT_DELETED,
+                "message" => "Tag couldn't be deleted");
+
+        echo json_encode($result);
+        CRM_Utils_System::civiExit();
     }
 }
 
