42b2e6bd04d80cd08f8fefab8c2f23db500b0455
Brian Shaughnessy <brian@lcdservices.biz>
2011-08-26 14:01:34 -0400

    3983 activity report dashlet context redirection

--- civicrm/custom/php/CRM/Report/Form/Activity.php
+++ civicrm/custom/php/CRM/Report/Form/Activity.php
@@ -534,11 +534,19 @@ class CRM_Report_Form_Activity extends CRM_Report_Form {
                                                           '&aid=' . $rows[$rowNum]['civicrm_activity_id'] . '&caseID=' . $rows[$rowNum]['civicrm_case_activity_case_id'] . '&context=' . $context, //NYSS 3983
                                                           $this->_absoluteUrl );
                         } else {
-                            $url = CRM_Utils_System::url( "civicrm/activity/view", //NYSS 3986
-                                                          'action=view&reset=1&cid=' . $rows[$rowNum]['civicrm_activity_source_contact_id'] .
-                                                          '&id=' . $rows[$rowNum]['civicrm_activity_id'] . '&atype=' . $value . '&context=' . $context, //NYSS 3983
-                                                          $this->_absoluteUrl );
-                        }
+                            if ( $value ==  19 ) { //NYSS 3986
+								$url = CRM_Utils_System::url( "civicrm/activity/view", 
+                            	                              'action=view&reset=1&cid=' . $rows[$rowNum]['civicrm_activity_source_contact_id'] .
+                            	                              '&id=' . $rows[$rowNum]['civicrm_activity_id'] . '&atype=' . $value . '&context=' . $context, //NYSS 3983
+                            	                              $this->_absoluteUrl );
+                        	} else {
+								$url = CRM_Utils_System::url( "civicrm/contact/view/activity"  , 
+                                                              'action=view&reset=1&cid=' . $rows[$rowNum]['civicrm_activity_source_contact_id'] .
+                            	                              '&id=' . $rows[$rowNum]['civicrm_activity_id'] . '&atype=' . $value . '&context=' . $context, //NYSS 3983
+                            	                              $this->_absoluteUrl );
+								
+							}
+						}
                         $rows[$rowNum]['civicrm_activity_activity_type_id_link'] = $url;
                         $rows[$rowNum]['civicrm_activity_activity_type_id_hover'] = $onHoverAct;
                     }
