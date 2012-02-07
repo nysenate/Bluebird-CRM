e97140d437bb78ca9118b53ac3e7b60c65115311
Ken Zalewski <zalewski.senate@gmail.com>
2012-02-07 14:50:53 -0500

    Handle db.port as optional parameter.

--- civicrm/scripts/signup_reports/utils.php
+++ civicrm/scripts/signup_reports/utils.php
@@ -1,18 +1,29 @@
 <?php
 
-function get_connection($config) {
-    $conn_str = "{$config['signups.db.user']}:{$config['signups.db.pass']}@{$config['signups.db.host']}:{$config['signups.db.port']}";
+function get_connection($config)
+{
+    $dbuser = $config['signups.db.user'];
+    $dbpass = $config['signups.db.pass'];
+    $dbhost = $config['signups.db.host'];
+    $dbport = (isset($config['signups.db.port']) ? ':'.$config['signups.db.port'] : '';
+    $dbname = $config['signups.db.name'];
 
-    if(! $conn = mysql_connect("{$config['signups.db.host']}:{$config['signups.db.port']}",$config['signups.db.user'],$config['signups.db.pass']))
+    $conn_str = "$dbuser@$dbhost$dbport";
+    $conn = mysql_connect("$dbhost$dbport", $dbuser, $dbpass);
+    if (!$conn) {
         die("Could not establish connection to $conn_str\n".mysql_error());
+    }
 
-    if(!mysql_select_db($config['signups.db.name'], $conn))
-        die("Could not use database {$config['signups.db.name']} at $conn_str\n".mysql_error($conn));
+    if (!mysql_select_db($dbname, $conn)) {
+        die("Could not use database $dbname at $conn_str\n".mysql_error($conn));
+    }
 
     return $conn;
-}
+} // get_connection()
 
-function get_report_path($config, $options) {
+
+function get_report_path($config, $options)
+{
     $directory = $config['data.rootdir'].'/'.$config['servername'].'/'.$config['signups.reports.dirname'];
 
     $date = $options['date'];
