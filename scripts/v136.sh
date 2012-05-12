f67fbb7d36d9a6507e8fbec83f9938a5a43f6812
Brian Shaughnessy <brian@lcdservices.biz>
2012-05-12 09:38:50 -0400

    5253 remove add user tab from manage user screen

---
+++ scripts/v136.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# v136.sh
+#
+# Project: BluebirdCRM
+# Authors: Brian Shaughnessy and Ken Zalewski
+# Organization: New York State Senate
+# Date: 2012-05-12
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+readConfig=$script_dir/readConfig.sh
+drush=$script_dir/drush.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT_DATA_ROOTDIR"
+app_rootdir=`$readConfig --ig $instance app.rootdir` || app_rootdir="$DEFAULT_APP_ROOTDIR"
+webdir=`$readConfig --global drupal.rootdir` || webdir="$DEFAULT_DRUPAL_ROOTDIR"
+base_domain=`$readConfig --ig $instance base.domain` || base_domain="$DEFAULT_BASE_DOMAIN"
+db_basename=`$readConfig --ig $instance db.basename` || db_basename="$instance"
+log_db_prefix=`$readConfig --ig $instance db.log.prefix` || log_db_prefix="$DEFAULT_BASE_DOMAIN"
+
+###### Begin Upgrade Scripts ######
+
+### Drupal ###
+
+# 5253 remove create users perm
+roles="UPDATE permission SET perm = REPLACE(perm, 'create users, ', '') WHERE rid IN (4,9);"
+$execSql -i $instance -c "$roles" --drupal
+
+
+### CiviCRM ###
+
+
+
+### Cleanup ###
+
+$script_dir/clearCache.sh $instance
