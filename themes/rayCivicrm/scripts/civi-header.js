75a30c9386692631b73d3a4e5d14aab4855176b5
Brian Shaughnessy <brian@lcdservices.biz>
2012-05-18 15:51:32 -0400

    5330 job ID in IE8

--- themes/rayCivicrm/scripts/civi-header.js
+++ themes/rayCivicrm/scripts/civi-header.js
@@ -1,62 +1,61 @@
 $(document).ready(function() {
-	$('h1.title[innerHTML=CiviCRM Home]').html('Dashboard');
+    $('h1.title[innerHTML=CiviCRM Home]').html('Dashboard');
 
-	$('.civi-advanced-search-button').click(function(){
-		$('.crm-advanced_search_form-accordion').toggleClass('crm-accordion-open');
-		$('.crm-advanced_search_form-accordion').toggleClass('crm-accordion-closed');
+    $('.civi-advanced-search-button').click(function(){
+        $('.crm-advanced_search_form-accordion').toggleClass('crm-accordion-open');
+        $('.crm-advanced_search_form-accordion').toggleClass('crm-accordion-closed');
         if($('.crm-advanced_search_form-accordion').hasClass('crm-accordion-open')) {
-        	    $(this).addClass('civi-adv-search-linkwrap-active');
-            } else {
-        	    $(this).removeClass('civi-adv-search-linkwrap-active');                
-                }
-		return false;
-	});
+          $(this).addClass('civi-adv-search-linkwrap-active');
+        } else {
+          $(this).removeClass('civi-adv-search-linkwrap-active');
+        }
+        return false;
+    });
     
     $('.civi-advanced-search-link').click(function(){
-		$(this).toggleClass('civi-adv-search-linkwrap-active');
-		$('.civi-adv-search-body').toggle();
-		return false;
-	});
+        $(this).toggleClass('civi-adv-search-linkwrap-active');
+        $('.civi-adv-search-body').toggle();
+        return false;
+    });
 
-	$("#civi_text_search").focus(function(){
-		var defaultText = $(this).val();
-		if(defaultText === 'enter any text'){
-			$(this).val('');
-			$(this).addClass('input-active');			
-			}
-		});
-	$("#civi_text_search").blur(function(){
-		var defaultText = $(this).val();
-		if(defaultText === ''){
-			$(this).val('enter any text');
-			$(this).removeClass('input-active');
-			}
-		});
-		
-	 $('body').click(function() {
-	 	$('.primary-link').removeClass('primary-link-active');
-	 });
-	
-	 $('.primary-link').click(function(event){
-	     event.stopPropagation();
-	 });
-	
-			
-	$('.primary-link .main-menu-item').click( function(){
-		$('.civi-admin-block-wrapper').hide();
-		$('.admin-link a').removeClass('active'); 
-		$('.primary-link').removeClass('primary-link-active');
-		$(this).parent().toggleClass('primary-link-active')
-		});
-	
-	$('#civicrm-menu').hide();
-	if ($('#civicrm-menu').length >0){
-		$('#civi-admin-menu').show();
-		}
-	
-	$('.action-item[innerHTML=File On Case]').remove();
-	
-	var extID = $('#external_identifier').val();
+    $("#civi_text_search").focus(function(){
+        var defaultText = $(this).val();
+        if(defaultText === 'enter any text'){
+            $(this).val('');
+            $(this).addClass('input-active');
+            }
+        });
+    $("#civi_text_search").blur(function(){
+        var defaultText = $(this).val();
+        if(defaultText === ''){
+            $(this).val('enter any text');
+            $(this).removeClass('input-active');
+            }
+        });
+
+     $('body').click(function() {
+         $('.primary-link').removeClass('primary-link-active');
+     });
+
+     $('.primary-link').click(function(event){
+         event.stopPropagation();
+     });
+
+    $('.primary-link .main-menu-item').click( function(){
+        $('.civi-admin-block-wrapper').hide();
+        $('.admin-link a').removeClass('active');
+        $('.primary-link').removeClass('primary-link-active');
+        $(this).parent().toggleClass('primary-link-active')
+        });
+
+    $('#civicrm-menu').hide();
+    if ($('#civicrm-menu').length >0){
+        $('#civi-admin-menu').show();
+        }
+
+    $('.action-item[innerHTML=File On Case]').remove();
+
+    var extID = $('#external_identifier').val();
     $('#external_identifier').after(extID).remove();
 
     //3674 prevent duplicate form submission with enter key
@@ -64,11 +63,15 @@ $(document).ready(function() {
     var submitted = false;
     $('form').submit(function(e){
         //console.log(e);
-        var fname   = e.target.name; //alert(fid); //form name
-        var faction = e.target.action; //action value
+        var fname        = e.target.name; //alert(fid); //form name
+        var faction      = e.target.action; //action value
         var factionmatch = faction.search("civicrm/report/"); //-1 if not found
-        var fbaseuri = e.target.baseURI;
-        var fbaseurimatch = fbaseuri.search("civicrm/report/");
+        var fbaseuri     = e.target.baseURI;
+        if ( fbaseuri ) {
+          var fbaseurimatch = fbaseuri.search("civicrm/report/");
+        }
+
+        //console.log('gfN', global_formNavigate);
         if ( submitted &&
              factionmatch == -1 &&
              fbaseurimatch == -1 &&
