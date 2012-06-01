314ff10cbc83123181a624f8bd6b26d14a8ddce9
Brian Shaughnessy <brian@lcdservices.biz>
2012-06-01 16:21:55 -0400

    create rebuildTriggers script

---
+++ civicrm/scripts/rebuildTriggers.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Author:      Brian Shaughnessy
+ * Date:        2012-06-01
+ * Description: rebuild triggers
+ */
+
+$prog = basename(__FILE__);
+
+require_once 'script_utils.php';
+$optList = civicrm_script_init("", array(), False);
+
+drupal_script_init();
+
+require_once 'CRM/Core/Config.php';
+$config = CRM_Core_Config::singleton();
+
+echo "Rebuilding triggers...\n";
+CRM_Core_DAO::triggerRebuild( );
+
