1f719d38ddc601bc8fd2dac55ea12f55e5d72e9c
Brian Shaughnessy <brian@lcdservices.biz>
2012-10-02 00:27:00 -0400

    logging summary report alias correction

--- civicrm/custom/php/CRM/Report/Form/Contact/LoggingSummary.php
+++ civicrm/custom/php/CRM/Report/Form/Contact/LoggingSummary.php
@@ -113,6 +113,7 @@ class CRM_Report_Form_Contact_LoggingSummary extends CRM_Logging_ReportSummary {
             'name' => 'display_name',
             'title' => ts('Altered Contact'),
             'type' => CRM_Utils_Type::T_STRING,
+            'alias' => 'modified_contact_civireport',//NYSS
           ),
           'altered_contact_id' => array(
             'name' => 'id',
