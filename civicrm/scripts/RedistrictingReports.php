1c5c90b8cccc36751c7f5c8289145b8b1381183a
Ash Islam <ashislam858@gmail.com>
2012-12-07 18:02:39 -0500

    Added basic senate district counts reporting

--- civicrm/scripts/RedistrictingReports.php
+++ civicrm/scripts/RedistrictingReports.php
@@ -13,6 +13,7 @@
 
 // This is per the Redistricting Process Flow ( Step 5 ) outlined at:
 // http://dev.nysenate.gov/projects/2012_redistricting/wiki/Redistricting_Process_Flow
+// and Issue 5940: http://dev.nysenate.gov/issues/5940
 //-------------------------------------------------------------------------------------
 
 // ./RedistrictingReports.php -S mcdonald --format = [html|txt|csv], --infolevel [summary|details|reference]
@@ -23,14 +24,13 @@ define('DEFAULT_FORMAT', 'txt');
 define('DEFAULT_INFO_LEVEL', 'summary');
 
 $formats = array( 'html', 'txt', 'csv', 'excel' );
-$info_levels = array( 'summary', 'details', 'reference');
 
 // Parse the options
 require_once 'script_utils.php';
-$shortopts = "fi";
-$longopts = array("format=", "infolevel=");
+$shortopts = "fsdr";
+$longopts = array("format=", "summary", "details", "references");
 $optlist = civicrm_script_init($shortopts, $longopts);
-$usage = 'RedistrictingReports.php -S mcdonald --format= [html|txt|csv], --infolevel= [summary|details|reference]';
+$usage = 'RedistrictingReports.php -S mcdonald --format= [html|txt|csv], --summary, --details, --references';
 
 if ($optlist === null) {
     $stdusage = civicrm_script_usage();
@@ -39,15 +39,11 @@ if ($optlist === null) {
 }
 
 // Set the options
-$format = $optlist['format'] ? $optlist['format'] : DEFAULT_FORMAT;
-$info_level = $optlist['infolevel'] ? $optlist['infolevel'] : DEFAULT_INFO_LEVEL;
-
-// Check if the format and info level specified is proper or terminate.
-if ( !in_array( $format, $formats ) || !in_array( $info_level, $info_levels ) ) {
-	bbscript_log("error", "Check usage for correct --format --infolevel parameters");
-	bbscript_log("info", "Usage: $usage");
-	exit();
-}
+$opt = array();
+$opt['format'] = get($optlist, 'format', DEFAULT_FORMAT);
+$opt['summary'] = get($optlist, 'summary', FALSE);
+$opt['details'] = get($optlist, 'details', FALSE);
+$opt['references'] = get($optlist, 'references', FALSE);
 
 // Initialize CiviCRM
 require_once 'CRM/Core/Config.php';
@@ -58,112 +54,139 @@ $session =& CRM_Core_Session::singleton();
 $dao = new CRM_Core_DAO();
 $db = $dao->getDatabaseConnection()->connection;
 
+// Get the senate district for this instance
+$bb_cfg = get_bluebird_instance_config($optlist['site']);
+$inst_senate_district = $bb_cfg['district'];
+
+// Template for outputting text consistently.
+// ---------------------------------------------------------------------------------
+$tmpl = array(
+
+);
+// ---------------------------------------------------------------------------------
+
+// Outputs to store report text
+$output = array(
+	'summary' => "",
+	'detail' => "",
+	'references' => ""
+);
+
 // `Note subject` prefixes used in Redistricting.php
-$subject_prefixes = array(
+$subjects = array(
     "unchanged" => "RD12 VERIFIED DISTRICTS",
     "changed" => "RD12 UPDATED DISTRICTS",
     "removed" => "RD12 REMOVED DISTRICTS"
 );
 
-// The SQL queries used in this script are contained here:
-// -----------------------------------------------------------
-$query_same_district = "
-	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
-	FROM `civicrm_note`\n
-	WHERE `subject` LIKE '{$subject_prefixes['unchanged']}'
-";
-
-$query_new_districts = "
-	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
-	FROM `civicrm_note`\n
-	WHERE `subject` LIKE '{$subject_prefixes['changed']}%'
-";
-
-$query_removed_districts = "
-	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
-	FROM `civicrm_note`\n
-	WHERE `subject` LIKE '{$subject_prefixes['removed']}'
-";
-// -----------------------------------------------------------
-
-// The arrays to store statistics based on the info level specified
-
 // Summary level counts
-$count = array(
+$summary_count = array(
 	'unchanged' => 0,
 	'changed' => 0,
-	'removed' => 0
+	'removed' => 0,
+	'districts' => array()
 );
 
-// Detail level counts
-$detail_count = array();
+// The SQL queries used in this script are contained here:
+// -----------------------------------------------------------
+$query = array();
 
-// Store Reference Information
-$reference = array();
+// List of address counts per district
+$query['district_counts'] = "
+	SELECT `ny_senate_district_47` AS district, COUNT( * ) AS count
+	FROM `civicrm_value_district_information_7`
+	WHERE `ny_senate_district_47` IS NOT NULL
+	GROUP BY `ny_senate_district_47`
+";
 
-$same_district_result = mysql_query( $query_same_district, $db );
-$count['unchanged'] = mysql_num_rows( $same_district_result );
-bbscript_log("info", "Found {$count['unchanged']} contacts that remain in the same district");
+// Number of addresses where no districts changed
+$query['same_districts'] = "
+	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
+	FROM `civicrm_note`\n
+	WHERE `subject` LIKE '{$subjects['unchanged']}'
+";
 
-$new_district_result = mysql_query( $query_new_districts, $db );
-$count['changed'] = mysql_num_rows( $new_district_result );
-bbscript_log("info", "Found {$count['changed']} contacts that moved to other districts");
+// Number of addresses where atleast one district changed
+$query['new_districts'] = "
+	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
+	FROM `civicrm_note`\n
+	WHERE `subject` LIKE '{$subjects['changed']}%'
+";
 
-$removed_district_result = mysql_query( $query_removed_districts, $db );
-$count['removed'] = mysql_num_rows( $removed_district_result );
-bbscript_log("info", "Found {$count['removed']} contacts that were out of state");
+// Number of non-NY addresses whose district information was zeroed
+$query['removed_districts'] = "
+	SELECT `entity_id`, `note`, `modified_date`, `subject`\n
+	FROM `civicrm_note`\n
+	WHERE `subject` LIKE '{$subjects['removed']}'
+";
+// -----------------------------------------------------------
 
-// Template for outputting the pertinent information consistently.
-// ---------------------------------------------------------------------------------
-$redistrict_tmpl = array(
-	'report_title' => '2012 Redistricting Results',
-	'summary' => array(
-		'title' => 'Summary of redistricting results:',
-		'counts' => array(
-			'unchanged' => 'District information remained the same for %d contacts.',
-			'changed' => 'One or more districts were updated for %d contacts.',
-			'removed' => 'There were %d `non NY state addresses` found in this district.'
-		)
-	),
-	'detail' => array(
-		'counts' => array(
-			'new_districts' => 'Constituents have been transferred into the following new districts:'
-		)
-	)
-);
-// ---------------------------------------------------------------------------------
+if ( $opt['summary'] != FALSE ){
 
-generateTextReport( $redistrict_tmpl, $count );
+	$results = array();
 
-// Generate output functions
+	$results['district_counts'] = mysql_query( $query['district_counts'], $db );
+	while ( ($row = mysql_fetch_assoc($results['district_counts'])) != null ){
+		$summary_count['districts'][$row['district']] = $row['count'];
+	}
 
-function generateHTMLReport( $tmpl ) {
+	$results['same_districts'] = mysql_query( $query['same_districts'], $db );
+	$summary_count['unchanged'] = mysql_num_rows( $results['same_districts'] );
+	bbscript_log("info", "Found {$summary_count['unchanged']} contacts that remain in the same district");
+
+	$results['new_districts'] = mysql_query( $query['new_districts'], $db );
+	$summary_count['changed'] = mysql_num_rows( $results['new_districts'] );
+	bbscript_log("info", "Found {$summary_count['changed']} contacts that moved to other districts");
+
+	$results['removed_districts'] = mysql_query( $query['removed_districts'], $db );
+	$summary_count['removed'] = mysql_num_rows( $results['removed_districts'] );
+	bbscript_log("info", "Found {$summary_count['removed']} contacts that were out of state");
+
+	if ( $opt['format'] == 'txt' ) {
+
+		$str = "Summary of Senate Redistricting\n\n";
+		$str .= "SD\tOut of district records\n";
+		foreach( $summary_count['districts'] as $district => $count ){
+			if ( $district != $inst_senate_district ) {
+				$str .= "$district\t$count\n";
+			}
+		}
+
+		$output['summary'] = $str;
+	}
+
+	// [TODO] save to file...
+	print $output['summary'];
+}
+
+if ( $opt['details'] != FALSE ){
+
+	$a = "
+	SELECT `civicrm_contact`.last_name,
+		   `civicrm_contact`.first_name,
+		   `civicrm_contact`.gender_id,
+		   `civicrm_contact`.birth_date,
+		   `civicrm_address`.city,
+		   `civicrm_address`.state,
+		   `civicrm_address`.zip,
+		   `civicrm_contact`.email,
+
+	FROM `civicrm_contact`
+	JOIN `civicrm_address` ON `civicrm_contact`.id = `civicrm_address`.contact_id
+
+	";
 
 }
 
-function generateTextReport( $tmpl, $count ) {
+if ( $opt['references'] != FALSE ){
 
-	$output = $tmpl['report_title'] . "\n\n";
-	$output .= $tmpl['summary']['title'] . "\n\n";
-	$output .= sprintf($tmpl['summary']['counts']['unchanged'], $count['unchanged']) . "\n";
-	$output .= sprintf($tmpl['summary']['counts']['changed'], $count['changed']) . "\n";
-	$output .= sprintf($tmpl['summary']['counts']['removed'], $count['removed']) . "\n";
-
-	print $output;
-}
-
-function generateCSVReport( $tmpl ) {
 
 }
 
-function generateExcelReport( $tmpl ) {
-
+function get($array, $key, $default) {
+    // blank, null, and 0 values are bad.
+    return isset($array[$key]) && $array[$key]!=NULL && $array[$key]!=="" && $array[$key]!==0 && $array[$key]!=="000" ? $array[$key] : $default;
 }
-
-
-
-
-
 
 
 
