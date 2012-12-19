2bd30c55b5a15000cfc52a7c0810c7bda2407000
Ash Islam <ashislam858@gmail.com>
2012-12-19 14:24:55 -0500

    Use output buffering to render html

--- civicrm/scripts/RedistrictingReportsTmpl.php
+++ civicrm/scripts/RedistrictingReportsTmpl.php
@@ -50,12 +50,13 @@
 		</style>
 	</head>
 	<body>
+
 		<?php if ($mode == "summary"): ?>
 		<h1>Redistricting Summary for Senate District <?= $senator_district ?></h1>
 		<h3><?= $senator_name ?></h3>
 		<hr/>
 		<p>The following table indicates the number of individuals, households, and organizations that will
-		   be moving from this district.
+		   be in the districts shown in the left column.
 		</p>
 
 		<table>
@@ -78,6 +79,79 @@
 				</tr>
 			<?php endforeach; ?>
 		</table>
+
+	<?php elseif ($mode == "detail"): ?>
+		<h1>Redistricting Details for Senate District <?= $senator_district ?></h1>
+		<h3><?= $senator_name ?></h3>
+		<hr/>
+
+		<p>The tables below list the contacts that will be in the districts specified.
+		</p>
+		<?php
+			// Table columns for contact details
+			$html_columns = array(
+				"individual" => array(
+					"Name" => 30, "Sex" => 6, "Age" => 6, "Address" => 25, "City" => 17, "Zip" => 6,
+					"Email" => 20, "Source" => 9, "Cases" => 8, "Actvities" => 10, "BB Rec#" => 9 ),
+
+				"organization" => array(
+					"Organization Name" => 30, "Address" => 37, "City" => 17, "Zip" => 6, "Email" => 20,
+			        "Source" => 9, "Cases" => 8, "Actvities" => 10, "BB Rec#" => 9 ),
+
+				"household" => array(
+					"Household Name" => 30, "Address" => 37, "City" => 17, "Zip" => 6, "Email" => 20,
+			        "Source" => 9, "Cases" => 8, "Actvities" => 10, "BB Rec#" => 9)
+			);
+		?>
+
+		<?php
+
+		// Sort the detailed data by district number
+		ksort($detail_data);
+
+		foreach( $detail_data as $dist => $contact_types )
+			foreach( $contact_types as $type => $contact_array ): ?>
+
+				<h3>District <?= "$dist : " . ucfirst($type) . "s" ?></h3>
+				<table id="dist_<?= $dist . "_" . $type ?>">
+					<tr>
+					<?php foreach($html_columns[$type] as $name => $width): ?>
+						<th><?= $name ?></th>
+					<?php endforeach; ?>
+					</tr>
+
+					<?php foreach($contact_array as $contact): ?>
+				 	<tr>
+				 		<?php if($type == "individual"): ?>
+				 			<td><?= $contact['last_name'].", ".$contact['first_name']?></td>
+				 			<td><?= get_gender($contact['gender_id']) ?></td>
+				 			<td><?= get_age($contact['birth_date']) ?></td>
+				 			<td><?= $contact['street_address'] ?></td>
+
+				 	    <?php elseif($type == "household"): ?>
+				 			<td><?= $contact['household_name'] ?></td>
+				 			<td><?= $contact['street_address'] ?></td>
+
+				 		<?php elseif($type == "organization"): ?>
+				 			<td><?= $contact['organization_name'] ?></td>
+				 			<td><?= $contact['street_address'] ?></td>
+
+						<?php endif; ?>
+
+							<td><?= $contact['city'] ?></td>
+							<td><?= $contact['postal_code'] ?></td>
+					      	<td><?= $contact['email'] ?></td>
+					      	<td><?= $contact['source'] ?></td>
+					      	<td><?= $contact['case_count'] ?></td>
+					      	<td><?= $contact['activity_count'] ?></td>
+					      	<td><?= $contact['contact_id'] ?></td>
+					</tr>
+					<?php endforeach; ?>
+
+				</table>
+				<br/>
+				<br/>
+			<?php endforeach; ?>
 	<?php endif; ?>
 	</body>
 </html>
