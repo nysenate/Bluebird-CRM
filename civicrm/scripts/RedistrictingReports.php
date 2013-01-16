3b1d4b5d4e13824145cd9dedb3753c7c3bb8c660
Ashfaqul Islam <Ashfaqul Islam>
2013-01-16 12:38:44 -0500

    Added senator name, sorted by totals, and updated css. refs #6048

--- civicrm/scripts/RedistrictingReports.php
+++ civicrm/scripts/RedistrictingReports.php
@@ -79,6 +79,71 @@ $district_counts = array();
 // Store detailed contact information per district
 $contacts_per_dist = array();
 
+$senator_names = array(
+	"1" => "LaValle, Kenneth P.",
+	"2" => "Flanagan, John J.",
+	"3" => "Zeldin, Lee M.",
+	"4" => "Boyle, Philip M.",
+	"5" => "Marcellino, Carl L",
+	"6" => "Hannon, Kemp",
+	"7" => "Martins, Jack M.",
+	"8" => "Fuschillo, Charles J. Jr.",
+	"9" => "Skelos, Dean G.",
+	"10" => "Sanders, James",
+	"11" => "Avella, Tony",
+	"12" => "Gianaris, Michael",
+	"13" => "Peralta, Jose",
+	"14" => "Smith, Malcolm A.",
+	"15" => "Addabbo, Joseph P., Jr",
+	"16" => "Stavisky, Toby Ann",
+	"17" => "Felder, Simcha",
+	"18" => "Dilan, Martin Malave",
+	"19" => "Sampson, John L.",
+	"20" => "Adams, Eric",
+	"21" => "Parker, Kevin S.",
+	"22" => "Golden, Martin J.",
+	"23" => "Savino, Diane J.",
+	"24" => "Lanza, Andrew J",
+	"25" => "Montgomery, Velmanette",
+	"26" => "Squadron, Daniel L",
+	"27" => "Hoylman, Brad",
+	"28" => "Krueger, Liz",
+	"29" => "Serrano, Jose M.",
+	"30" => "Perkins, Bill",
+	"31" => "Espaillat, Adriano",
+	"32" => "Diaz, Ruben",
+	"33" => "Rivera, Gustavo",
+	"34" => "Klein, Jeffrey D.",
+	"35" => "Stewart-Cousins, Andrea",
+	"36" => "Hassell-Thompson, Ruth",
+	"37" => "Latimer, George S.",
+	"38" => "Carlucci, David",
+	"39" => "Larkin, William J., Jr.",
+	"40" => "Ball, Greg",
+	"41" => "Gipson, Terry",
+	"42" => "Bonacic, John J.",
+	"43" => "Marchione, Kathleen A.",
+	"44" => "Breslin, Neil D.",
+	"45" => "Little, Elizabeth",
+	"47" => "Griffo, Joseph A.",
+	"48" => "Ritchie, Patty",
+	"49" => "Farley, Hugh T.",
+	"50" => "DeFrancisco, John A.",
+	"51" => "Seward, James L.",
+	"52" => "Libous, Tom",
+	"53" => "Valesky, David J.",
+	"54" => "Nozzolio, Michael F.",
+	"55" => "O'Brien, Ted",
+	"56" => "Robach, Joseph E.",
+	"57" => "Young, Catharine",
+	"58" => "O'Mara, Thomas F.",
+	"59" => "Gallivan, Patrick M.",
+	"60" => "Grisanti, Mark",
+	"61" => "Ranzenhofer, Michael H.",
+	"62" => "Maziarz, George D.",
+	"63" => "Kennedy, Timothy"
+);
+
 // ----------------------------------------------------------------------
 // Request Handler 														|
 // ----------------------------------------------------------------------
@@ -490,5 +555,10 @@ function get_age($birth_date, $default = '-'){
 function table_exists($db, $table_name){
 	$res = bb_mysql_query("SHOW TABLES LIKE '" . $table_name . "'", $db, true);
 	return (mysql_num_rows($res) > 0);
+}
 
+function get_senator_name($district){
+
+	global $senator_names;
+	return get($senator_names, $district, "Undecided");
 }
