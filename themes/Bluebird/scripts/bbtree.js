ca99a98ec463ec2c829dbeb3e86d6cc95e42cb42
Dan Pozzie <dpozzie@gmail.com>
2013-04-08 13:37:17 -0400

    Fixes #6511 fixes IE8 html bug in keyword add

--- themes/Bluebird/scripts/bbtree.js
+++ themes/Bluebird/scripts/bbtree.js
@@ -1614,7 +1614,13 @@ var BBTreeModal = {
 				{
 					toAddDT += 'isReserved';
 				}
-				toAddDT += '" id="tagLabel_'+data.id+'" description="'+data.description+'" tlvl="1" tid="'+data.id+'" parent="'+removeTagLabel(BBTreeModal.taggedID)+'"><div class="ddControl"></div><div class="tag"><span class="name">'+data.name+'</span></div><span class="entityCount" style="display:none">Unknown</span><span class="fCB">'+addControlBox(addTagLabel(data.id), callTree.currentSettings.displaySettings.currentTree )+'</dt>';
+				toAddDT += '" id="tagLabel_'+data.id+'" description="'+data.description+'" tlvl="1"';
+				toAddDT += 'tid="'+data.id+'" parent="'+removeTagLabel(BBTreeModal.taggedID)+'">';
+				toAddDT += '<div class="ddControl"></div>';
+				toAddDT += '<div class="tag"><span class="name">'+data.name+'</span></div>';
+				toAddDT += '<span class="entityCount" style="display:none">Unknown</span>';
+				toAddDT += addControlBox(addTagLabel(data.id), callTree.currentSettings.displaySettings.currentTree );
+				toAddDT += '</dt>';
 				cj('dl.tagLabel_296').prepend(toAddDT);
 				callTree.slideDownTree();
 				BBTreeEdit.setTagInfo();
