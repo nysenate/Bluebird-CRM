d64d44d9fd0a1241c01c94d52c4ed00ccd98fbba
Brian Shaughnessy <brian@lcdservices.biz>
2013-04-09 17:25:54 -0400

    refs #5353 implement moonocolor skin; restore underline button

--- modules/civicrm/packages/ckeditor/config.js
+++ modules/civicrm/packages/ckeditor/config.js
@@ -7,6 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+  //NYSS set skin
+  config.skin = 'moonocolor';
     
   // disable auto spell check
   config.scayt_autoStartup = false;
@@ -47,7 +50,7 @@ CKEDITOR.editorConfig = function( config ) {
 
   // Remove some buttons, provided by the standard plugins, which we don't
   // need to have in the Standard(s) toolbar.
-  config.removeButtons = 'Underline,Subscript,Superscript,Anchor,Table,Styles,Format,Symbols,Strike,About';
+  config.removeButtons = 'Subscript,Superscript,Anchor,Table,Styles,Format,Symbols,Strike,About';
 
   //NYSS old config retained for reference
   /*config.toolbar_Full =
