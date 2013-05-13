0f467f46df19b5edc9230519cb5963d316104163
Graylin Kim <graylin.kim@gmail.com>
2013-05-13 12:34:00 -0400

    Smarter handling of fn_group_contacts for #6737.

--- modules/nyss_dedupe/shadow.sql
+++ modules/nyss_dedupe/shadow.sql
@@ -187,10 +187,8 @@ CREATE TRIGGER shadow_contact_insert_fn_trigger AFTER INSERT ON shadow_contact
 
         DECLARE CONTINUE HANDLER FOR NOT FOUND SET not_found = 'True';
 
-        -- Records with a first name of NULL can be skipped here
-        IF NEW.first_name IS NOT NULL AND NEW.first_name != '' THEN
-
-            -- Loop over the matching first name groups
+        IF IFNULL(NEW.first_name,'') != '' THEN
+            -- If the new name isn't empty, create new mappings from matching groups
             OPEN find_fn;
             insert_loop: LOOP
                 FETCH find_fn INTO new_fn_group_id;
@@ -199,13 +197,14 @@ CREATE TRIGGER shadow_contact_insert_fn_trigger AFTER INSERT ON shadow_contact
                     LEAVE insert_loop;
                 END IF;
 
-                -- Insert a new contact record for the fn_group
                 INSERT INTO fn_group_contact (fn_group_id, contact_id) VALUES (new_fn_group_id, NEW.contact_id);
                 SET new_insert = 0;
             END LOOP;
             CLOSE find_fn;
 
-            -- If we never found matches, insert a new record set instead
+            -- If we no matching groups are found, create a new one marked as new=1
+            -- so that we can easily find names that weren't part of the original
+            -- nickname mappings
             IF new_insert = 1 THEN
                 INSERT INTO fn_group (given, new) VALUES (NEW.first_name, 1);
                 SET new_fn_group_id = LAST_INSERT_ID();
@@ -231,33 +230,46 @@ CREATE TRIGGER shadow_contact_update_fn_trigger AFTER UPDATE ON shadow_contact
 
         DECLARE CONTINUE HANDLER FOR NOT FOUND SET not_found = 'True';
 
-        -- Records with a first name of NULL can be skipped here
-        IF NEW.first_name IS NOT NULL THEN
+        IF IFNULL(NEW.first_name,'') != IFNULL(OLD.first_name,'') THEN
+            -- If the first name has changed, delete the old mappings
+            DELETE FROM fn_group_contact WHERE contact_id = OLD.contact_id;
 
-            -- Loop over the matching first name groups
-            OPEN find_fn;
-            insert_loop: LOOP
-                FETCH find_fn INTO new_fn_group_id;
+            IF IFNULL(NEW.first_name,'') != '' THEN
+                -- If the new name isn't empty, create new mappings from matching groups
+                OPEN find_fn;
+                insert_loop: LOOP
+                    FETCH find_fn INTO new_fn_group_id;
 
-                IF not_found = 'True' THEN
-                    LEAVE insert_loop;
+                    IF not_found = 'True' THEN
+                        LEAVE insert_loop;
+                    END IF;
+
+                    INSERT INTO fn_group_contact (fn_group_id, contact_id) VALUES (new_fn_group_id, NEW.contact_id);
+                    SET new_insert = 0;
+                END LOOP;
+                CLOSE find_fn;
+
+                -- If we no matching groups are found, create a new one marked as new=1
+                -- so that we can easily find names that weren't part of the original
+                -- nickname mappings
+                IF new_insert = 1 THEN
+                    INSERT INTO fn_group (given, new) VALUES (NEW.first_name, 1);
+                    SET new_fn_group_id = LAST_INSERT_ID();
+                    INSERT INTO fn_group_name (fn_group_id, name) VALUES (new_fn_group_id, NEW.first_name);
+                    INSERT INTO fn_group_contact (fn_group_id, contact_id) VALUES (new_fn_group_id, NEW.contact_id);
                 END IF;
 
-                -- Insert a new contact record for the fn_group
-                INSERT INTO fn_group_contact (fn_group_id, contact_id) VALUES (new_fn_group_id, NEW.contact_id);
-                SET new_insert = 0;
-            END LOOP;
-            CLOSE find_fn;
-
-            -- If we never found matches, insert a new record set instead
-            IF new_insert = 1 THEN
-                INSERT INTO fn_group (given, new) VALUES (NEW.first_name, 1);
-                SET new_fn_group_id = LAST_INSERT_ID();
-                INSERT INTO fn_group_name (fn_group_id, name) VALUES (new_fn_group_id, NEW.first_name);
-                INSERT INTO fn_group_contact (fn_group_id, contact_id) VALUES (new_fn_group_id, NEW.contact_id);
             END IF;
 
         END IF;
+    END
+|
+
+
+DROP TRIGGER IF EXISTS shadow_contact_delete_fn_trigger |
+CREATE TRIGGER shadow_contact_delete_fn_trigger AFTER DELETE ON shadow_contact
+    FOR EACH ROW BEGIN
+        DELETE FROM fn_group_contact WHERE contact_id = OLD.contact_id;
     END
 |
 
