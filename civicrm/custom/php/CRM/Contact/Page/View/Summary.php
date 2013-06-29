67648ddc0fba5f253e29a33621ff359d9970f866
Brian Shaughnessy <brian@lcdservices.biz>
2013-06-28 22:45:01 -0400

    fixes #6900 alter who can view mailings tab

--- civicrm/custom/php/CRM/Contact/Page/View/Summary.php
+++ civicrm/custom/php/CRM/Contact/Page/View/Summary.php
@@ -281,9 +281,11 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
     $components = CRM_Core_Component::getEnabledComponents();
 
     foreach ($components as $name => $component) {
-      //NYSS 6698
+      //NYSS 6698/6900
       if ( CRM_Utils_Array::value($name, $this->_viewOptions) &&
-         CRM_Core_Permission::access($component->name)
+        ( CRM_Core_Permission::access($component->name) ||
+          ( $component->name == 'CiviMail' && CRM_Core_Permission::check('access CiviCRM') )
+        )
       ) {
         $elem = $component->registerTab();
 
