38221456c1f3bb7908b189b398cc6fa1f08d6983
Brian Shaughnessy <brian@lcdservices.biz>
2013-07-29 11:04:46 -0400

    add --quiet option to greeting script to suppress verbose output

--- civicrm/scripts/updateAllGreetings.php
+++ civicrm/scripts/updateAllGreetings.php
@@ -24,10 +24,10 @@ error_reporting(E_ERROR | E_PARSE | E_WARNING);
 function run()
 {
   $prog = basename(__FILE__);
-  $shortopts = 'c:nf';
-  $longopts = array('ct=', 'dry-run', 'force');
+  $shortopts = 'c:nfq';
+  $longopts = array('ct=', 'dry-run', 'force', 'quiet');
   $stdusage = civicrm_script_usage();
-  $usage = "[--ct|-c {Individual|Household|Organization}] [--dry-run|-n] [--force|-f]";
+  $usage = "[--ct|-c {Individual|Household|Organization}] [--dry-run|-n] [--force|-f] [--quiet|-q]";
   $contactOpts = array(
     'i' => 'Individual',
     'h' => 'Household',
@@ -41,7 +41,7 @@ function run()
   }
 
   if (!is_cli_script()) {
-      echo "<pre>\n";
+    echo "<pre>\n";
   }
 
   //log the execution of script
@@ -97,10 +97,18 @@ function run()
       //make sure we have a contact type
       if ( empty($dao->contact_type) ) {
         echo "Contact ID {$dao->id} has no contact type set. We are unable to set the greeting values.\n";
+        flush();
+        ob_flush();
         continue;
       }
 
-      echo "Processing contact ID {$dao->id} (type={$dao->contact_type}) {$dao->display_name}\n";
+      if ($optlist['quiet'] == FALSE) {
+        echo "Processing contact ID {$dao->id} (type={$dao->contact_type}) {$dao->display_name}\n";
+        ob_end_flush();
+        ob_flush();
+        flush();
+        ob_start();
+      }
       CRM_Contact_BAO_Contact::processGreetings($dao, TRUE);
       $cnt++;
     }
@@ -111,9 +119,7 @@ function run()
     }
   }
 
-  echo "[{$optlist['site']}] Finished processing $cnt contacts.\n";
+  echo "[{$optlist['site']}] Finished processing greetings for $cnt contacts.\n";
 }
 
-
 run();
-
