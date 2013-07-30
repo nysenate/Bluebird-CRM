0fb65e09bb2a156e7bc6abb3ea5b74e066552784
Brian Shaughnessy <brian@lcdservices.biz>
2013-07-30 13:42:33 -0400

    refs #7023 3rd party importer improvements

--- modules/nyss_io/nyss_lib.php
+++ modules/nyss_io/nyss_lib.php
@@ -2,97 +2,87 @@
 
 function nyss_out($type,$v,$toscreen=false) {
 
-	global $nyss_ioline,$nyss_iototallines;
+  global $nyss_ioline,$nyss_iototallines;
 
-	if(!empty($v) && (($type=='debug' && NYSSIODEBUG) || $type!='debug')) {
-		$v = print_r($v,true);
+  if(!empty($v) && (($type=='debug' && NYSSIODEBUG) || $type!='debug')) {
+    $v = print_r($v,true);
 
-		if ($type=='error') error_log($v. " (line: $nyss_ioline)");
+    if ($type=='error') error_log($v. " (line: $nyss_ioline)");
 
-		if ($toscreen) {
-			echo "<pre>$v (line: $nyss_ioline of $nyss_iototallines memory: ".(round(memory_get_usage()/1048576,4))." MB".")</pre>";
-			flush();
-			ob_flush();
-		}
+    if ($toscreen) {
+      echo "<pre>$v (line: $nyss_ioline of $nyss_iototallines memory: ".(round(memory_get_usage()/1048576,4))." MB".")</pre>";
+      flush();
+      ob_flush();
+    }
 
-		unset($v);
-	}
+    unset($v);
+  }
 }
 
 class nyss_iofileobject {
 
- public $filepath;
- public $type = ",";
- public $hasHeader = 1;
- public $linecount = 0;
- public $header = array();
+  public $filepath;
+  public $type = ",";
+  public $hasHeader = 1;
+  public $linecount = 0;
+  public $header = array();
 
- private $file_handle;
+  private $file_handle;
 
- function nyss_iofileobject($filepath, $type=',') {
+  function nyss_iofileobject($filepath, $type=',') {
+    $this->type=$type;
+    $this->filepath = $filepath;
 
-  $this->type=$type;
-  $this->filepath = $filepath;
+    ini_set('auto_detect_line_endings',TRUE);
 
-  ini_set('auto_detect_line_endings',TRUE);
+    // open the file for reading
+    $this->file_handle = fopen($filepath, 'r');
 
-  // open the file for reading
-  $this->file_handle = fopen($filepath, 'r');
+    $line = $this->getLineAsArray();
 
-  $line = $this->getLineAsArray();
+    if (!is_array($line)) {
+      //set error message and exit
+      drupal_set_message('Bad file. Either the file is not a csv or has the wrong number of headers.','error');
+      return false;
+    }
 
- if (!is_array($line)) {
-
-         //set error message and exit
-         drupal_set_message('Bad file. Either the file is not a csv or has the wrong number of headers.','error');
-         return false;
+    //get the header row
+    $this->header = array();
+    foreach ($line as $key=>$val) $this->header[strtolower($val)]=$key;
   }
 
-  //get the header row
-  $this->header = array();
-  foreach ($line as $key=>$val) $this->header[strtolower($val)]=$key;
+  public function countLines() {
+    $ns=$rs=0;
+    $fh = fopen($this->filepath, 'r');
 
- }
+    while ($chunk = fread($fh, 1024000)) {
+      $ns += substr_count($chunk, "\n");
+      $rs += substr_count($chunk, "\r");
+    }
 
- public function countLines() {
-
-	$ns=$rs=0;
-
-	$fh = fopen($this->filepath, 'r');
-
-	while ($chunk = fread($fh, 1024000)) {
-		$ns += substr_count($chunk, "\n");
-		$rs += substr_count($chunk, "\r");
-	}
-
-	if ($rs>$ns) $ns=$rs;
-	return $rs;
+    if ($rs>$ns) $ns=$rs;
+    return $rs;
   }
 
- public function getLine() {
+  public function getLine() {
+    if ($line = $this->getLineAsArray()) {
+      ++$this->linecount;
+      foreach ( $this->header as $colname=>$colnum ) {
+        $aLine[$colname] = $line[$colnum];
+      }
+      return $aLine;
+    }
 
-	if ($line = $this->getLineAsArray()) {
+    return false;
+  }
 
-		++$this->linecount;
-
-	        foreach ($this->header as $colname=>$colnum) $aLine[$colname] = $line[$colnum];
-
-	 	return $aLine;
- 	}
-
-	return false;
- }
-
- function getLineAsArray()
- {
-   $line = fgets($this->file_handle);
-   if ($line) {
-     return explode($this->type, rtrim($line));
-   }
-   else {
-     return false;
-   }
- } // getLineAsArray()
-
-}
-
+  function getLineAsArray() {
+    $line = fgets($this->file_handle);
+    if ($line) {
+      return explode($this->type, rtrim($line));
+    }
+    else {
+      return false;
+    }
+  }// getLineAsArray()
+}//end class
