ed9417567447e8f0e90c722f9ed0127e2e84a646
Ken Zalewski <zalewski.senate@gmail.com>
2013-08-05 17:29:40 -0400

    Code cleanup prior to deprecating these "Form" classes.

--- civicrm/scripts/signup_reports/nysenate_api/ContactForm.php
+++ civicrm/scripts/signup_reports/nysenate_api/ContactForm.php
@@ -1,162 +1,168 @@
 <?php
-    require_once 'get_services/xmlrpc-api-messages.inc';
+require_once 'get_services/xmlrpc-api-messages.inc';
+require_once 'Form.php';
+require_once 'NySenateContact.php';
 
-    require_once 'Form.php';
-    require_once 'NySenateContact.php';
 
-	class ContactForm extends Form {
+class ContactForm extends Form
+{
+  function __construct($api_key, $domain_name)
+  {
+    parent::__construct($api_key, $domain_name);
+  }
 
-		function __construct($api_key, $domain_name) {
-			parent::__construct($api_key, $domain_name);
-		}
 
-		function getRawEntries($start_date, $end_date, $start_id, $end_id, $limit = 1000) {
-			$service = new ContactMessages($this->domain_name, $this->api_key);
-			$values = $service->get(array(
-			    'start_date' => $start_date,
-			    'end_date' => $end_date,
-			    'start_mid' => $start_id,
-			    'end_mid' => $end_id,
-			    'limit' => $limit,
-				'source_form' => NULL,
-				'senator_short_name' => NULL,
-				'district_number' => NULL,
-			));
+  function getRawEntries($start_date, $end_date, $start_id, $end_id, $limit = 1000)
+  {
+    $service = new ContactMessages($this->domain_name, $this->api_key);
+    $values = $service->get(array(
+        'start_date' => $start_date,
+        'end_date' => $end_date,
+        'start_mid' => $start_id,
+        'end_mid' => $end_id,
+        'limit' => $limit,
+      'source_form' => NULL,
+      'senator_short_name' => NULL,
+      'district_number' => NULL,
+    ));
 
-			return $values;
-		}
+    return $values;
+  } // getRawEntries()
 
-		function getFormContacts($start_date, $end_date, $start_id, $end_id, $limit = 1000) {
-			$values = $this->getRawEntries(
-				$start_date, $end_date, $start_id, $end_id, $limit
-			);
 
-			$contacts = array();
+  function getFormContacts($start_date, $end_date, $start_id, $end_id, $limit = 1000)
+  {
+    $values = $this->getRawEntries(
+      $start_date, $end_date, $start_id, $end_id, $limit
+    );
 
-			foreach ($values['items'] as $entry) {
+    $contacts = array();
 
-				if($this->valid_instance($entry['to_short_name'])) {
-					$contacts[] = $this->formContactFromEntry($entry);
-				}
-			}
+    foreach ($values['items'] as $entry) {
+      if ($this->valid_instance($entry['to_short_name'])) {
+        $contacts[] = $this->formContactFromEntry($entry);
+      }
+    }
 
-			return $contacts;
-		}
+    return $contacts;
+  } // getFormContacts()
 
-		/*
-		 * given an entry from the services module
-		 * convert it in to a NySenateContact object
-		 */
-		function formContactFromEntry($entry) {
-			$params = $this->paramsFromEntry($entry);
 
-			$formContact = new NySenateContact();
-			$formContact->civicrm_contact_params = $params;
+  /*
+   * given an entry from the services module
+   * convert it in to a NySenateContact object
+   */
+  function formContactFromEntry($entry)
+  {
+    $params = $this->paramsFromEntry($entry);
 
-			if($entry['issues'] && count($entry['issues']) > 0) {
-				if(preg_match("/^\s*$/", $entry['issues'][0])) {
-					$entry['issues'] = NULL;
-				}
-			}
+    $formContact = new NySenateContact();
+    $formContact->civicrm_contact_params = $params;
 
-			$formContact->activity =
-				array(
-					'activity_name'		=>  'Other',
-					'activity_type' 	=>	'Website Contact',
-					'activity_subject'	=>	$entry['subject'],
-					'activity_details'	=>	$this->getActivityNotes($entry),
-					'keywords'			=>  $entry['issues']
-				);
+    if ($entry['issues'] && count($entry['issues']) > 0) {
+      if (preg_match("/^\s*$/", $entry['issues'][0])) {
+        $entry['issues'] = NULL;
+      }
+    }
 
-			$formContact->issues = $entry['issues'];
+    $formContact->activity =
+      array(
+        'activity_name' => 'Other',
+        'activity_type' => 'Website Contact',
+        'activity_subject' => $entry['subject'],
+        'activity_details' => $this->getActivityNotes($entry),
+        'keywords' => $entry['issues']
+      );
 
-			$formContact->senator_short_name = $entry['to_short_name'];
+    $formContact->issues = $entry['issues'];
+    $formContact->senator_short_name = $entry['to_short_name'];
+    return $formContact;
+  } // formContactFromEntry()
 
-			return $formContact;
-		}
 
-		function getActivityNotes($entry) {
-			$notes = "";
+  function getActivityNotes($entry)
+  {
+    $notes = "";
 
-			$notes = $notes."<p>Submitted on "
-						.date("F j, Y, g:i a", $entry['submitted'])."</p>";
+    $notes = $notes."<p>Submitted on "
+          .date("F j, Y, g:i a", $entry['submitted'])."</p>";
 
-			$name = $entry['first_name']." ".$entry['last_name'];
+    $name = $entry['first_name']." ".$entry['last_name'];
 
-			$notes = $notes."<p>".$name."'s message:</p>";
-			$notes = $notes."<p>\"".$entry['message']."\"</p>";
+    $notes = $notes."<p>".$name."'s message:</p>";
+    $notes = $notes."<p>\"".$entry['message']."\"</p>";
 
-			$notes = $notes."<p>The submitter <b>is </b>";
-			if($entry['voter_registered'] == 1) {
-				$notes = $notes."<b>not</b> ";
-			}
-			$notes = $notes."registered to vote at the following address:<br/>";
-			$notes = $notes.$entry['address']."<br/>".$entry['city']
-						   .", ".$entry['state'].", ".$entry['zip']."</p>";
+    $notes = $notes."<p>The submitter <b>is </b>";
+    if ($entry['voter_registered'] == 1) {
+      $notes = $notes."<b>not</b> ";
+    }
+    $notes = $notes."registered to vote at the following address:<br/>";
+    $notes = $notes.$entry['address']."<br/>".$entry['city']
+             .", ".$entry['state'].", ".$entry['zip']."</p>";
 
-			return $notes;
-		}
+    return $notes;
+  } // getActivityNotes()
 
-		/**
-		 *
-		 * Given a contact entry from the services module
-		 * convert it to params readable by civi
-		 * @param $entry
-		 * @param $contact_type
-		 */
-		function paramsFromEntry($entry, $contact_type = "Individual") {
-			static $config;
 
-			if(!$config) {
-				$config = $this->get_config();
-			}
+  /**
+   *
+   * Given a contact entry from the services module
+   * convert it to params readable by civi
+   * @param $entry
+   * @param $contact_type
+   */
+  function paramsFromEntry($entry, $contact_type = "Individual")
+  {
+    static $config;
 
-			$params = array();
+    if (!$config) {
+      $config = $this->get_config();
+    }
 
-			$params['contact_type'] = $contact_type;
+    $params = array();
+    $params['contact_type'] = $contact_type;
+    $params['first_name'] = $entry['first_name'];
+    $params['last_name'] = $entry['last_name'];
 
-			$params['first_name'] 	= $entry['first_name'];
-			$params['last_name'] 	= $entry['last_name'];
+    $address = array(
+      'street_address' => $entry['address'],
+      'city' => $entry['city'],
+      'state_province' => $entry['state'],
+      'postal_code' => $entry['zip']
+    );
 
-			$address = array(
-				'street_address' 	=> $entry['address'],
-				'city' 			 	=> $entry['city'],
-				'state_province' 	=> $entry['state'],
-				'postal_code' 	 	=> $entry['zip']
-			);
+    if (array_key_exists('apartment', $entry)) {
+      $address['street_address2'] = $entry['apartment'];
+    }
+    else if (array_key_exists('address2', $entry)) {
+      $address['street_address2'] = $entry['address2'];
+    }
 
-			if(array_key_exists('apartment', $entry)) {
-				$address['street_address2']	= $entry['apartment'];
-			} else if (array_key_exists('address2', $entry)) {
-				$address['street_address2']	= $entry['address2'];
-			}
+    //geocode, dist assign and format address
+    require_once 'CRM/Utils/SAGE.php';
+    CRM_Utils_SAGE::lookup(&$address);
 
-			//geocode, dist assign and format address
-			require_once 'CRM/Utils/SAGE.php';
-			CRM_Utils_SAGE::lookup(&$address);
+    $address['location_type_id'] = '1';
+    $address['is_primary'] = true;
 
-			$address['location_type_id'] 	= '1';
-			$address['is_primary'] 		= true;
+    $params['address'] = array();
+    $params['address'][] = $address;
 
-			$params['address'] = array();
-			$params['address'][] = $address;
+    $params['phone'] = array(
+      'phone' => $entry['phone'],
+      'location_type_id' => '1',
+      'phone_type_id' => '1',
+      'is_primary' => true
+    );
 
-			$params['phone'] = array(
-				'phone' 			=> $entry['phone'],
-				'location_type_id' 	=> '1',
-				'phone_type_id' 	=> '1',
-				'is_primary' 		=> true
-			);
+    $params['email'] = array();
+    $params['email'][] = array(
+      'email' => $entry['from_email'],
+      'email_type_id' => '1',
+      'is_primary' => true
+    );
 
-			$params['email'] = array();
-			$params['email'][] = array(
-				'email' 			=> $entry['from_email'],
-				'email_type_id' 	=> '1',
-				'is_primary' 		=> true
-			);
-
-			$params['source'] = "Website - Contact";
-
-			return $params;
-		}
-	}
+    $params['source'] = "Website - Contact";
+    return $params;
+  } // paramsFromEntry()
+}
--- civicrm/scripts/signup_reports/nysenate_api/NySenateContact.php
+++ civicrm/scripts/signup_reports/nysenate_api/NySenateContact.php
@@ -1,8 +1,9 @@
 <?php
-	class NySenateContact {
-		public $senator_short_name;
-		public $civicrm_contact_params;
-		public $activity;
-		public $issues;
-		public $initiative;
-	}
+class NySenateContact
+{
+  public $senator_short_name;
+  public $civicrm_contact_params;
+  public $activity;
+  public $issues;
+  public $initiative;
+}
