578dd3cbd569bcddc2862d6163cde1895f16de89
Graylin Kim <graylin.kim@gmail.com>
2013-11-26 17:28:11 -0500

    Initial prototype base. Reflects current production.

---
+++ themes/Bluebird/scripts/prototypes/current.php
@@ -0,0 +1,187 @@
+<html>
+<head>
+    <title>Current Production Tagging</title>
+    <style type="text/css" media="all">
+        @import url("http://sd99/modules/system/system.base.css?mwmgvm");
+        @import url("http://sd99/modules/system/system.menus.css?mwmgvm");
+        @import url("http://sd99/modules/system/system.messages.css?mwmgvm");
+        @import url("http://sd99/modules/system/system.theme.css?mwmgvm");
+        @import url("http://sd99/modules/field/theme/field.css?mwmgvm");
+        @import url("http://sd99/modules/node/node.css?mwmgvm");
+        @import url("http://sd99/modules/search/search.css?mwmgvm");
+        @import url("http://sd99/modules/user/user.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/packages/jquery/css/jquery.autocomplete.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/packages/jquery/css/menu.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/packages/jquery/css/token-input-facebook.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/packages/jquery/jquery-ui-1.9.0/css/smoothness/jquery-ui-1.9.0.custom.min.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/DataTables/media/css/demo_table_jui.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/css/civicrm.css?mwmgvm");
+        @import url("http://sd99/sites/all/modules/civicrm/css/extras.css?mwmgvm");
+    </style>
+    <style type="text/css" media="screen,projection">
+        @import url("http://sd99/sites/default/themes/Bluebird/css/Bluebird.css?mwmgvm");
+        @import url("http://sd99/sites/default/themes/Bluebird/nyss_skin/skin.css?mwmgvm");
+        @import url("http://sd99/sites/default/themes/Bluebird/nyss_skin/civi-header.css?mwmgvm");
+        @import url("http://sd99/sites/default/themes/Bluebird/css/style.css?mwmgvm");
+    </style>
+    <script type="text/javascript" src="http://sd99/misc/jquery.js?v=1.4.4"></script>
+    <script type="text/javascript" src="http://sd99/misc/jquery.once.js?v=1.2"></script>
+    <script type="text/javascript" src="http://sd99/misc/drupal.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/jquery-1.8.3.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/jquery-ui-1.9.0/js/jquery-ui-1.9.0.custom.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.autocomplete.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.menu.pack.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.chainedSelects.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.tableHeader.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.textarearesizer.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.form.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.tokeninput.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.timeentry.pack.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.mousewheel.pack.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/NYSS/jquery.toolTip.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/DataTables/media/js/jquery.dataTables.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.FormNavigate.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.validate.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.ui.datepicker.validation.pack.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery-fieldselection.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.jeditable.mini.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.blockUI.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.notify.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jquery.redirect.min.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/js/rest.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/js/Common.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/js/jquery/jquery.crmeditable.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/js/jquery/jquery.crmasmselect.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/NYSS/jquery.cursorStart.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/packages/jquery/plugins/jstree/jquery.jstree.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/all/modules/civicrm/js/noconflict.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/default/themes/Bluebird/scripts/message-manager.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/default/themes/Bluebird/scripts/civi-header.js?mwmgvm"></script>
+    <script type="text/javascript" src="http://sd99/sites/default/themes/Bluebird/scripts/general.js?mwmgvm"></script>
+
+
+    <script src="/sites/default/themes/Bluebird/scripts/bbtree.js" type="text/javascript"></script>
+    <link type="text/css" rel="stylesheet" media="screen,projection" href="/sites/default/themes/Bluebird/nyss_skin/tags/tags.css" />
+    <script type="text/javascript">
+    var BBCID = 18304;
+    var BBActionConst = 16;
+    BBTree.startInstance({pullSets: [291], buttonType: 'tagging'});
+    BBTree.initContainer('', {pullSets: [291]});
+    </script>
+</head>
+<body>
+<div class="container" style="padding-top: 25px;">
+<div class="clear span-24 main-container">
+<div id="crm-container" class="crm-container crm-accordion-processed" lang="en" xml:lang="en">
+  <div class="crm-block crm-content-block crm-contact-page crm-inline-edit-container crm-edit-ready">
+      <div id="mainTabContainer" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+          <ul class="crm-contact-tabs-list ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist">
+              <li id="tab_summary" class="crm-tab-button ui-state-default ui-corner-top ui-corner-bottom" role="tab" tabindex="-1" aria-controls="contact-summary" aria-labelledby="ui-id-1" aria-selected="false">
+                <a href="#" title="Summary" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-1">
+                <span> </span> Summary                <em>&nbsp;</em>
+                </a>
+              </li>
+                            <li id="tab_activity" class="crm-tab-button crm-count- ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Activities" aria-labelledby="ui-id-2" aria-selected="false">
+                <a href="#" title="Activities" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-2">
+                  <span> </span> Activities
+                  <em></em>
+                <em>0</em></a>
+              </li>
+                            <li id="tab_case" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Cases" aria-labelledby="ui-id-3" aria-selected="false">
+                <a href="#" title="Cases" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-3">
+                  <span> </span> Cases
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_mailing" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Mailings" aria-labelledby="ui-id-4" aria-selected="false">
+                <a href="#" title="Mailings" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-4">
+                  <span> </span> Mailings
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_rel" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Relationships" aria-labelledby="ui-id-5" aria-selected="false">
+                <a href="#" title="Relationships" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-5">
+                  <span> </span> Relationships
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_group" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Groups" aria-labelledby="ui-id-6" aria-selected="false">
+                <a href="#" title="Groups" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-6">
+                  <span> </span> Groups
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_note" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Notes" aria-labelledby="ui-id-7" aria-selected="false">
+                <a href="#" title="Notes" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-7">
+                  <span> </span> Notes
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_tag" class="crm-tab-button crm-count-0 ui-state-default ui-corner-top ui-corner-bottom disabled ui-tabs-active ui-state-active" role="tab" tabindex="0" aria-controls="Tags" aria-labelledby="ui-id-8" aria-selected="true">
+                <a href="#" title="Tags" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-8">
+                  <span> </span> Tags
+                  <em>0</em>
+                </a>
+              </li>
+                            <li id="tab_log" class="crm-tab-button crm-count- ui-state-default ui-corner-top ui-corner-bottom disabled" role="tab" tabindex="-1" aria-controls="Change_Log" aria-labelledby="ui-id-9" aria-selected="false">
+                <a href="#" title="Change Log" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-9">
+                  <span> </span> Change Log
+                  <em></em>
+                <em>3</em></a>
+              </li>
+        </ul>
+
+<div id="Tags" class="ui-tabs-panel ui-widget-content ui-corner-bottom" aria-live="polite" aria-labelledby="ui-id-8" role="tabpanel" aria-expanded="true" aria-hidden="false" style="display: block;">
+    <div class="crm-container-snippet" bgcolor="white">
+        <div id="tagSelect" class="crm-block crm-content-block">
+            <div id="TagGroups" class="view-content">
+                <h3>Tags</h3>
+                <div id="crm-tagListWrap">
+                    <div class="crm-section tag-section contact-tagset-296-section crm-processed-input">
+                        <div class="label">
+                            <label>Keywords</label>
+                        </div>
+                        <div class="content">
+                            &nbsp;
+                            <ul class="token-input-list-facebook">
+                                <li class="token-input-input-token-facebook token-input-highlighted-token-facebook">
+                                    <input type="text" autocomplete="off" maxlength="64" id="token-input-contact_taglist_296" style="outline: none; width: 30px;">
+                                    <tester style="position: absolute; top: -9999px; left: -9999px; width: auto; font-size: 12px; font-family: Verdana, Arial, sans-serif; font-weight: 400; letter-spacing: 0px; white-space: nowrap;"></tester>
+                                </li>
+                            </ul>
+                            <input type="hidden" name="contact_taglist[296]" value="" id="contact_taglist_296" class="contact-taglist_296" style="display: none;">
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                    <div id="BBTreeContainer" class="BB_default">
+                        <div class="BBTree tagging BB_default tagLabel_291" id="tagLabel_291">
+                        </div>
+                    </div>
+
+                    <div class="crm-section tag-section contact-tagset-292-section crm-processed-input">
+                        <div class="label">
+                            <label>Positions</label>
+                        </div>
+                        <div class="content">
+                            &nbsp;
+                            <ul class="token-input-list-facebook">
+                                <li class="token-input-input-token-facebook">
+                                    <input type="text" autocomplete="off" maxlength="64" id="token-input-contact_taglist_292" style="outline: none;">
+                                    <tester style="position: absolute; top: -9999px; left: -9999px; width: auto; font-size: 12px; font-family: Verdana, Arial, sans-serif; font-weight: 400; letter-spacing: 0px; white-space: nowrap;"></tester>
+                                </li>
+                            </ul>
+                            <input type="hidden" name="contact_taglist[292]" value="" id="contact_taglist_292" class="contact-taglist_292" style="display: none;">
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clear"></div>
+</div>
+</div>
+</div>
+</body>
+</html>
