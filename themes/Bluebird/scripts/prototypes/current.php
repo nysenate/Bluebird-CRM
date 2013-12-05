e77f1e3f37e86e7920feb5f537cdd80338a58e83
Graylin Kim <graylin.kim@gmail.com>
2013-12-05 11:58:34 -0500

    Fix Keyword and Position functionality on prototype base.

--- themes/Bluebird/scripts/prototypes/current.php
+++ themes/Bluebird/scripts/prototypes/current.php
@@ -58,9 +58,7 @@
     <script type="text/javascript" src="/sites/default/themes/Bluebird/scripts/message-manager.js?mwmgvm"></script>
     <script type="text/javascript" src="/sites/default/themes/Bluebird/scripts/civi-header.js?mwmgvm"></script>
     <script type="text/javascript" src="/sites/default/themes/Bluebird/scripts/general.js?mwmgvm"></script>
-
-
-    <script src="/sites/default/themes/Bluebird/scripts/bbtree.js" type="text/javascript"></script>
+    <script type="text/javascript" src="/sites/default/themes/Bluebird/scripts/bbtree.js"></script>
     <style>
     /*
 
@@ -536,53 +534,205 @@ SassCommand*nix: sass --update themes/Bluebird/nyss_skin/tags/tags.scss:themes/B
                     <div id="TagGroups" class="view-content">
                         <h3>Tags</h3>
                         <div id="crm-tagListWrap">
-                            <div class="crm-section tag-section contact-tagset-296-section crm-processed-input">
-				<div class="tag-label">
-                                    <label>Keywords</label>
-                                </div>
-                                <div class="content">
-                                    &nbsp;
-                                    <ul class="token-input-list-facebook">
-                                        <li class="token-input-input-token-facebook token-input-highlighted-token-facebook">
-                                            <input type="text" autocomplete="off" maxlength="64" id="token-input-contact_taglist_296" style="outline: none; width: 30px;">
-                                            <tester style="position: absolute; top: -9999px; left: -9999px; width: auto; font-size: 12px; font-family: Verdana, Arial, sans-serif; font-weight: 400; letter-spacing: 0px; white-space: nowrap;"></tester>
-                                        </li>
-                                    </ul>
-                                    <input type="hidden" name="contact_taglist[296]" value="" id="contact_taglist_296" class="contact-taglist_296" style="display: none;">
+                          <div class="crm-section tag-section contact-tagset-296-section">
+                            <div class="label">
+                              <label>Keywords</label>
+                            </div>
+                            <div class="content">
+                              &nbsp;
+                              <input type="hidden" name="contact_taglist[296]" value="" id="contact_taglist_296" />
+                              <script type="text/javascript">
+
+                                  var tagUrl = "/civicrm/ajax/taglist?parentId=296&key=459191e4b5403730b0af7a496ac52055";
+                                  var contactEntityTags = '';
+
+                                  //NYSS
+                                  var hintText = "Begin typing a tag name.";
+
+                                  cj( ".contact-tagset-296-section:not(.crm-processed-input) input")
+                                    .addClass("contact-taglist_296");
+                                  cj( ".contact-tagset-296-section:not(.crm-processed-input) .contact-taglist_296"  )
+                                    .tokenInput( tagUrl, {
+                                      prePopulate: contactEntityTags,
+                                      theme: 'facebook',
+                                      hintText: hintText,
+                                      onAdd: function ( item ) {
+                                        processContactTags_296( 'select', item.id );
+
+                                        //NYSS update count of tags in summary tab
+                                        if ( cj( '.ui-tabs-nav #tab_tag a' ).length ) {
+                                          var tagCount = cj('.ui-tabs-nav #tab_tag a em').html();
+                                          tagCount++;
+                                          cj( '.ui-tabs-nav #tab_tag a em' ).html(tagCount);
+                                        }
+                                      },
+                                      onDelete: function ( item ) {
+                                        processContactTags_296( 'delete', item.id );
+                                        //NYSS update count of tags in summary tab
+                                        if ( cj( '.ui-tabs-nav #tab_tag a' ).length ) {
+                                          var tagCount = cj('.ui-tabs-nav #tab_tag a em').html();
+                                          tagCount--;
+                                          cj( '.ui-tabs-nav #tab_tag a em' ).html(tagCount);
+                                        }
+                                      }
+                                    }
+                                  );
+
+                                  cj( ".contact-tagset-296-section:not(.crm-processed-input)").addClass("crm-processed-input");
+
+                                  function processContactTags_296( action, id ) {
+                                    var postUrl          = "/civicrm/ajax/processTags";
+                                    var parentId         = "296";
+                                    var entityId         = "3550";
+                                    var entityTable      = "civicrm_contact";
+                                    var skipTagCreate    = "";
+                                    //NYSS
+                                    var skipEntityAction = "0";
+
+                                    cj.post( postUrl, { action: action, tagID: id, parentId: parentId, entityId: entityId, entityTable: entityTable,
+                                      skipTagCreate: skipTagCreate, skipEntityAction: skipEntityAction, key: "3bbd3ba7c057828fc6e35045fbe32394" },
+                                      function ( response ) {
+                                        // update hidden element
+                                        if ( response.id ) {
+                                          var curVal   = cj( ".contact-taglist_296" ).val( );
+                                          var valArray = curVal.split(',');
+                                          var setVal   = Array( );
+                                          if ( response.action == 'delete' ) {
+                                            for ( x in valArray ) {
+                                              if ( valArray[x] != response.id ) {
+                                                setVal[x] = valArray[x];
+                                              }
+                                            }
+                                            CRM.alert('', 'Removed', 'success');
+                                          }
+                                          else {
+                                            CRM.alert('', 'Saved', 'success');
+                                          }
+                                          if ( response.action == 'select' ) {
+                                            setVal    = valArray;
+                                            setVal[ setVal.length ] = response.id;
+                                          }
+
+                                          var actualValue = setVal.join( ',' );
+                                          cj( ".contact-taglist_296" ).val( actualValue );
+                                        }
+                                        // update contact summary tab
+                                        if (CRM.updateContactSummaryTags) {
+                                          CRM.updateContactSummaryTags();
+                                        }
+                                      },
+                                    "json" );
+                                  }
+
+                                </script>
                                 </div>
                                 <div class="clear"></div>
                             </div>
-			    <div id="BBTreeContainer" class="BB_default crm-container crm-section tag-section crm-processed-input">
-				 <div class="tag-label">
-				    <label>Issue Codes</label>
-				</div>
-				<input type="text" autocomplete="off" maxlength="64" id="issue-code-search" style="outline: none; ">
-
-                                <script type="text/javascript">
+                            <div id="BBTreeContainer" class="BB_default crm-container crm-section tag-section crm-processed-input">
+                              <div class="tag-label">
+                                <label>Issue Codes</label>
+                              </div>
+                              <input type="text" autocomplete="off" maxlength="64" id="issue-code-search" style="outline: none; " />
+                              <script type="text/javascript">
                                 var BBCID = 18304;
                                 var BBActionConst = 16;
                                 BBTree.startInstance({pullSets: [291], buttonType: 'tagging'});
                                 BBTree.initContainer('', {pullSets: [291]});
-                                </script>
-				<div class="clear"></div>
-
+                              </script>
+                              <div class="clear"></div>
                             </div>
+                            <div class="crm-section tag-section contact-tagset-292-section">
+                              <div class="tag-label">
+                                <label>Positions</label>
+                              </div>
+                              <div class="content">
+                                &nbsp;
+                                <input type="hidden" name="contact_taglist[292]" value="" id="contact_taglist_292" />
+                                <script type="text/javascript">
+                                  var tagUrl = "/civicrm/ajax/taglist?parentId=292&key=459191e4b5403730b0af7a496ac52055";
+                                  var contactEntityTags = '';
 
-			    <div class="crm-section tag-section contact-tagset-296-section crm-processed-input">
-				<div class="tag-label">
-                                    <label>Positions</label>
-                                </div>
-                                <div class="content">
-                                    &nbsp;
-                                    <ul class="token-input-list-facebook">
-                                        <li class="token-input-input-token-facebook">
-                                            <input type="text" autocomplete="off" maxlength="64" id="token-input-contact_taglist_292" style="outline: none;">
-                                            <tester style="position: absolute; top: -9999px; left: -9999px; width: auto; font-size: 12px; font-family: Verdana, Arial, sans-serif; font-weight: 400; letter-spacing: 0px; white-space: nowrap;"></tester>
-                                        </li>
-                                    </ul>
-                                    <input type="hidden" name="contact_taglist[292]" value="" id="contact_taglist_292" class="contact-taglist_292" style="display: none;">
-                                </div>
-                                <div class="clear"></div>
+                                  //NYSS
+                                  var hintText = "Begin typing a tag name.";
+
+                                  cj( ".contact-tagset-292-section:not(.crm-processed-input) input")
+                                    .addClass("contact-taglist_292");
+                                  cj( ".contact-tagset-292-section:not(.crm-processed-input) .contact-taglist_292"  )
+                                    .tokenInput( tagUrl, {
+                                      prePopulate: contactEntityTags,
+                                      theme: 'facebook',
+                                      hintText: hintText,
+                                      onAdd: function ( item ) {
+                                        processContactTags_292( 'select', item.id );
+
+                                        //NYSS update count of tags in summary tab
+                                        if ( cj( '.ui-tabs-nav #tab_tag a' ).length ) {
+                                          var tagCount = cj('.ui-tabs-nav #tab_tag a em').html();
+                                          tagCount++;
+                                          cj( '.ui-tabs-nav #tab_tag a em' ).html(tagCount);
+                                        }
+                                      },
+                                      onDelete: function ( item ) {
+                                        processContactTags_292( 'delete', item.id );
+                                        //NYSS update count of tags in summary tab
+                                        if ( cj( '.ui-tabs-nav #tab_tag a' ).length ) {
+                                          var tagCount = cj('.ui-tabs-nav #tab_tag a em').html();
+                                          tagCount--;
+                                          cj( '.ui-tabs-nav #tab_tag a em' ).html(tagCount);
+                                        }
+                                      }
+                                    }
+                                  );
+
+                                  cj( ".contact-tagset-292-section:not(.crm-processed-input)").addClass("crm-processed-input");
+
+                                  function processContactTags_292( action, id ) {
+                                    var postUrl          = "/civicrm/ajax/processTags";
+                                    var parentId         = "292";
+                                    var entityId         = "3550";
+                                    var entityTable      = "civicrm_contact";
+                                    var skipTagCreate    = "";
+                                    //NYSS
+                                    var skipEntityAction = "";
+
+                                    cj.post( postUrl, { action: action, tagID: id, parentId: parentId, entityId: entityId, entityTable: entityTable,
+                                      skipTagCreate: skipTagCreate, skipEntityAction: skipEntityAction, key: "3bbd3ba7c057828fc6e35045fbe32394" },
+                                      function ( response ) {
+                                        // update hidden element
+                                        if ( response.id ) {
+                                          var curVal   = cj( ".contact-taglist_292" ).val( );
+                                          var valArray = curVal.split(',');
+                                          var setVal   = Array( );
+                                          if ( response.action == 'delete' ) {
+                                            for ( x in valArray ) {
+                                              if ( valArray[x] != response.id ) {
+                                                setVal[x] = valArray[x];
+                                              }
+                                            }
+                                            CRM.alert('', 'Removed', 'success');
+                                          }
+                                          else {
+                                            CRM.alert('', 'Saved', 'success');
+                                          }
+                                          if ( response.action == 'select' ) {
+                                            setVal    = valArray;
+                                            setVal[ setVal.length ] = response.id;
+                                          }
+
+                                          var actualValue = setVal.join( ',' );
+                                          cj( ".contact-taglist_292" ).val( actualValue );
+                                        }
+                                        // update contact summary tab
+                                        if (CRM.updateContactSummaryTags) {
+                                          CRM.updateContactSummaryTags();
+                                        }
+                                      },
+                                    "json" );
+                                  }
+
+                                </script>
+                              </div>
+                            <div class="clear"></div>
                             </div>
                         </div>
                     </div>
