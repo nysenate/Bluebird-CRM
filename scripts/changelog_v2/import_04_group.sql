83853c535b3186a0d78f3c9610a4fbd2d3ecd09c
Ken Zalewski <zalewski.senate@gmail.com>
2014-08-08 17:39:15 -0400

    Default the group name to "Unknown Group (id=NN)" if a group cannot be matched during conversion.

--- scripts/changelog_v2/import_04_group.sql
+++ scripts/changelog_v2/import_04_group.sql
@@ -4,7 +4,8 @@ INSERT IGNORE INTO nyss_changelog_detail
    tmp_contact_id, tmp_change_ts, tmp_entity_info)
 SELECT
   a.log_action, 'group_contact', a.id, a.log_conn_id, a.log_user_id,
-  a.contact_id, a.log_date, CONCAT_WS(CHAR(1), 'Group', b.title, a.status)
+  a.contact_id, a.log_date,
+  CONCAT_WS(CHAR(1), 'Group', IFNULL(b.title,CONCAT('*Unknown Group (id=',a.group_id,')*')), a.status)
 FROM @LOGDB@.log_civicrm_group_contact a
 LEFT JOIN @LOGDB@.nyss_temp_staging_group b
 ON a.group_id=b.id AND a.log_date BETWEEN b.log_date AND b.log_end_date
