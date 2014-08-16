d6509070ffbc9bfd261138710ecd2edd7d1b602a
Ken Zalewski <zalewski.senate@gmail.com>
2014-08-16 00:53:30 -0400

    Simplified the log entry grouping logic.

--- civicrm/custom/php/CRM/Logging/create_detail_runtime_trigger.sql
+++ civicrm/custom/php/CRM/Logging/create_detail_runtime_trigger.sql
@@ -12,73 +12,65 @@
 ** types), plus optional Group status info (for Group entity type).
 */
 
+/*
+DROP TRIGGER IF EXISTS nyss_changelog_detail_before_insert;
+
+DELIMITER //
+CREATE DEFINER = CURRENT_USER
+TRIGGER nyss_changelog_detail_before_insert
+BEFORE INSERT
+ON nyss_changelog_detail FOR EACH ROW
+BEGIN
+*/
+  CASE NEW.db_op
+    WHEN 'Insert' THEN SET @user_action = 'Added';
+    WHEN 'Delete' THEN SET @user_action = 'Removed';
+    ELSE SET @user_action = 'Updated';
+  END CASE;
+
   SET @entity_type = SUBSTRING_INDEX(@nyss_entity_info, CHAR(1), 1);
+
   IF @entity_type != 'Contact' THEN
-    SET @entity_info = SUBSTRING_INDEX(SUBSTRING_INDEX(@nyss_entity_info, CHAR(1), 2),CHAR(1),-1);
+    SET @entity_info = SUBSTRING_INDEX(SUBSTRING_INDEX(@nyss_entity_info, CHAR(1), 2), CHAR(1), -1);
   ELSE
     SET @entity_info = NULL;
   END IF;
+
   IF @entity_type = 'Group' THEN
     SET @group_action = SUBSTRING_INDEX(@nyss_entity_info, CHAR(1), -1);
-  ELSE
-    SET @group_action = NULL;
+    IF @group_action = 'Removed' THEN
+      SET @user_action = 'Unjoined';
+    ELSEIF @group_action = 'Added' THEN
+      SET @user_action = 'Rejoined';
+    END IF;
   END IF;
-
-  CASE NEW.db_op 
-    WHEN 'Insert' THEN SET @tmp_user_action = 'Added';
-    WHEN 'Delete' THEN SET @tmp_user_action = 'Removed';
-    ELSE
-      BEGIN
-        IF @entity_type = 'Group' AND @group_action = 'Removed' THEN 
-          SET @tmp_user_action = 'Unjoined';
-        ELSEIF @entity_type = 'Group' AND @group_action = 'Added' THEN 
-          SET @tmp_user_action = 'Rejoined';
-        ELSE
-          SET @tmp_user_action = 'Updated';
-        END IF;
-      END;
-  END CASE;
 
   SET @summary_id = NULL;
-  IF @entity_type IN ('Contact', 'Activity') THEN 
-    BEGIN 
-      SELECT id INTO @summary_id
-      FROM nyss_changelog_summary 
-      WHERE user_id = @civicrm_user_id 
-        AND conn_id = CONNECTION_ID() 
-        AND entity_type = @entity_type 
-        AND contact_id = @nyss_altered_contact_id
-      ORDER BY id DESC LIMIT 1; 
-      IF @entity_type='Contact' AND @summary_id IS NULL THEN
-        SET @nyss_session_contact_added = NULL;
-      END IF;
-    END; 
-  END IF;  
 
-  IF NEW.table_name='contact' AND NEW.db_op='INSERT' THEN
-    SET @nyss_session_contact_added = 1;
+  IF @entity_type IN ('Contact', 'Activity') THEN
+    SELECT id INTO @summary_id
+    FROM nyss_changelog_summary
+    WHERE (user_id = @civicrm_user_id OR (user_id IS NULL AND @civicrm_user_id IS NULL))
+      AND conn_id = CONNECTION_ID()
+      AND entity_type = @entity_type
+      AND contact_id = @nyss_contact_id
+    ORDER BY id DESC LIMIT 1;
   END IF;
 
-  IF NEW.table_name!='contact' AND @entity_type='Contact'  AND @summary_id IS NOT NULL THEN
-    SET @tmp_user_action='Updated';
-  END IF;
-
-  IF @summary_id IS NULL THEN 
-    BEGIN 
+  IF @summary_id IS NULL THEN
+    BEGIN
       INSERT INTO nyss_changelog_summary
         (conn_id, user_id, contact_id, entity_type, user_action, entity_info)
-        VALUES
-        (CONNECTION_ID(), @civicrm_user_id, @nyss_altered_contact_id,
-        @entity_type, @tmp_user_action, @entity_info);
+      VALUES
+        (CONNECTION_ID(), @civicrm_user_id, @nyss_contact_id,
+         @entity_type, @user_action, @entity_info);
       SET NEW.summary_id = LAST_INSERT_ID();
-    END; 
+    END;
   ELSE
-    BEGIN 
-      SET NEW.summary_id = @summary_id;
-  		IF @entity_type != 'Contact' OR IFNULL(@nyss_session_contact_added,0) != 1 THEN 
-  			BEGIN
-  		      UPDATE nyss_changelog_summary SET user_action = 'Updated' WHERE id = @summary_id; 
-  		   END;
-  		END IF;
-    END; 
+    SET NEW.summary_id = @summary_id;
   END IF;
+/*
+END;
+//
+DELIMITER ;
+*/
