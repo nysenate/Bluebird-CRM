bbe10c32e471be661df96a7ceec3bfacf3815576
Ken Zalewski <zalewski.senate@gmail.com>
2014-08-18 13:23:22 -0400

    Added a blank line.  Could not find another commit to piggyback this one on to.  Enjoy.

--- scripts/changelog_v2/import_06_relationship.sql
+++ scripts/changelog_v2/import_06_relationship.sql
@@ -10,6 +10,7 @@ LEFT JOIN @LOGDB@.nyss_temp_staging_relationship b
 ON a.relationship_type_id=b.id AND a.log_date BETWEEN b.log_date AND b.log_end_date
 WHERE a.log_action != 'Initialization';
 
+
 INSERT IGNORE INTO nyss_changelog_detail
   (db_op, table_name, entity_id, tmp_conn_id, tmp_user_id,
    tmp_contact_id, tmp_change_ts, tmp_entity_info)
