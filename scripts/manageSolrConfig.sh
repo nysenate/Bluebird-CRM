53a001769741ff591ea5b26c00c074f8c5abdfe9
Ken Zalewski <zalewski.senate@gmail.com>
2015-01-29 08:08:48 -0500

    Added script to manage SOLR configuration.  Added solr.url, solr.read_only, solr.direct_commit, solr.soft_commit config parameters.  Modified v155 upgrade script to utilize new SOLR management script.

---
+++ scripts/manageSolrConfig.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+#
+# manageSolrConfig.sh - Handle setup of SOLR integration
+#
+# Project: BluebirdCRM
+# Author: Ken Zalewski
+# Organization: New York State Senate
+# Date: 2015-01-28
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+readConfig=$script_dir/readConfig.sh
+drush=$script_dir/drush.sh
+
+. $script_dir/defaults.sh
+
+usage() {
+  echo "Usage: $prog [--list] [--update | --clear] [--enable-module | --disable-module] [--reindex] [--bluebird-setup | -bs] instanceName" >&2
+}
+
+if [ $# -lt 1 ]; then
+  usage
+  exit 1
+fi
+
+op=list
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --list) op=list ;;
+    --update) op=update;;
+    --clear) op=clear ;;
+    --en*) op=enable ;;
+    --dis*) op=disable ;;
+    --rein*) op=reindex ;;
+    --bluebird-setup|-bs) op=bs ;;
+    -*) echo "$prog: $1: Invalid option" >&2; usage; exit 1 ;;
+    *) instance="$1" ;;
+  esac
+  shift
+done
+
+
+if [ ! "$instance" ]; then
+  echo "$prog: Must specify an instance to manage" >&2
+  usage
+  exit 1
+elif ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+app_rootdir=`$readConfig --ig $instance app.rootdir` || app_rootdir="$DEFAULT_APP_ROOTDIR"
+solr_url=`$readConfig --ig $instance solr.url` || solr_url="$DEFAULT_SOLR_URL"
+solr_ro=`$readConfig --ig $instance solr.read_only` || solr_ro=0
+solr_dc=`$readConfig --ig $instance solr.direct_commit` || solr_dc=1
+solr_sc=`$readConfig --ig $instance solr.soft_commit` || solr_sc=1
+
+if [ $op = list ]; then
+  sql="SELECT * FROM apachesolr_environment;"
+  solr_env=`$execSql $instance -c "$sql" --drupal -q`
+  sql="SELECT * FROM apachesolr_environment_variable;"
+  solr_env_var=`$execSql $instance -c "$sql" --drupal -q`
+  sql="SELECT * FROM apachesolr_index_bundles;"
+  solr_index_bundles=`$execSql $instance -c "$sql" --drupal -q`
+  sql="SELECT * FROM variable where name like 'apachesolr%';"
+  solr_var=`$execSql $instance -c "$sql" --drupal -q`
+  echo "Apache SOLR environment:"
+  echo "$solr_env"
+  echo "Apache SOLR environment variables:"
+  echo "$solr_env_var"
+  echo "Apache SOLR index bundles:"
+  echo "$solr_index_bundles"
+  echo "Apache SOLR settings in the Drupal variable table:"
+  echo "$solr_var"
+fi
+
+if [ $op = enable -o $op = bs ]; then
+  echo "Enabling apachesolr and apachesolr_civiAttachments modules"
+  $drush $instance en -y apachesolr apachesolr_civiAttachments
+elif [ $op = disable ]; then
+  echo "Disabling apachesolr and apachesolr_civiAttachments modules"
+  $drush $instance dis -y apachesolr apachesolr_civiAttachments
+fi
+
+if [ $op = update -o $op = bs ]; then
+  echo "Storing configuration data into apachesolr tables"
+  sql="TRUNCATE TABLE apachesolr_environment;
+       INSERT INTO apachesolr_environment (env_id, name, url)
+       VALUES ('solr', 'Apache Solr server', '$solr_url');
+       TRUNCATE TABLE apachesolr_environment_variable;
+       INSERT INTO apachesolr_environment_variable (env_id, name, value)
+       VALUES ('solr', 'apachesolr_read_only', 's:1:\"$solr_ro\";'),
+              ('solr', 'apachesolr_direct_commit', 'i:$solr_dc;'),
+              ('solr', 'apachesolr_soft_commit', 'i:$solr_sc;');
+       TRUNCATE TABLE apachesolr_index_bundles;
+       INSERT INTO apachesolr_index_bundles (env_id, entity_type, bundle)
+       VALUES ('solr', 'civiFile', 'civiFile');"
+  $execSql $instance -c "$sql" --drupal -q
+  $drush $instance vset apachesolr_civiAttachments_extract_using solr
+elif [ $op = clear ]; then
+  echo "Clearing apachesolr tables"
+  sql="TRUNCATE TABLE apachesolr_environment;
+       TRUNCATE TABLE apachesolr_environment_variable;
+       TRUNCATE TABLE apachesolr_index_bundles;"
+  $execSql $instance -c "$sql" --drupal -q
+fi
+
+if [ $op = reindex -o $op = bs ]; then
+  echo "Archiving orphaned attachments"
+  php $app_rootdir/civicrm/scripts/fileCleanup.php -S$instance --action=archive
+  echo "Marking all attachments for processing"
+  $drush $instance solr-mark-all
+  echo "Indexing all attachments"
+  $drush $instance solr-index
+fi
+
+exit $?
--- scripts/v155.sh
+++ scripts/v155.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #
-# v1543.sh
+# v155.sh
 #
 # Project: BluebirdCRM
 # Authors: Brian Shaughnessy and Ken Zalewski
 # Organization: New York State Senate
 # Date: 2014-12-16
+# Revised: 2015-01-29
 #
 
 prog=`basename $0`
 script_dir=`dirname $0`
 execSql=$script_dir/execSql.sh
 readConfig=$script_dir/readConfig.sh
-drush=$script_dir/drush.sh
 
 . $script_dir/defaults.sh
 
@@ -28,51 +28,7 @@ if ! $readConfig --instance $instance --quiet; then
   exit 1
 fi
 
-app_rootdir=`$readConfig --ig $instance app.rootdir` || app_rootdir="$DEFAULT_APP_ROOTDIR"
+echo "$prog: Setting up SOLR"
+$script_dir/manageSolrConfig.sh $instance --bluebird-setup
+echo "$prog: Done with SOLR setup"
 
-#determine which solr core to use
-envHost=`$readConfig --global db.host`
-if [ $envHost == "crmdbprod" ]; then
-  echo "implementing on production environment..."
-  solrCore="BluebirdCRM_Prod"
-else
-  echo "implementing on dev/test/staging environment..."
-  solrCore="BluebirdCRM_Dev"
-fi
-
-echo "implementing solr attachment search..."
-
-$drush $instance en apachesolr -y
-$drush $instance en apachesolr_civiAttachments -y
-
-sql="
-  TRUNCATE TABLE apachesolr_environment;
-  INSERT INTO apachesolr_environment (env_id, name, url, service_class)
-  VALUES
-    ('solr', 'Apache Solr server', 'http://doral.nysenate.gov:8080/solr/$solrCore', '');
-
-  TRUNCATE TABLE apachesolr_environment_variable;
-  INSERT INTO apachesolr_environment_variable (env_id, name, value)
-  VALUES
-    ('solr', 'apachesolr_direct_commit', 0x693a313b),
-    ('solr', 'apachesolr_read_only', 0x733a313a2230223b),
-    ('solr', 'apachesolr_soft_commit', 0x693a313b);
-
-  TRUNCATE TABLE apachesolr_index_bundles;
-  INSERT INTO apachesolr_index_bundles (env_id, entity_type, bundle)
-  VALUES
-    ('solr', 'civiFile', 'civiFile');
-"
-$execSql $instance -c "$sql" --drupal -q
-
-#add civi attachment variables
-$drush $instance vset apachesolr_civiAttachments_extract_using solr
-
-echo "finished configuring solr. now cleanup file attachments..."
-php $app_rootdir/civicrm/scripts/fileCleanup.php -S $instance --action=archive
-
-echo "mark all attachments for processing and begin indexing..."
-$drush $instance solr-mark-all
-$drush $instance solr-index
-
-echo "finished solr setup."
