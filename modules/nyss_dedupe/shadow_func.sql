d1a71de57d8ba7179bdbb50a67ca58e86cd1241e
Ken Zalewski <zalewski.senate@gmail.com>
2017-01-04 13:36:42 -0500

    Move functions and triggers into separate SQL file.  Rename table creation script.

--- modules/nyss_dedupe/shadow_func.sql
+++ modules/nyss_dedupe/shadow_func.sql
@@ -1,52 +1,4 @@
--- Deletes aren't cascaded (possibly a good thing) so we can't use foreign keys
--- here without running into referential integrity issues or rewriting a bunch
--- of civicrm core code...
-
--- --------------------------------------------
--- CREATE shadow tables
--- --------------------------------------------
-DROP TABLE IF EXISTS shadow_contact;
-CREATE TABLE shadow_contact (
-    contact_id int(10) unsigned PRIMARY KEY,
-    first_name varchar(255),
-    middle_name varchar(255),
-    last_name varchar(255),
-    household_name varchar(255),
-    organization_name varchar(255),
-    suffix_id varchar(255),
-    birth_date date,
-    gender_id int(10) unsigned,
-    contact_type varchar(255),
-    INDEX (first_name, last_name, middle_name),
-    INDEX (last_name),
-    INDEX (gender_id),
-    INDEX (contact_type),
-    INDEX (household_name),
-    INDEX (organization_name),
-    INDEX (birth_date)
-) ENGINE=InnoDB;
-
-DROP TABLE IF EXISTS shadow_address;
-CREATE TABLE shadow_address (
-    address_id int(10) unsigned PRIMARY KEY,
-    contact_id int(10) unsigned,
-    street_address varchar(255),
-    country_id int(10) unsigned,
-    state_province_id int(10) unsigned,
-    supplemental_address_1 varchar(255),
-    supplemental_address_2 varchar(255),
-    postal_code varchar(255),
-    city varchar(255),
-    INDEX (street_address),
-    INDEX (supplemental_address_1),
-    INDEX (supplemental_address_2),
-    INDEX (country_id),
-    INDEX (state_province_id),
-    INDEX (postal_code),
-    INDEX (city),
-    INDEX (contact_id)
-) ENGINE=InnoDB;
-
+-- Functions and triggers for the shadow tables
 
 -- Change the delimiter to make stored triggers/functions easier to write!
 DELIMITER |
@@ -55,9 +7,10 @@ DELIMITER |
 -- Stored Utility Functions
 -- -----------------------------
 
+SET NAMES UTF8 COLLATE utf8_unicode_ci;
 DROP FUNCTION IF EXISTS BB_ADDR_REPLACE |
-CREATE FUNCTION BB_ADDR_REPLACE (address varchar(255))
-    RETURNS varchar(255) DETERMINISTIC
+CREATE FUNCTION BB_ADDR_REPLACE (address varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci)
+    RETURNS varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DETERMINISTIC
 
     BEGIN
         -- Start with the first alpha word occurance and loop
@@ -170,6 +123,7 @@ CREATE FUNCTION BB_NORMALIZE_ADDR (value VARCHAR(255))
     END
 |
 
+
 -- --------------------------------
 -- Triggers for first name groups
 -- --------------------------------
@@ -272,5 +226,6 @@ CREATE TRIGGER shadow_contact_delete_fn_trigger AFTER DELETE ON shadow_contact
         DELETE FROM fn_group_contact WHERE contact_id = OLD.contact_id;
     END
 |
+
 
 DELIMITER ;
