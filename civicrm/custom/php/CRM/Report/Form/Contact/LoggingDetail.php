13aee04c8b5d589d51ee049f7d75732c31465c3a
Brian Shaughnessy <brian@lcdservices.biz>
2017-11-28 00:21:20 -0500

    fixes #11531 restore much of the native logging report tool

--- civicrm/custom/php/CRM/Report/Form/Contact/LoggingDetail.php
+++ civicrm/custom/php/CRM/Report/Form/Contact/LoggingDetail.php
@@ -36,30 +36,10 @@ class CRM_Report_Form_Contact_LoggingDetail extends CRM_Logging_ReportDetail {
   /**
    */
   public function __construct() {
-    //NYSS
-    /* Old table-search method relied on this list of tables
-    $logging = new CRM_Logging_Schema();
-    $this->tables[] = 'civicrm_contact';
-    $this->tables = array_merge($this->tables, array_keys($logging->customDataLogTables()));
-    $this->tables[] = 'civicrm_email';
-    $this->tables[] = 'civicrm_phone';
-    $this->tables[] = 'civicrm_im';
-    $this->tables[] = 'civicrm_openid';
-    $this->tables[] = 'civicrm_website';
-    $this->tables[] = 'civicrm_address';
-    $this->tables[] = 'civicrm_note';
-    $this->tables[] = 'civicrm_relationship';
-    $this->tables[] = 'civicrm_activity';
-    $this->tables[] = 'civicrm_case';
-
-    // allow tables to be extended by report hook query objects
-    CRM_Report_BAO_Hook::singleton()->alterLogTables($this, $this->tables);
-    */
-
-    // NYSS report no longer searches all tables..  pull table manifest directly from nyss_changelog_detail
-    $this->summary_id = CRM_Utils_Request::retrieve('summary_id', 'Integer', CRM_Core_DAO::$_nullObject);
-    $this->populateTables();
-
+    $this->log_conn_id = CRM_Utils_Request::retrieve('log_conn_id', 'String');
+    $this->log_date = CRM_Utils_Request::retrieve('log_date', 'String');
+    $this->setTablesToContactRelatedTables();
+    $this->calculateContactDiffs();
     $this->detail = 'logging/contact/detail';
     $this->summary = 'logging/contact/summary';
 
@@ -81,7 +61,7 @@ class CRM_Report_Form_Contact_LoggingDetail extends CRM_Logging_ReportDetail {
       // link back to summary report
       //NYSS preserve summary instance source
       $instanceID = CRM_Utils_Request::retrieve('instanceID', 'Integer');
-      if ( $instanceID ) {
+      if ($instanceID) {
         $backURL = CRM_Utils_System::url('civicrm/report/instance/'.$instanceID, "reset=1", false, null, false);
       }
       else {
@@ -91,35 +71,4 @@ class CRM_Report_Form_Contact_LoggingDetail extends CRM_Logging_ReportDetail {
     }
   }
 
-  //NYSS ? 
-  function buildRows($sql, &$rows) {
-    if (empty($rows)) {
-      $rows = array();
-    }
-
-    // if no summary_id was passed or if $this->tables is blank, no rows can be found
-    if (!($this->summary_id && $this->tables)) {
-      return;
-    }
-
-    foreach ($this->tables as $table) {
-      $rows = array_merge($rows, $this->diffsInTable($table));
-    }
-  }
-
-  // populate $this->tables based on a query to nyss_changelog_detail
-  // i.e., setting up to search only the tables relevant to this change
-  function populateTables($summary_id = NULL) {
-    if (!$summary_id) { $summary_id = $this->summary_id; }
-    $this->tables = array();
-    if ($summary_id) {
-      // we need to add the standard 'civicrm_' prefix.  The 'log_' prefix comes later
-      $sql = "SELECT DISTINCT CONCAT('civicrm_', table_name) as table_name " .
-             "FROM nyss_changelog_detail WHERE summary_id = %1";
-      $rows = CRM_Core_DAO::executeQuery($sql, array( 1=>array($summary_id, 'Integer') ));
-      while ($rows->fetch()) {
-        $this->tables[] = $rows->table_name;
-      }
-    }
-  }
 }
