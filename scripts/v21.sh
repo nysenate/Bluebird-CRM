215fccf4b306c08b2e78ad71c7763251e434f63d
Ken Zalewski <zalewski.senate@gmail.com>
2017-12-28 11:49:11 -0500

    Removed unused variables

--- scripts/v21.sh
+++ scripts/v21.sh
@@ -12,8 +12,6 @@ prog=`basename $0`
 script_dir=`dirname $0`
 execSql=$script_dir/execSql.sh
 readConfig=$script_dir/readConfig.sh
-drush=$script_dir/drush.sh
-app_rootdir=`$readConfig --ig $instance app.rootdir` || app_rootdir="$DEFAULT_APP_ROOTDIR"
 
 . $script_dir/defaults.sh
 
@@ -28,16 +26,6 @@ if ! $readConfig --instance $instance --quiet; then
   echo "$prog: $instance: Instance not found in config file" >&2
   exit 1
 fi
-
-data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT_DATA_ROOTDIR"
-app_rootdir=`$readConfig --ig $instance app.rootdir` || app_rootdir="$DEFAULT_APP_ROOTDIR"
-webdir=`$readConfig --global drupal.rootdir` || webdir="$DEFAULT_DRUPAL_ROOTDIR"
-base_domain=`$readConfig --ig $instance base.domain` || base_domain="$DEFAULT_BASE_DOMAIN"
-db_basename=`$readConfig --ig $instance db.basename` || db_basename="$instance"
-civi_db_prefix=`$readConfig --ig $instance db.civicrm.prefix` || civi_db_prefix="$DEFAULT_BASE_DOMAIN"
-log_db_prefix=`$readConfig --ig $instance db.log.prefix` || log_db_prefix="$DEFAULT_BASE_DOMAIN"
-cdb="$civi_db_prefix$db_basename"
-ldb=$log_db_prefix$db_basename;
 
 ## migrate messages matched_to and activity_id column to separate table
 echo "$prog: create messages_matched table"
