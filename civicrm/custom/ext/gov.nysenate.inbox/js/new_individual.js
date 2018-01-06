fcc968f31244eb3a47001c825d9fab358a1c4224
Brian Shaughnessy <brian@lcdservices.biz>
2018-01-06 17:22:35 -0500

    refs #11625 tweak js to avoid error on other uses of form

--- civicrm/custom/ext/gov.nysenate.inbox/js/new_individual.js
+++ civicrm/custom/ext/gov.nysenate.inbox/js/new_individual.js
@@ -1,6 +1,6 @@
 CRM.$(function($) {
   var message = $('div.message-body').html();
-  if (message.length > 0) {
+  if (message.length) {
     $('div.crm-profile-name-new_individual div.crm-submit-buttons').before('<div class="crm-accordion-wrapper crm-accordion collapsed new-indiv-message-details">\n' +
       '<div class="crm-accordion-header">Message Details</div>\n' +
       '<div class="crm-accordion-body">\n' +
