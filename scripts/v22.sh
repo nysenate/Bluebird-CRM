84ef9bd1d012acdf94a37e72ca0d207d55d308ed
Brian Shaughnessy <brian@lcdservices.biz>
2018-05-15 10:36:14 -0400

    fixes #8034 constituents name to activity subject

---
+++ scripts/v22.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# v22.sh
+#
+# Project: BluebirdCRM
+# Authors: Brian Shaughnessy and Ken Zalewski
+# Organization: New York State Senate
+# Date: 2018-05-15
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+drush=$script_dir/drush.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+## 8034
+echo "$prog: alter activity assignment email subject"
+sql="
+  UPDATE civicrm_msg_template
+  SET msg_subject = '[Bluebird] Constituent Activity: {contact.display_name}'
+  WHERE msg_title = 'Cases - Send Copy of an Activity';
+"
+$execSql $instance -c "$sql" -q
+
+## record completion
+echo "$prog: upgrade process is complete."
