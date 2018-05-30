b4309b17de6df7d5938806d3ba6946055284bb8f
Brian Shaughnessy <brian@lcdservices.biz>
2018-05-30 05:29:34 -0400

    fixes #11725 user edit layout mods

--- scripts/v22.sh
+++ scripts/v22.sh
@@ -41,5 +41,30 @@ $execSql $instance -c "$sql" -q
 echo "$prog: install activity extension"
 $drush $instance cvapi extension.install key=gov.nysenate.activity --quiet
 
+## 11725
+echo "$prog: update role sort order"
+sql="
+  UPDATE role SET weight = 51 WHERE role.rid = 4;
+  UPDATE role SET weight = 31 WHERE role.rid = 8;
+  UPDATE role SET weight = 0 WHERE role.rid = 1;
+  UPDATE role SET weight = 0 WHERE role.rid = 2;
+  UPDATE role SET weight = 32 WHERE role.rid = 5;
+  UPDATE role SET weight = 4 WHERE role.rid = 12;
+  UPDATE role SET weight = 11 WHERE role.rid = 16;
+  UPDATE role SET weight = 12 WHERE role.rid = 14;
+  UPDATE role SET weight = 13 WHERE role.rid = 15;
+  UPDATE role SET weight = 14 WHERE role.rid = 17;
+  UPDATE role SET weight = 21 WHERE role.rid = 19;
+  UPDATE role SET weight = 1 WHERE role.rid = 9;
+  UPDATE role SET weight = 2 WHERE role.rid = 10;
+  UPDATE role SET weight = 33 WHERE role.rid = 7;
+  UPDATE role SET weight = 52 WHERE role.rid = 18;
+  UPDATE role SET weight = 34 WHERE role.rid = 6;
+  UPDATE role SET weight = 3 WHERE role.rid = 11;
+  UPDATE role SET weight = 53 WHERE role.rid = 3;
+  UPDATE role SET weight = 5 WHERE role.rid = 13;
+"
+$execSql -i $instance -c "$sql" --drupal -q
+
 ## record completion
 echo "$prog: upgrade process is complete."
