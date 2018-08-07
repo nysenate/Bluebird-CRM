5e8b7a21da44a2f7dd00ac0e902e831dc99456b6
Brian Shaughnessy <brian@lcdservices.biz>
2018-08-07 09:01:41 -0400

    refs #12109, 12090 case performance

---
+++ scripts/v221.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# v221.sh
+#
+# Project: BluebirdCRM
+# Authors: Brian Shaughnessy and Ken Zalewski
+# Organization: New York State Senate
+# Date: 2018-08-07
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+drush=$script_dir/drush.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+## 12109
+echo "$prog: cleanup duplicate entity_file records"
+sql="
+  DELETE ef1
+  FROM civicrm_entity_file ef1
+  INNER JOIN civicrm_entity_file ef2
+  WHERE ef1.id > ef2.id
+    AND ef1.entity_table = ef2.entity_table
+    AND ef1.entity_id = ef2.entity_id
+    AND ef1.file_id = ef2.file_id;
+"
+$execSql $instance -c "$sql" -q
+
+## record completion
+echo "$prog: upgrade process is complete."
