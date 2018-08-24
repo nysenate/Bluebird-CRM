195f55c1d6fb5a609321485ec00037b7310e0663
Brian Shaughnessy <brian@lcdservices.biz>
2018-08-24 17:18:57 -0400

    ui: add title to logout icon

--- civicrm/custom/ext/gov.nysenate.navigation/js/navigation.js
+++ civicrm/custom/ext/gov.nysenate.navigation/js/navigation.js
@@ -2,4 +2,7 @@ CRM.$(function($) {
   //replace home link
   $('li.menumain.crm-link-home').html('<a href="/civicrm/dashboard?reset=1"><i class="nyss-i fa-home"></i></a>');
   $('a.crm-logout-link').parents('ul.innerbox').remove();
+
+  //add title to logout
+  $('i.fa-sign-out').prop('title', 'Log Out');
 });
