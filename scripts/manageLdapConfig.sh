b9238f05ac6ff15815a109d368c819caba1c9a13
Ken Zalewski <zalewski.senate@gmail.com>
2018-10-16 01:48:25 -0400

    Add support for 10 new LDAP configuration parameters.  Add --set-server option to set all LDAP server parameters in a single call.  refs #12157

--- scripts/manageLdapConfig.sh
+++ scripts/manageLdapConfig.sh
@@ -6,8 +6,8 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-12-02
-# Revised: 2013-06-21
-# Revised: 2013-11-08 - 
+# Revised: 2013-06-21 and 2013-11-08
+# Revised: 2018-10-03 - Add more parameters to support ActiveDirectory LDAP
 #
 
 prog=`basename $0`
@@ -16,6 +16,27 @@ readConfig=$script_dir/readConfig.sh
 
 . $script_dir/defaults.sh
 
+map_param_to_field() {
+  p="$1"
+  if [ "$p" = "host" ]; then
+    fld="address"
+  elif [ "$p" = "type" ]; then
+    fld="ldap_type"
+  elif [ "$p" = "anonymous" ]; then
+    fld="bind_method"
+  elif [ "$p" = "user" ]; then
+    fld="binddn"
+  elif [ "$p" = "pass" ]; then
+    fld="bindpw"
+  elif [ "$p" = "group_class" ]; then
+    fld="group_object_category"
+  else
+    # port, basedn, user_attr, mail_attr
+    fld="$p"
+  fi
+  echo "$fld"
+}
+
 usage() {
   echo "Usage: $prog [cmd [param]] instanceName" >&2
   echo "  where [cmd] is one of:" >&2
@@ -23,8 +44,10 @@ usage() {
   echo "    --list-all, --list-server, --list-authentication" >&2
   echo "    --list-authorization --list-entries, --list-mappings" >&2
   echo "    --set-name {name}, --set-host {host}, --set-port {port}" >&2
+  echo "    --set-server {serverParams}" >&2
   echo "    --set-entries {groupList}, --set-mappings {mappingList}" >&2
   echo "    --set-php-auth {phpSnippet}" >&2
+  echo "  {serverParams} is of the form param1=value1 | param2=value2 | ..." >&2
   echo "  {groupList} is of the form ldapGroup1,ldapGroup2,..." >&2
   echo "  {mappingList} is of the form ldapGroup1|role1, ldapGroup2|role2, ..." >&2
 }
@@ -50,6 +73,7 @@ while [ $# -gt 0 ]; do
     --set-name|-sn) shift; cmd=setName; param="$1" ;;
     --set-host|-sh) shift; cmd=setHost; param="$1" ;;
     --set-port|-sp) shift; cmd=setPort; param="$1" ;;
+    --set-server|-ss) shift; cmd=setServer; param="$1" ;;
     --set-entries|-se) shift; cmd=setEntries; param="$1" ;;
     --set-mappings|-sm) shift; cmd=setMappings; param="$1" ;;
     --set-php-auth|-spa) shift; cmd=setPhpAuth; param="$1" ;;
@@ -70,9 +94,27 @@ elif ! $readConfig --instance $instance --quiet; then
 fi
 
 if [ "$cmd" = "setup" ]; then
+  ldap_server=""
+  for p in host port type anonymous user pass basedn user_attr mail_attr group_class; do
+    fld=`map_param_to_field $p`
+    val=`$readConfig --ig $instance ldap.$p`
+    if [ "$p" = "anonymous" ]; then
+      [ "$val" = "true" -o "$val" = "1" ] && bind_method=3 || bind_method=1
+      ldap_server="$ldap_server|$fld=$bind_method"
+    elif [ "$val" ]; then
+      ldap_server="$ldap_server|$fld=$val"
+    fi
+  done
+
+  ldap_server=`echo "$ldap_server" | cut -c2-`
   ldap_entries=`$readConfig --ig $instance ldap.entries`
   ldap_mappings=`$readConfig --ig $instance ldap.mappings`
+
+  echo "Setting LDAP server parameters: $ldap_server"
+  $0 --set-server "$ldap_server" $instance
+  echo "Setting LDAP entries: $ldap_entries"
   $0 --set-entries "$ldap_entries" $instance
+  echo "Setting LDAP mappings: $ldap_mappings"
   $0 --set-mappings "$ldap_mappings" $instance
 else
   php "$script_dir/manageLdapConfig.php" "$instance" "$cmd" "$param"
