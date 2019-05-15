eb9afa64db4705053a0ca7449fc1b888f4d95204
Brian Shaughnessy <brian@lcdservices.biz>
2019-05-15 10:32:31 -0400

    civicrm 5.13.3 core additional files

---
+++ modules/civicrm/ext/api4/tests/phpunit/Mock/MockV4ReflectionChild.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Civi\Test\Api4\Mock;
+
+/**
+ * @inheritDoc
+ */
+class MockV4ReflectionChild extends MockV4ReflectionBase {
+  /**
+   * @inheritDoc
+   *
+   * In the child class, foo has been barred.
+   */
+  public $foo = ['bar' => 1];
+
+}
---
+++ modules/civicrm/ext/api4/tests/phpunit/Mock/MockV4ReflectionGrandchild.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Civi\Test\Api4\Mock;
+
+
+/**
+ * Grandchild class
+ *
+ * This is an extended description.
+ *
+ * There is a line break in this description.
+ *
+ * @inheritdoc
+ */
+class MockV4ReflectionGrandchild extends MockV4ReflectionChild {
+
+}
