b2c66311c071230f91570af30e3d5b3c11643003
Brian Shaughnessy <brian@lcdservices.biz>
2019-08-26 18:04:19 -0400

    fixes #12871 adjust fn_group.given column size

---
+++ scripts/v226.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# v226.sh
+#
+# Project: BluebirdCRM
+# Authors: Brian Shaughnessy and Ken Zalewski
+# Organization: New York State Senate
+# Date: 2019-08-26
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+drush=$script_dir/drush.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+echo "$prog: Starting v2.2.6 upgrade process"
+
+## 12639
+echo "$prog: nyss #12871 - alter fn_group.given"
+sql="
+  ALTER TABLE fn_group
+  MODIFY COLUMN given VARCHAR(64);
+"
+$execSql $instance -c "$sql" -q
+
+## record completion
+echo "$prog: Finished the v2.2.6 upgrade process"
