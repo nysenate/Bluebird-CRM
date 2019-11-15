44bcc34bfb5138f0e4d0efbe6b0888a6da2800fb
Brian Shaughnessy <brian@lcdservices.biz>
2019-11-15 13:45:51 -0500

    fixes #13121 add tag text

--- civicrm/custom/ext/gov.nysenate.tags/js/page_manage_tags.js
+++ civicrm/custom/ext/gov.nysenate.tags/js/page_manage_tags.js
@@ -3,8 +3,19 @@ CRM.$(function($) {
     //open tag tree main parent on load
     var interval_id = setInterval(function(){
       if ($('li#291').length != 0){
-        clearInterval(interval_id)
-        $('div.tag-tree').jstree('open_node', $('li#291'))
+        clearInterval(interval_id);
+        $('div.tag-tree').jstree('open_node', $('li#291'));
+      }
+    }, 5);
+
+    //13121
+    var intKeywords = setInterval(function(){
+      if ($('div#tagset-296 div.crm-submit-buttons span').length != 0) {
+        $('div#tagset-296 div.crm-submit-buttons span').each(function () {
+          var html = $(this).html();
+          $(this).html(html.replace('Add Tag', 'Add Keyword'));
+        });
+        clearInterval(intKeywords);
       }
     }, 5);
   });
