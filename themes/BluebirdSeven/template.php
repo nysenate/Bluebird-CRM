0cb87f7bd18dd145963062f99c73d61ed47eaeef
Ken Zalewski <zalewski.senate@gmail.com>
2019-12-06 15:54:32 -0500

    Fix some PHP notices and warnings regarding undefined indices and variables.

--- themes/BluebirdSeven/template.php
+++ themes/BluebirdSeven/template.php
@@ -3,38 +3,41 @@
 /**
  * Get rid of Home in breadcrumb trail.
  */
-function BluebirdSeven_breadcrumb($variables) {
+function BluebirdSeven_breadcrumb($variables)
+{
   $breadcrumb = $variables['breadcrumb'];
 
   if (!empty($breadcrumb)) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-
     array_shift($breadcrumb); // Removes the Home item
     $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
-
     return $output;
   }
 
-  return NULL;
-}
+  return null;
+} // BluebirdSeven_breadcrumb()
+
 
 //unset login failed message
-function BluebirdSeven_page_alter(&$page) {
-  $errors = drupal_get_messages('error');
-  foreach($errors['error'] as $error) {
-    if(strpos($error, 'Sorry, unrecognized username or password') === FALSE &&
-      strpos($error, 'Sorry, there have been more than 5 failed login attempts') === FALSE
-    ) {
-      drupal_set_message($error, 'error');
+function BluebirdSeven_page_alter(&$page)
+{
+  $msgs = drupal_get_messages('error');
+  if (isset($msgs['error']) && is_array($msgs['error'])) {
+    foreach ($msgs['error'] as $error) {
+      if (strpos($error, 'Sorry, unrecognized username or password') === false
+        && strpos($error, 'Sorry, there have been more than 5 failed login attempts') === false) {
+        drupal_set_message($error, 'error');
+      }
     }
   }
-
   //echo '<pre>';print_r($page);echo $title;echo '</pre>';
-}
+} // BluebirdSeven_page_alter()
 
-function BluebirdSeven_preprocess_page(&$vars) {
+
+function BluebirdSeven_preprocess_page(&$vars)
+{
   //echo '<pre>';print_r($vars);echo '</pre>';
 
   //set title when no roles
