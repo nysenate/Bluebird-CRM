61b277907e83edde1ef297c9679769a3b4df09f6
Ken Zalewski <zalewski.senate@gmail.com>
2020-01-28 13:35:31 -0500

    Enforce unique values for each of the two new activity types.  Calculate max(value) correctly by using a cast.  Check for activity type existence, rather than using a delete-then-insert methodology.  fixes #13251

--- scripts/v302.sh
+++ scripts/v302.sh
@@ -29,19 +29,28 @@ if ! $readConfig --instance $instance --quiet; then
 fi
 
 echo "$prog: 13251 new activity types"
-sql="
-  SELECT @optGroup := id FROM civicrm_option_group WHERE name = 'activity_type';
-  SELECT @maxVal := max(value) FROM civicrm_option_value WHERE option_group_id = @optGroup;
-  DELETE FROM civicrm_option_value
-  WHERE option_group_id = @optGroup
-    AND (name = 'Letter of Support' OR name = 'Letter of Opposition');
-  INSERT INTO civicrm_option_value
-  (id, option_group_id, label, value, name, grouping, filter, is_default, weight, description, is_optgroup, is_reserved, is_active, component_id, domain_id, visibility_id)
-  VALUES
-  (NULL, @optGroup, 'Letter of Support', @maxVal + 1, 'Letter of Support', NULL, '0', NULL, @maxVal + 1, NULL, '0', '0', '1', NULL, NULL, NULL),
-  (NULL, @optGroup, 'Letter of Opposition', @maxVal + 1, 'Letter of Opposition', NULL, '0', NULL, @maxVal + 1, NULL, '0', '0', '1', NULL, NULL, NULL);
-"
-$execSql $instance -c "$sql" -q
+for act_type in "Letter of Support" "Letter of Opposition"; do
+  sql="SELECT id FROM civicrm_option_value
+       WHERE name='$act_type'
+         AND option_group_id = (SELECT id FROM civicrm_option_group
+                                WHERE name='activity_type')"
+  id=`$execSql $instance -q -c "$sql"`
+  if [ "$id" ]; then
+    echo "$prog: Activity type [$act_type] already exists"
+  else
+    echo "$prog: Creating new activity type [$act_type]"
+    sql="
+      SELECT @optGroup := id FROM civicrm_option_group WHERE name = 'activity_type';
+      SELECT @maxVal := max(cast(value as unsigned)) FROM civicrm_option_value WHERE option_group_id = @optGroup;
+      INSERT INTO civicrm_option_value
+      (id, option_group_id, label, value, name, grouping, filter, is_default, weight, description, is_optgroup, is_reserved, is_active, component_id, domain_id, visibility_id)
+      VALUES
+      (NULL, @optGroup, '$act_type', @maxVal + 1, '$act_type', NULL, '0', NULL, @maxVal + 1, NULL, '0', '0', '1', NULL, NULL, NULL);
+    "
+    $execSql $instance -q -c "$sql"
+  fi
+done
+
 
 echo "$prog: #13259 update non-admin menu block"
 sql="
