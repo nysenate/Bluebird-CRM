25378457056516a4e2419383cade554cc22b18fa
Brian Shaughnessy <brian@lcdservices.biz>
2020-02-26 19:34:59 -0500

    fixes #13254 quicksearch help tweaks

--- civicrm/custom/ext/gov.nysenate.resources/js/menuBar.js
+++ civicrm/custom/ext/gov.nysenate.resources/js/menuBar.js
@@ -3,11 +3,19 @@ CRM.$(function($) {
     if ($('li#crm-qsearch input[value=sort_name]').length) {
       $('li#crm-qsearch input[value=sort_name]')
         .parents('label')
-        .append('<i class="fa-lightbulb-o crm-i nyss-qsearch-help" id="nyss-qsearch-help-sort_name" />'
+        .append('<i class="fa-question crm-i nyss-qsearch-help" id="nyss-qsearch-help-sort_name" />'
       );
 
       $('#nyss-qsearch-help-sort_name').hover(function(){
-        CRM.alert('Use the Name option to search individual, organization, or household names. When searching for an individual, the sort name will be "last name, first name." For example, to search for John Doe you could begin by typing "doe, j" -- and continue typing additional letters as necessary to refine the results further.', 'Quicksearch Name Option', 'info', {unique:true, expires:0});
+        if ($('div.ui-notify-message h1').length && $('div.ui-notify-message h1').text() == 'Quicksearch Name Option') {
+          //message already exists; do nothing;
+        }
+        else {
+          CRM.alert('Use the Name option to search individual, organization, or household names. When searching for an individual, the sort name will be "last name, first name." For example, to search for John Doe you could begin by typing "doe, j" -- and continue typing additional letters as necessary to refine the results further.', 'Quicksearch Name Option', 'info', {
+            unique: true,
+            expires: 0
+          });
+        }
       });
     }
   });
