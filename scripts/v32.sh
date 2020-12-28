3c538826e4431354ecc523ccba6f71800aee9592
Brian Shaughnessy <brian@lcdservices.biz>
2020-12-28 12:12:56 -0500

    init v32 upgrade script

---
+++ scripts/v32.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# v32.sh
+#
+# Project: BluebirdCRM
+# Authors: Brian Shaughnessy and Ken Zalewski
+# Organization: New York State Senate
+# Date: 2020-12-28
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+execSql=$script_dir/execSql.sh
+drush=$script_dir/drush.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT_DATA_ROOTDIR"
+pubfiles_dir="$data_rootdir/$instance/pubfiles"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+## upgrade drupal db
+echo "running drupal db upgrade..."
+$drush $instance updb -y -q
+
+## upgrade civicrm db
+echo "running civicrm db upgrade..."
+$drush $instance civicrm-upgrade-db -y -q
+
+## record completion
+echo "$prog: upgrade process is complete."
