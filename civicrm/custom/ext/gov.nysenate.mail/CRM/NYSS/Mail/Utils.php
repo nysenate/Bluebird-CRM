b428b04f2997741bbade903ece67f7299f8e6b7b
Brian Shaughnessy <brian@lcdservices.biz>
2021-01-08 11:48:23 -0500

    Mosaico: create missing thumbnails; delete thumbnails with no base image

---
+++ civicrm/custom/ext/gov.nysenate.mail/CRM/NYSS/Mail/Utils.php
@@ -0,0 +1,43 @@
+<?php
+
+class CRM_NYSS_Mail_Utils {
+  /**
+   * @param false $all
+   *
+   * create missing thumbnails for Mosaico images if they don't exist
+   * delete thumbnails if there is no corresponding base file
+   * optionally recreate thumbnails for all files
+   */
+  static function createMosaicoThumbnails($all = FALSE) {
+    if (!class_exists('CRM_Mosaico_Utils')) {
+      return;
+    }
+
+    $config = CRM_Mosaico_Utils::getConfig();
+
+    //create thumbnail images if they don't exist or we are recaching all of them
+    $uploads = scandir($config['BASE_DIR'] . $config['UPLOADS_DIR']);
+    foreach ($uploads as $file_name) {
+      $file_path = $config['BASE_DIR'] . $config['UPLOADS_DIR'] . $file_name;
+
+      if (is_file($file_path)) {
+        if ($all || !file_exists($config['BASE_DIR'] . $config['THUMBNAILS_DIR'] . $file_name)) {
+          $thumbnail_path = $config['BASE_DIR'] . $config[ 'THUMBNAILS_DIR' ] . $file_name;
+          Civi::service('mosaico_graphics')->createResizedImage($file_path, $thumbnail_path, $config['THUMBNAIL_WIDTH'], $config['THUMBNAIL_HEIGHT']);
+        }
+      }
+    }
+
+    //delete thumbnail if there is no corresponding base image
+    $thumbnails = scandir($config['BASE_DIR'] . $config['THUMBNAILS_DIR']);
+    foreach ($thumbnails as $file_name) {
+      $file_path = $config['BASE_DIR'] . $config['THUMBNAILS_DIR'] . $file_name;
+
+      if (is_file($file_path) &&
+        !file_exists($config['BASE_DIR'] . $config['UPLOADS_DIR'] . $file_name)
+      ) {
+        unlink($file_path);
+      }
+    }
+  }
+}
