00603c15123aef476d7096f9bafbf466e41f3a6f
Steve Binkowski <binkowsk@nysenate.gov>
2021-01-15 12:14:10 -0500

    Update reset_links.bat for Windows installations

--- scripts/reset_links.bat
+++ scripts/reset_links.bat
@@ -24,6 +24,36 @@ if exist modules del modules
 mklink /D modules ..\..\..\modules
 echo Done.
 
+echo Linking %app_root%\drupal\sites\all\docs =^> %app_root%\civicrm\docs
+set t_root=%app_root%\drupal\sites\all
+if not exist %t_root% goto :tnotfound
+if not exist %t_root%\..\..\..\civicrm\docs goto :tnotfound
+cd %t_root%
+if exist docs\ rmdir docs
+if exist docs del docs
+mklink /D docs ..\..\..\civicrm\docs
+echo Done.
+
+echo Linking %app_root%\drupal\sites\all\ext =^> %app_root%\civicrm\custom\ext
+set t_root=%app_root%\drupal\sites\all
+if not exist %t_root% goto :tnotfound
+if not exist %t_root%\..\..\..\civicrm\custom\ext goto :tnotfound
+cd %t_root%
+if exist ext\ rmdir ext
+if exist ext del ext
+mklink /D ext ..\..\..\civicrm\custom\ext
+echo Done.
+
+echo Linking %app_root%\drupal\sites\all\mosaico =^> %app_root%\civicrm\custom\mosaico
+set t_root=%app_root%\drupal\sites\all
+if not exist %t_root% goto :tnotfound
+if not exist %t_root%\..\..\..\civicrm\custom\mosaico goto :tnotfound
+cd %t_root%
+if exist mosaico\ rmdir mosaico
+if exist mosaico del mosaico
+mklink /D mosaico ..\..\..\civicrm\custom\mosaico
+echo Done.
+
 echo Linking %app_root%\drupal\sites\default\themes =^> %app_root%\themes
 set t_root=%app_root%\drupal\sites\default
 if not exist %t_root% goto :tnotfound
