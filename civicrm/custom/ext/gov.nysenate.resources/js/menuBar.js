790a42c6816a6b718cb636c8358186fea9a45a49
Brian Shaughnessy <brian@lcdservices.biz>
2021-11-30 11:52:43 -0500

    fixes #14379 case id quick search link

--- civicrm/custom/ext/gov.nysenate.resources/js/menuBar.js
+++ civicrm/custom/ext/gov.nysenate.resources/js/menuBar.js
@@ -1,10 +1,24 @@
 CRM.$(function($) {
   $(document).ready(function(){
     //wait for element existence to attach to
-    var intervalId = setInterval(function(){
+    var sortName = setInterval(function(){
       if ($('li#crm-qsearch input[value=sort_name]').length != 0){
-        clearInterval(intervalId);
+        clearInterval(sortName);
         buildQSearchHelp();
+      }
+    }, 5);
+
+    //14379 wait for quick search results the first time
+    var caseId = setInterval(function(){
+      if ($('li#crm-qsearch input[value=case_id]').length > 0 && $('ul.crm-quickSearch-results div.ui-menu-item-uiMenuItemWrapper a').length > 0){
+        clearInterval(caseId);
+        buildCaseLink();
+
+        $('input[name=case_id]').on('input', function() {
+          setTimeout(function () {
+            buildCaseLink();
+          }, 1500);
+        });
       }
     }, 5);
 
@@ -30,6 +44,23 @@ CRM.$(function($) {
           });
         }
       });
+    }
+
+    //14379
+    function buildCaseLink() {
+      var caseId = $('input[name=case_id]').val();
+      //console.log('caseId: ', caseId);
+
+      $('ul.crm-quickSearch-results div.ui-menu-item-uiMenuItemWrapper a').each(function() {
+        var href = $(this).prop('href');
+        //console.log('href: ', href);
+
+        var urlParams = new URLSearchParams(href);
+        var cid = urlParams.get('cid');
+        //console.log('cid: ', cid);
+
+        $(this).prop('href', '/civicrm/contact/view/case?reset=1&id=' + caseId + '&cid=' + cid + '&action=view&context=case');
+      })
     }
   });
 });
