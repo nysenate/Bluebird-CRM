444e34b3059a9210c1b7f2d01e0888e5b6944bc1
Brian Shaughnessy <brian@lcdservices.biz>
2022-02-28 11:37:12 -0500

    fixes #14555 set inactive roles unchecked

---
+++ civicrm/custom/ext/gov.nysenate.case/js/CaseView.js
@@ -0,0 +1,8 @@
+CRM.$(function ($) {
+  $(document).ready(function () {
+    //14555
+    if ($('#role_inactive').is(':checked')) {
+      $('#role_inactive').prop('checked', false);
+    }
+  });
+});
