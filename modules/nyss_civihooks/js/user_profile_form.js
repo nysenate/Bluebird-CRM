0d0ddfb92a99f2f1f67a6c4d4ec626630b0b3cf4
Brian Shaughnessy <brian@lcdservices.biz>
2022-06-27 11:21:10 -0400

    fixes #13802 manage users roles UI

--- modules/nyss_civihooks/js/user_profile_form.js
+++ modules/nyss_civihooks/js/user_profile_form.js
@@ -1,4 +1,5 @@
 (function ($) {
+  //13802
   //there is contention with the roleassign module for modifying this layout
   //need to ensure the block exists before we modify with js
   var layoutRoles = function(selector, callback) {
@@ -18,9 +19,9 @@
       '<div class="nyss-roles-description">In order for a staff member to get started in Bluebird, they must first have ONE main role assigned to them.</div>' +
       $('div.form-item-roles-change-9')[0].outerHTML +
       $('div.form-item-roles-change-10')[0].outerHTML +
-      $('div.form-item-roles-change-6')[0].outerHTML +
       $('div.form-item-roles-change-11')[0].outerHTML +
       $('div.form-item-roles-change-13')[0].outerHTML +
+      $('div.form-item-roles-change-12')[0].outerHTML +
       '<div class="nyss-roles-section-header">Add-on Roles</div>' +
       '<div class="nyss-roles-description">Additional roles to extend access in specific feature areas.</div>' +
       '<div class="nyss-roles-subsection-header">Bluebird Mass Email</div>' +
@@ -30,11 +31,6 @@
       $('div.form-item-roles-change-17')[0].outerHTML +
       '<div class="nyss-roles-subsection-header">Bluebird Inbound Email</div>' +
       $('div.form-item-roles-change-19')[0].outerHTML +
-      '<div class="nyss-roles-section-header">Other Senate Office Roles</div>' +
-      '<div class="nyss-roles-description">Several Senate central staff offices have permission to access your Senator\'s Bluebird database. The roles below are used specifically for those offices.</div>' +
-      $('div.form-item-roles-change-8')[0].outerHTML +
-      $('div.form-item-roles-change-5')[0].outerHTML +
-      $('div.form-item-roles-change-12')[0].outerHTML +
       '</div>'
     ;
     //console.log('html: ', html);
