fd20366103d85f1ad723b256ef728bae68ba3514
Brian Shaughnessy <brian@lcdservices.biz>
2024-05-15 15:28:10 -0400

    upgrade mosaicoextras

--- civicrm/custom/ext/mosaicoextras/mosaicoextras.php
+++ civicrm/custom/ext/mosaicoextras/mosaicoextras.php
@@ -24,59 +24,12 @@ function mosaicoextras_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function mosaicoextras_civicrm_postInstall() {
-  _mosaicoextras_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function mosaicoextras_civicrm_uninstall() {
-  _mosaicoextras_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function mosaicoextras_civicrm_enable() {
   _mosaicoextras_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function mosaicoextras_civicrm_disable() {
-  _mosaicoextras_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function mosaicoextras_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mosaicoextras_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function mosaicoextras_civicrm_entityTypes(&$entityTypes) {
-  _mosaicoextras_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
