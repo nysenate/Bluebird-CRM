64473c30fbea9436e27c5e15b8c7ebd143fe3573
Nate Frank <nfrank@nysenate.gov>
2024-08-16 11:13:29 -0400

    stubbed out remaining event type classes

--- civicrm/custom/php/CRM/NYSS/BAO/Integration/WebsiteEvent/IssueEvent.php
+++ civicrm/custom/php/CRM/NYSS/BAO/Integration/WebsiteEvent/IssueEvent.php
@@ -1,52 +1,62 @@
 <?php
 
-namespace CRM_NYSS_BAO_Integration_WebsiteEvent;
-
+use Civi\API\Exception\UnauthorizedException;
 use CRM_Core_Exception;
-use CRM_NYSS_BAO_Integration\WebsiteEventInterface;
+use NYSS_Integration\WebsiteEvent;
+use NYSS_Integration\WebsiteEventInterface;
 use InvalidArgumentException;
 
-class IssueEvent extends BaseEvent implements WebsiteEventInterface
+class CRM_NYSS_BAO_Integration_WebsiteEvent_IssueEvent extends CRM_NYSS_BAO_Integration_WebsiteEvent implements CRM_NYSS_BAO_Integration_WebsiteEventInterface
 {
-    use FollowableEvent;
-
+    use CRM_NYSS_BAO_Integration_WebsiteEvent_FollowableEvent;
 
     const PARENT_TAG_NAME = 'Website Issues';
 
-    public function __construct(int $contact_id, string $action, array $event_info)
+    public function __construct(CRM_NYSS_BAO_Integration_WebsiteEventData $event_data)
     {
-        parent::__construct($contact_id,$action, $event_info);
+        parent::__construct($event_data);
 
         if (empty($this->getIssueName())) {
             throw new InvalidArgumentException("issue_name must be in event info.");
         }
 
-        // Get Tag
-        $this->setTagName($this->getIssueName());
-        $this->setTag($this->findTag($this->getTagName(),$this->getParentTagId(), false));
+        // Archive Table Name
+        $this->setArchiveTableName('archive_committee');
+        $this->archiveFields = [];
 
         return $this;
     }
 
     /**
-     * @inheritDoc
+     * @throws UnauthorizedException
+     * @throws \CRM_Core_Exception
      */
-    public function process(): static
-    {
+    public function process(int $contact_id): static {
+
+        if (empty($contact_id)) {
+            throw new InvalidArgumentException("Contact ID required to process event.");
+        }
+
+        parent::process($contact_id);
+
+        // Get Tag
+        $this->setTagName($this->getIssueName());
+        $this->setTag($this->findTag($this->getTagName(),$this->getParentTagId(), false));
+
         // Process Specific Action
-        switch ($this->getAction()) {
+        switch ($this->getEventAction()) {
             case self::ACTION_FOLLOW:
-                if (! $this->isFollowing()) {
-                    $this->follow();
+                if (! $this->isFollowing($contact_id)) {
+                    $this->follow($contact_id);
                 }
                 break;
             case self::ACTION_UNFOLLOW:
-                if ($this->isFollowing()) {
-                    $this->unfollow();
+                if ($this->isFollowing($contact_id)) {
+                    $this->unfollow($contact_id);
                 }
                 break;
             default:
-                throw new CRM_Core_Exception("Unable to determine bill action");
+                throw new CRM_Core_Exception("Unable to determine committee action");
         }
 
         return $this;
@@ -62,4 +72,28 @@ class IssueEvent extends BaseEvent implements WebsiteEventInterface
         return self::PARENT_TAG_NAME;
     }
 
+    public function getEventDetails(): string
+    {
+        // TODO: Implement getEventDetails() method.
+    }
+
+    public function getEventDescription(): string
+    {
+        // TODO: Implement getEventDescription() method.
+    }
+
+    public function getActivityData(): ?string
+    {
+        // TODO: Implement getActivityData() method.
+    }
+
+    public function getArchiveValues(): ?array
+    {
+        // TODO: Implement getArchiveValues() method.
+    }
+
+    public function getArchiveSQL(int $archive_id, ?string $prefix = null): string
+    {
+        // TODO: Implement getArchiveSQL() method.
+    }
 }
---
+++ civicrm/custom/php/CRM/NYSS/BAO/Integration/WebsiteEvent/PollEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NYSS\BAO\Integration\WebsiteEvent;
+
+class CRM_NYSS_BAO_Integration_WebsiteEvent_PollEvent extends \CRM_NYSS_BAO_Integration_WebsiteEvent implements \CRM_NYSS_BAO_Integration_WebsiteEventInterface
+{
+
+    function getParentTagName(): string
+    {
+        // TODO: Implement getParentTagName() method.
+    }
+
+    public function getEventDetails(): string
+    {
+        // TODO: Implement getEventDetails() method.
+    }
+
+    public function getEventDescription(): string
+    {
+        // TODO: Implement getEventDescription() method.
+    }
+
+    public function getActivityData(): ?string
+    {
+        // TODO: Implement getActivityData() method.
+    }
+
+    public function getArchiveValues(): ?array
+    {
+        // TODO: Implement getArchiveValues() method.
+    }
+
+    public function getArchiveSQL(int $archive_id, ?string $prefix = null): string
+    {
+        // TODO: Implement getArchiveSQL() method.
+    }
+}
