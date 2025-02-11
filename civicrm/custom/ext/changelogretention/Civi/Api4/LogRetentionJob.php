f08247755f3eb50dacd8a64a83d8760cf90a36c8
djnatedagreat <nate@greatdayatwork.com>
2025-02-11 07:28:22 -0500

    implement LCD extension for purging old log table entries -- refs #16897 (#111)
    * implement LCD extension for purging old log table entries -- refs #16897
    
    * remove v3 API call -- refs #16897
    
    * pull table names from logging schema for admin settings page -- refs #16897
    
    ---------
    
    Co-authored-by: Nate Frank <nfrank@nysenate.gov>

---
+++ civicrm/custom/ext/changelogretention/Civi/Api4/LogRetentionJob.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Civi\Api4;
+
+class LogRetentionJob extends Generic\AbstractEntity {
+  /**
+   * @param bool $checkPermissions
+   * @return Action\ChangeLogRetention\PurgeData
+   */
+  public static function purgeData(bool $checkPermissions = TRUE) {
+    return (new Action\ChangeLogRetention\PurgeData('LogRetentionJob', __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
+
+  public static function getFields($checkPermissions = TRUE) {
+    return (new Generic\BasicGetFieldsAction(__CLASS__, __FUNCTION__, function($getFieldsAction) {
+      return [
+
+      ];
+    }))->setCheckPermissions($checkPermissions);
+  }
+}
