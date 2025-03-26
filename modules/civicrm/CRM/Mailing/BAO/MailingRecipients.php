4c83357e63ff9d6fbd6b35b2738363441c378520
Nate Frank <nfrank@nysenate.gov>
2025-03-26 15:47:25 -0400

    use id instead of contact_id for predictable ordering of mailing recipients in SQL. Core Modification, but expected to be incorporated into CiviCore. refs #17075

--- modules/civicrm/CRM/Mailing/BAO/MailingRecipients.php
+++ modules/civicrm/CRM/Mailing/BAO/MailingRecipients.php
@@ -77,7 +77,7 @@ WHERE  mailing_id = %1
     if ($limitString) {
       $sqlObject->limit($limit, $offset);
     }
-    $sqlObject->orderBy(['r.contact_id']);
+    $sqlObject->orderBy(['r.id']);
     $sql = $sqlObject->toSQL();
     return CRM_Core_DAO::executeQuery($sql);
   }
