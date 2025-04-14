6fcd1e63ce73ffb376285daf011b48500b89abba
Nate Frank <nfrank@nysenate.gov>
2025-04-14 12:16:10 -0400

    add trim() to fix errors caused by tag name with leading white space - fixes #17132

--- civicrm/custom/php/CRM/NYSS/BAO/Integration/WebsiteEvent.php
+++ civicrm/custom/php/CRM/NYSS/BAO/Integration/WebsiteEvent.php
@@ -294,8 +294,12 @@ abstract class CRM_NYSS_BAO_Integration_WebsiteEvent implements CRM_NYSS_BAO_Int
     return $this->parent_tag_id;
   }
 
-  protected function setTagName(string $tag_name): static {
-    $this->tag_name = $tag_name;
+  protected function setTagName(string $tag_name, bool $clean=TRUE): static {
+    if ($clean) {
+      $this->tag_name = trim($tag_name);
+    } else {
+      $this->tag_name = $tag_name;
+    }
     return $this;
   }
 
