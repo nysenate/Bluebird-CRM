882c77821fb365105a08294cebf81410acfeab04
Nate Frank <nfrank@nysenate.gov>
2025-05-06 14:10:21 -0400

    fix glitchy strobe effect on Contact Activity List - fixes #17188

--- civicrm/custom/ext/gov.nysenate.contact/js/ActivityTab.js
+++ civicrm/custom/ext/gov.nysenate.contact/js/ActivityTab.js
@@ -1,44 +1,56 @@
 CRM.$(function($) {
-  var checkExist = setInterval(function() {
-    if ($('table.contact-activity-selector-activity tbody tr').length) {
-      $('table.contact-activity-selector-activity tbody tr').each(function() {
-        var id = $(this).prop('id');
-        var subject = $('table.contact-activity-selector-activity tbody tr#' + id + ' td.crmf-subject').text();
-        //console.log('subject: ', subject);
+  // NYSS #13163 and #12035
+  var nyss_customize_activities = function(trElement) {
+    try {
 
-        var url = '';
-        $(this).find('a.action-item').each(function(){
-          if ($(this).text() === 'View') {
-            url = $(this).prop('href');
-          }
-          //console.log('item: ', $(this).text());
-        });
-        //console.log('url: ', url);
+      var id = $(trElement).prop('id');
+      var subject = $(trElement).find('td.crmf-subject').text().trim();
 
-        //determine if there are attachments
-        var att = '';
-        CRM.api3('Attachment', 'get', {
-          "sequential": 1,
-          "entity_table": "civicrm_activity",
-          "entity_id": id
-        }).then(function(result) {
-          if (result.count) {
-            att = ' <i class="crm-i fa-paperclip"></i>';
-          }
-        }, function(error) {
-        });
-
-        var checkExist2 = setInterval(function() {
-          if ($('table.contact-activity-selector-activity tbody tr#' + id + ' td.crmf-subject div.crm-editable')) {
-            if (subject && url) {
-              $('table.contact-activity-selector-activity tbody tr#' + id + ' td.crmf-subject')
-                .html('<a href="' + url + '" class="crm-popup" title="View Activity">' + subject + att + '</a>');
-            }
-          }
-        }, 100);
+      var url = '';
+      $(trElement).find('a.action-item').each(function () {
+        if ($(this).text() === 'View') {
+          url = $(this).prop('href');
+        }
+        //console.log('item: ', $(this).text());
       });
 
-      clearInterval(checkExist);
+      if (subject && url) {
+        $(trElement).find('td.crmf-subject').html('<a href="' + url + '" class="crm-popup" title="View Activity">' + subject + '</a>');
+      }
+
+      CRM.api3('Attachment', 'get', {
+        "sequential": 1,
+        "entity_table": "civicrm_activity",
+        "entity_id": id
+      }).then(function(result) {
+        if (result.count) {
+          if ($(trElement).find('td.crmf-subject i.crm-i').length === 0) {
+            $(trElement).find('td.crmf-subject a').last().after('&nbsp;<i class="crm-i fa-paperclip"></i>');
+          }
+          //att = ' ';
+        }
+      }, function(error) {
+      });
+
+    } catch(e) {
+      console.error('MutationObserver error:', e);
     }
-  }, 100);
+  };
+
+  var callback = function(mutationsList, observer) {
+    for (var mutation of mutationsList) {
+        // be very specific about which elements we take action on
+        // to avoid too much repetitive processing.
+        if (mutation.target instanceof HTMLElement &&
+            mutation.target.classList.contains('crm-editable-enabled') &&
+            $(mutation.target).parent().hasClass('crmf-subject'))
+        {
+          nyss_customize_activities($(mutation.target).closest('tr'));
+        }
+    }
+  };
+
+  var observer = new MutationObserver(callback);
+  observer.observe(document.getElementById('ui-id-3'), {childList: true, subtree:true });
+
 });
