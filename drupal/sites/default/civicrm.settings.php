6534a6979a905bc4fc15bd10ee59db7447718e85
Nate Frank <nfrank@nysenate.gov>
2025-07-08 15:02:09 -0400

    remove Instant Message fields from Contact add/edit screens -- fixes #17215

--- drupal/sites/default/civicrm.settings.php
+++ drupal/sites/default/civicrm.settings.php
@@ -116,6 +116,7 @@ $prefs = &$civicrm_setting['domain'];
 // do_not_notify_assignees_for
 $prefs['advanced_search_options'] = SEP.implode(SEP, [1,2,3,4,5,6,10,13,16,17,18,19]).SEP;
 $prefs['contact_view_options'] = SEP.implode(SEP, [1,2,3,4,5,6,10,14,15]).SEP;
+$prefs['contact_edit_options'] = SEP.implode(SEP, [1,2,3,4,5,6,7,8,12,14,15,16,17]).SEP;
 $prefs['checksum_timeout'] = 7;
 $prefs['communityMessagesUrl'] = false;
 $prefs['contact_autocomplete_options'] = SEP.implode(SEP, [1,2,3,4,5,8,9]).SEP;
