d796037afbd0ae36c3ec50c5e075f99b02f6f2f9
Nate Frank <nfrank@nysenate.gov>
2025-10-29 10:26:09 -0400

    implement searching cases by subject in entity ref field; add Create Case functionality from matched incoming mail -- fixes #17369

--- civicrm/custom/ext/gov.nysenate.case/CRM/NYSS/Case/APIWrapper.php
+++ civicrm/custom/ext/gov.nysenate.case/CRM/NYSS/Case/APIWrapper.php
@@ -1,5 +1,6 @@
 <?php
 
+#[CRM_NYSS_Attribute_IssueRefs('17369')]
 class CRM_NYSS_Case_APIWrapper {
 
   public static function respond($event) {
@@ -13,6 +14,11 @@ class CRM_NYSS_Case_APIWrapper {
         $new_result = self::change_case_select_label($event);
         $event->setResponse($new_result);
         break;
+      case '3.casecontact.get':
+        // NYSS 17369
+        if (isset($apiRequest['params']['search_field_fallback'])) {
+          self::extend_case_contact_search($event);
+        }
     }
   }
 
@@ -34,4 +40,32 @@ class CRM_NYSS_Case_APIWrapper {
     return $result;
   }
 
+  #[CRM_NYSS_Attribute_IssueRefs('17369')]
+  public static function extend_case_contact_search($event) {
+    $result= $event->getResponse();
+    $apiRequest = $event->getApiRequest();
+    $params = $apiRequest['params'];
+    // NYSS #17369
+    // Replicate search_field_fallback behavior from...
+    // civicrm/api/v3/Generic/Getlist.php _civicrm_api3_generic_getlist_get_result
+    // because the Case Contact API doesn't provide a search_field_fallback.
+    if (isset($params['contact_id.sort_name']['LIKE']) &&
+        isset($params['search_field_fallback']) &&
+        $result['count'] < $apiRequest['params']['options']['limit']) {
+      //Civi::log()->debug('Result Count', [count($result)]);
+      $apiRequest['params'][$params['search_field_fallback']] = ['LIKE' => $params['contact_id.sort_name']['LIKE']];
+      if ($apiRequest['params']['options']['sort'] === 'contact_id.sort_name') {
+        $apiRequest['params']['options']['sort'] = $params['search_field_fallback'];
+      }
+      $apiRequest['params']['contact_id.sort_name'] = ['NOT LIKE' => $apiRequest['params']['contact_id.sort_name']['LIKE']];
+      $apiRequest['params']['options']['limit'] -= $result['count'];
+      // Remove search_field_fallback to prevent recursion (I assume it might recurse)
+      unset($apiRequest['params']['search_field_fallback']);
+
+      $result2 = civicrm_api3($apiRequest['entity'], 'get', $apiRequest['params']);
+      $result['values'] = array_merge($result['values'], $result2['values']);
+      $result['count'] = count($result['values']);
+      $event->setResponse($result);
+    }
+  }
 }
