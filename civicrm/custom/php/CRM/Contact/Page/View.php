77cbf440336147b1be5ffecadfd9b685b20e8f44
djnatedagreat <nfrank@nysenate.gov>
2026-01-08 15:02:07 -0500

    remove old/unused customization - refs #17698

--- civicrm/custom/php/CRM/Contact/Page/View.php
+++ civicrm/custom/php/CRM/Contact/Page/View.php
@@ -296,9 +296,7 @@ class CRM_Contact_Page_View extends CRM_Core_Page {
     }
 
     // Inline-edit places its own title on the page
-    //CRM_Utils_System::setTitle('CiviCRM', '<span id="crm-remove-title" style="display:none">CiviCRM</span>');
-    //NYSS 7337
-    CRM_Utils_System::setTitle($displayName, $title);
+    CRM_Utils_System::setTitle('CiviCRM', '<span id="crm-remove-title" style="display:none">CiviCRM</span>');
 
     return $title;
   }
