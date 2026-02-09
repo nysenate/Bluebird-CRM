5965d9d8444f239918453ee42b5d980ec9eef42e
djnatedagreat <nfrank@nysenate.gov>
2026-02-09 10:57:50 -0500

    include upgrade script for 3.10 release - fixes #17687

---
+++ scripts/v310.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# v39.sh
+# Upgrade Drupal Core to version 6.4.0
+#
+# Project: BluebirdCRM
+# Authors: Nate Frank
+# Organization: New York State Senate
+# Date: 2025-08-15
+#
+
+prog=`basename $0`
+script_dir=`dirname $0`
+drush=$script_dir/drush.sh
+cv=$script_dir/cv.sh
+clearCache=$script_dir/clearCache.sh
+readConfig=$script_dir/readConfig.sh
+
+. $script_dir/defaults.sh
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $prog instanceName" >&2
+  exit 1
+fi
+
+instance="$1"
+
+data_rootdir=`$readConfig --ig $instance data.rootdir` || data_rootdir="$DEFAULT_DATA_ROOTDIR"
+pubfiles_dir="$data_rootdir/$instance/pubfiles"
+
+if ! $readConfig --instance $instance --quiet; then
+  echo "$prog: $instance: Instance not found in config file" >&2
+  exit 1
+fi
+
+## upgrade civicrm db
+echo "running civicrm db upgrade and extension upgrades..."
+$cv $instance upgrade:db
+
+# NYSS 17368
+echo "enable Admin UI extension..."
+$drush $instance cvapi extension.enable key=civicrm_admin_ui --quiet
+
+$clearCache $instance
+
+echo "$prog: upgrade process is complete for $instance."
