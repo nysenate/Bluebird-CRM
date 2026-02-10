64fa4f85dfb8cca9905951048c6e0f40b03359fc
djnatedagreat <nfrank@nysenate.gov>
2026-02-10 11:35:28 -0500

    upgrade extension format to silence some php warnings - refs #17687

--- civicrm/custom/ext/gov.nysenate.tagdemographics/mixin/mgd-php@2.0.0.mixin.php
+++ civicrm/custom/ext/gov.nysenate.tagdemographics/mixin/mgd-php@2.0.0.mixin.php
@@ -3,8 +3,16 @@
 /**
  * Auto-register "**.mgd.php" files.
  *
+ * The older (mgd-php@1) and newer (mgd-php@2) are similar in that both load `*.mgd.php` files.
+ * However, they differ in how the search:
+ *
+ * - v1.x does a broad search over the entire extension source-tree.
+ * - v2.x does a narrower search in folders which commonly have mgds.
+ *   Within 2.x, future increments may add other paths (after vetting `universe` for impacts).
+ *
  * @mixinName mgd-php
- * @mixinVersion 1.0.0
+ * @mixinVersion 2.0.0
+ * @since 6.9
  *
  * @param CRM_Extension_MixInfo $mixInfo
  *   On newer deployments, this will be an instance of MixInfo. On older deployments, Civix may polyfill with a work-a-like.
@@ -23,7 +31,21 @@ return function ($mixInfo, $bootCache) {
       return;
     }
 
-    $mgdFiles = CRM_Utils_File::findFiles($mixInfo->getPath(), '*.mgd.php');
+    // Optimization: if managed entities were requested for specific module(s),
+    // check name and return early if not applicable.
+    if ($event->modules && !in_array($mixInfo->longName, $event->modules, TRUE)) {
+      return;
+    }
+
+    $path = $mixInfo->getPath();
+    $mgdFiles = array_merge(
+      (array) glob("$path/*.mgd.php"),
+      CRM_Utils_File::findFiles("$path/managed", '*.mgd.php'),
+      CRM_Utils_File::findFiles("$path/api", '*.mgd.php'),
+      CRM_Utils_File::findFiles("$path/CRM", '*.mgd.php'),
+      CRM_Utils_File::findFiles("$path/Civi", '*.mgd.php'),
+    );
+
     sort($mgdFiles);
     foreach ($mgdFiles as $file) {
       $es = include $file;
@@ -33,6 +55,9 @@ return function ($mixInfo, $bootCache) {
         }
         if (empty($e['params']['version'])) {
           $e['params']['version'] = '3';
+        }
+        if (empty($e['source'])) {
+          $e['source'] = $file;
         }
         $event->entities[] = $e;
       }
--- civicrm/custom/ext/gov.nysenate.webintegration/webintegration.php
+++ civicrm/custom/ext/gov.nysenate.webintegration/webintegration.php
@@ -12,30 +12,12 @@ function webintegration_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function webintegration_civicrm_xmlMenu(&$files) {
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function webintegration_civicrm_install() {
   _webintegration_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function webintegration_civicrm_uninstall() {
 }
 
 /**
@@ -48,74 +30,6 @@ function webintegration_civicrm_enable() {
 }
 
 /**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function webintegration_civicrm_disable() {
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function webintegration_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return;
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function webintegration_civicrm_managed(&$entities) {
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function webintegration_civicrm_caseTypes(&$caseTypes) {
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function webintegration_civicrm_angularModules(&$angularModules) {
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function webintegration_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-}
-
-/**
  * Functions below this ship commented out. Uncomment as required.
  *
 
@@ -124,9 +38,8 @@ function webintegration_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function webintegration_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -238,12 +151,4 @@ function _webintegration_extractSubject($msgNote) {
   ));*/
 
   return (!empty($matches[1])) ? $matches[1] : '';
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function webintegration_civicrm_postInstall() {
 }
